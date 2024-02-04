f21d577fa7cd5cce0c7f380f199827c93956dd52
Mathias Buus <mathiasbuus@gmail.com>
2024-02-04 23:50:49 +0100

    now can spawn the sidecar

--- cli.js
+++ cli.js
@@ -3,3 +3,15 @@ console.log()
 console.log('  --boot-sidecar, runs the sidecar')
 console.log('  --boot-terminal, runs the terminal')
 console.log()
+
+const connect = require('./lib/connect.js')
+
+test()
+
+async function test () {
+  const channel = await connect()
+  const res = await channel.request('info')
+
+  console.log(res)
+  channel.close()
+}
