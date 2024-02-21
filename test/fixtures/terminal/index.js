f06f860999d14721cf37014902cb4196a896acfa
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-02-21 21:45:26 +0800

    Add usage of updates in the test terminal app and switch console.log to stdout.write

--- test/fixtures/terminal/index.js
+++ test/fixtures/terminal/index.js
@@ -1,5 +1,6 @@
 /* global Pear */
-const { config,versions } = Pear
+import Pipe from 'bare-pipe'
+const { config, versions, updates } = Pear
 const [grn, rst, dim] = ['\x1b[32m', '\x1b[0m', '\x1b[2m']
 const v = ({ key, length, fork }) => `v${fork}.${length}.${(key += '').length <= 12 ? key : key.slice(0, 12) + '…'}`
 const { app, platform } = await versions()
@@ -29,4 +30,15 @@ const out = `${grn}           ▅
        ▄▄▄▄▆▆▆▆
 `
 console.log('\n\x1b[s\x1b[J' + out + '\x1b[0m')
-if (debug.ready) console.log('[DEBUG] READY')
+if (debug.ready) {
+  setTimeout(() => Bare.exit(1), 20000)
+
+  const stdout = new Pipe(1)
+  stdout.write('[DEBUG] READY\n')
+
+  let counter = 0;
+  updates(() => {
+    stdout.write(`[DEBUG] UPDATE${++counter}\n`)
+    if (counter >= 2) Bare.exit(0)
+  })
+}
