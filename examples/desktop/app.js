8eb8584ec6efcfbfe99541212d23e73fc79add37
David Mark Clements <david.mark.clements@gmail.com>
2024-02-21 14:16:49 +0100

    release fix  (#42)
    * release fix
    
    * update example with release and channel
    
    * lint
    
    * clarify code and output

--- examples/desktop/app.js
+++ examples/desktop/app.js
@@ -13,6 +13,8 @@ async function receiveWakeups () {
 }
 
 receiveWakeups().catch(console.error)
+document.getElementById('channel').innerText = config.channel || 'none [ dev ]'
+document.getElementById('release').innerText = config.release || (config.dev ? 'none [ dev ]' : '0')
 const { app, platform } = await versions()
 document.getElementById('platformKey').innerText = platform.key
 document.getElementById('platformFork').innerText = platform.fork
