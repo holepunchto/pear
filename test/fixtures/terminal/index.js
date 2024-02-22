c5d57bd160ec358004eccf4120222d70b3f89311
ruy-dan <ruy-dan@protonmail.com>
2024-02-22 13:36:34 +0100

    fix import

--- test/fixtures/terminal/index.js
+++ test/fixtures/terminal/index.js
@@ -1,5 +1,5 @@
 /* global Pear */
-const { config,versions,teardown } = Pear
+const { config,versions } = Pear
 const [grn, rst, dim] = ['\x1b[32m', '\x1b[0m', '\x1b[2m']
 const v = ({ key, length, fork }) => `v${fork}.${length}.${(key += '').length <= 12 ? key : key.slice(0, 12) + '…'}`
 const { app, platform } = await versions()
