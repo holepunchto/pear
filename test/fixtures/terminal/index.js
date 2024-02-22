9e060f3c1c42c67ee3d6ade8b4f3104bb3afa179
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-02-23 01:31:13 +0800

    Rename update to updates and run it only when updates flag is set

--- test/fixtures/terminal/index.js
+++ test/fixtures/terminal/index.js
@@ -6,7 +6,7 @@ const [grn, rst, dim] = ['\x1b[32m', '\x1b[0m', '\x1b[2m']
 const v = ({ key, length, fork }) => `v${fork}.${length}.${(key += '').length <= 12 ? key : key.slice(0, 12) + '…'}`
 const { app, platform } = await versions()
 
-const debug = { ready: false, update: false }
+const debug = { ready: false, updates: false }
 
 const argv = global.Bare?.argv || global.process.argv
 const debarg = argv.find(arg => arg.startsWith('--debug='))
@@ -14,7 +14,7 @@ const debarg = argv.find(arg => arg.startsWith('--debug='))
 if (debarg) {
   const opts = debarg.split('=')[1].split(',')
   debug.ready = opts.includes('ready')
-  debug.update = opts.includes('update')
+  debug.updates = opts.includes('updates')
 }
 
 const out = `${grn}           ▅
@@ -31,15 +31,16 @@ const out = `${grn}           ▅
        ▄▄▄▄▆▆▆▆
 `
 console.log('\n\x1b[s\x1b[J' + out + '\x1b[0m')
-if (debug.ready) {
-  setTimeout(() => Bare.exit(1), 20000)
+const stdout = new Pipe(1)
 
-  const stdout = new Pipe(1)
-  stdout.write('[DEBUG] READY\n')
+if (debug.ready) stdout.write('[DEBUG] READY\n')
 
+if (debug.updates) {
   let counter = 0
   updates(() => {
     stdout.write(`[DEBUG] UPDATE${++counter}\n`)
     if (counter >= 2) Bare.exit(0)
   })
+} else {
+  stdout.unref()
 }
