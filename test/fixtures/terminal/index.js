7a3966aee0da1bd1443b00625852575317a730e4
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-02-26 21:33:13 +0800

    Replace terminal fixture with an inspectable terminal app

--- test/fixtures/terminal/index.js
+++ test/fixtures/terminal/index.js
@@ -1,52 +1,16 @@
 import Pipe from 'bare-pipe'
+import nodeInspector from 'inspector'
+import { Inspector } from 'pear-inspect'
 
-/* global Pear,Bare */
-const { config, versions, updates } = Pear
-const [grn, rst, dim] = ['\x1b[32m', '\x1b[0m', '\x1b[2m']
-const v = ({ key, length, fork }) => `v${fork}.${length}.${(key += '').length <= 12 ? key : key.slice(0, 12) + '…'}`
-const { app, platform } = await versions()
+global.Pipe = Pipe
+const stdout = global.stdout = new Pipe(1)
 
-const debug = { ready: false, updates: false }
+const inspector = new Inspector({ inspector: nodeInspector })
+const key = await inspector.enable()
 
-const argv = global.Bare?.argv || global.process.argv
-const debarg = argv.find(arg => arg.startsWith('--debug='))
-
-if (debarg) {
-  const opts = debarg.split('=')[1].split(',')
-  debug.ready = opts.includes('ready')
-  debug.updates = opts.includes('updates')
+global.endInspection = async function () {
+  await inspector.disable()
+  Bare.exit(0)
 }
 
-const out = `${grn}           ▅
-           ▀
-        ▂▂▄▟▙▃
-       ▄▄▄▄▆▆▆▆         ${config.name}
-      ▄▄▄▄▄▆▆▆▆▆        ${dim}${v(app)}${rst}${grn}
-      ▄▄▄▄▄▆▆▆▆▆
-     ▄▄▄▄▄▄▆▆▆▆▆▆       ${rst}${grn}pear
-    ▃▄▄▄▄▄▄▆▆▆▆▆▆▄      ${dim}${v(platform)}${rst}${grn}
-   ▄▄▄▄▄▄▄▄▆▆▆▆▆▆▆▆
-   ▄▄▄▄▄▄▄▄▆▆▆▆▆▆▆▆     ${rst}${grn}Welcome to the IoP
-     ▄▄▄▄▄▄▆▆▆▆▆▆
-       ▄▄▄▄▆▆▆▆
-`
-console.log('\n\x1b[s\x1b[J' + out + '\x1b[0m')
-const stdout = new Pipe(1)
-
-if (debug.ready) stdout.write('[DEBUG] READY\n')
-
-if (debug.updates) {
-  let counter = 0
-  updates(() => {
-    stdout.write(`[DEBUG] UPDATE${++counter}\n`)
-
-    if (counter >= 1) {
-      stdout.unref()
-
-      // Give time for stdout to drain before exiting
-      setTimeout(() => Bare.exit(0), 1000)
-    }
-  })
-} else {
-  stdout.unref()
-}
+stdout.write(`${key.toString('hex')}\n`)
