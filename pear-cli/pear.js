1196357980892a6ded9e1a6eec0f0be8fb549f12
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-04-26 04:31:38 +0800

    Add path setup command for MS PowerShell

--- pear-cli/pear.js
+++ pear-cli/pear.js
@@ -41,7 +41,12 @@ if (isInstalled()) {
       console.log()
       console.log('Or by adding the following to your path')
       console.log()
-      console.log(isWindows ? `set PATH="${BIN};%PATH%"` : `export PATH="${BIN}:$PATH"`)
+      if (isWindows) {
+        console.log(`cmd:        set PATH="${BIN};%PATH%"`)
+        console.log(`PowerShell: $env:PATH="${BIN};$env:PATH"`)
+      } else {
+        console.log(`export PATH="${BIN}:$PATH"`)
+      }
     }
   })
 }
