c78026de87552e0a6ab45752a33f48f0f83dba3d
Dan <ruy-dan@protonmail.com>
2024-05-01 17:35:40 +0200

    Pear gc sidecar (#137)
    * gc sidecar as stream, add usage
    
    * fix win
    
    * remove unused code
    
    * use stream, remove GC class
    
    * remove unused prop
    
    * GC as stream, use os.kill
    
    * use text-decoder
    
    * fix win
    
    * gc to lib/gc
    
    * rebase and update lib errors
    
    * remove text-decoder
    
    * cmd to resource, errors
    
    * add await
    
    * inline close

---
+++ cmd/gc.js
@@ -0,0 +1,33 @@
+'use strict'
+const { print, outputter, InputError } = require('./iface')
+const parse = require('../lib/parse')
+
+const output = outputter('gc', {
+  kill: ({ pid }) => `Killed sidecar with pid: ${pid}`,
+  complete: ({ killed }) => { return killed.length > 0 ? `Total killed sidecars: ${killed.length}` : 'No running sidecars' },
+  error: ({ code, message, stack }) => `GC Error (code: ${code || 'none'}) ${message} ${stack}`
+})
+
+module.exports = (ipc) => async function gc (args) {
+  try {
+    const flags = parse.args(args, {
+      boolean: ['json']
+    })
+    const { _, json } = flags
+    const [resource] = _
+    if (!resource) throw new InputError('A <cmd> must be specified.')
+    if (resource !== 'sidecar') throw new InputError(`Resource '${resource}' is not valid`)
+    const stream = ipc.gc({ pid: Bare.pid, resource }, ipc)
+    await output(json, stream)
+  } catch (err) {
+    if (err instanceof InputError || err.code === 'ERR_INVALID_FLAG') {
+      print(err.message, false)
+      ipc.userData.usage.output('gc')
+    } else {
+      print('An error occured', false)
+    }
+    Bare.exit(1)
+  } finally {
+    await ipc.close()
+  }
+}
