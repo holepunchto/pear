b09acd7494ce5249fcd1af6231c394f14b7979a7
dmc <david.mark.clements@gmail.com>
2024-05-01 15:23:21 +0100

    last InputError -> ERR_INPUT

--- cmd/dump.js
+++ cmd/dump.js
@@ -1,7 +1,8 @@
 'use strict'
 const os = require('bare-os')
 const { isAbsolute, resolve } = require('bare-path')
-const { outputter, InputError } = require('./iface')
+const { outputter } = require('./iface')
+const { ERR_INPUT } = require('../lib/errors')
 const output = outputter('stage', {
   dumping: ({ key, dir }) => `\n🍐 Dumping ${key} into ${dir}`,
   complete: '\nDumping complete!\n',
@@ -11,7 +12,7 @@ const output = outputter('stage', {
 module.exports = (ipc) => async function dump (cmd) {
   const { checkout, json } = cmd.flags
   const { key, dir = os.cwd() } = cmd.args
-  if (!dir) throw new InputError('Output dir must be specified.')
-  if (!key) throw new InputError('The pear key must be specified.')
+  if (!dir) throw new ERR_INPUT('Output dir must be specified.')
+  if (!key) throw new ERR_INPUT('The pear link must be specified.')
   await output(json, ipc.dump({ id: Bare.pid, key, dir: isAbsolute(dir) ? dir : resolve(os.cwd(), dir), checkout }))
 }
