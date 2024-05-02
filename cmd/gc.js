c5803fd488b2416c1ad8543f14da7b840151a641
ruy-dan <ruy-dan@protonmail.com>
2024-05-02 14:55:38 +0200

    add gc release

--- cmd/gc.js
+++ cmd/gc.js
@@ -3,8 +3,8 @@ const { outputter } = require('./iface')
 const { ERR_INVALID_INPUT } = require('../lib/errors')
 
 const output = outputter('gc', {
-  kill: ({ pid }) => `Killed sidecar with pid: ${pid}`,
-  complete: ({ killed }) => { return killed.length > 0 ? `Total killed sidecars: ${killed.length}` : 'No running sidecars' },
+  remove: ({ resource, id }) => `Removed ${resource} '${id}'`,
+  complete: ({ resource, count }) => { return count > 0 ? `Total ${resource}s removed: ${count}` : `No ${resource}s removed` },
   error: ({ code, message, stack }) => `GC Error (code: ${code || 'none'}) ${message} ${stack}`
 })
 
@@ -12,7 +12,7 @@ module.exports = (ipc) => async function gc (cmd) {
   const { json } = cmd.flags
   const { resource } = cmd.args
   if (!resource) throw new ERR_INVALID_INPUT('A <resource> must be specified.')
-  if (resource !== 'sidecar') throw new ERR_INVALID_INPUT(`Resource '${resource}' is not valid for gc`)
+  if (resource !== 'release' && resource !== 'sidecar') throw new ERR_INVALID_INPUT(`Resource '${resource}' is not valid for gc`)
   const stream = ipc.gc({ pid: Bare.pid, resource }, ipc)
   await output(json, stream)
 }
