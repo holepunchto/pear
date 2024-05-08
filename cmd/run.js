60fc1da322cc9e5bdf486460a53d9f36cd735f22
David Mark Clements <david.mark.clements@gmail.com>
2024-05-08 13:26:01 +0100

    Paparam (#140)
    * Clean up errors (#125)
    
    * added errors class and replaced gui errors
    
    * clean up errors in preload, parse and run
    
    * refactored sidecar errors
    
    * replaced iface InputError
    
    * reverted errors clean up in cmd and prefixed with ERR
    
    * removed static functions from PearError
    
    * added ERR_ASSERTION
    
    * added invalid storage error
    
    * added ERR_INSTANCE_CLOSED
    
    * reverted changes in cmd/seed
    
    * renamed ERR_SHIFT_STORAGE to ERR_SHIFT_STORAGE_ERROR
    
    * reverted error cleanup in gui/* files
    
    ---------
    
    Co-authored-by: rafapaezbas <rafa@holepunch.com>
    
    * paparam
    
    * working
    
    * fixes & towards bail handling
    
    * command runners, error handling, rm usage, almost everything working, run still todo
    
    * bail shows usage for known input errors, unknown errors log and exit early
    
    * last InputError -> ERR_INPUT
    
    * tweaks, term run fix
    
    * ERR_UKNOWN_GC_RESOURCE -> ERR_INVALID_GC_RESOURCE
    
    * ERR_INVALID_INPUT
    
    * nearly, still some run issues
    
    * run working
    
    * media.access api fix
    
    * run/parse-link rm lib/parse
    
    * run fix
    
    * trace semi-fixed
    
    * warmup fix
    
    * working with appling
    
    * rebase fixes
    
    * improve unrecognized arg output, do not stage cli crash log
    
    * rm stray log
    
    * test fix
    
    ---------
    
    Co-authored-by: rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>
    Co-authored-by: rafapaezbas <rafa@holepunch.com>

--- cmd/run.js
+++ cmd/run.js
@@ -1,10 +1,5 @@
 'use strict'
-const os = require('bare-os')
-const fs = require('bare-fs')
-const path = require('bare-path')
-const { fileURLToPath } = require('url-file-url')
-const { outputter, print, InputError, stdio, interact, ansi } = require('./iface')
-const parse = require('../lib/parse')
+const { outputter, trust, stdio } = require('./iface')
 
 const output = outputter('run', {
   exit: ({ code }) => Bare.exit(code),
@@ -13,84 +8,16 @@ const output = outputter('run', {
   loaded: (data, { loading }) => loading && loading.clear(data.forceClear || false)
 })
 
-module.exports = (ipc) => async function run (args, devrun = false) {
-  let dir = null
-  let key = null
-  let askTrust = false
+module.exports = (ipc) => async function run (cmd, devrun = false) {
   try {
-    const { json, dev, detached, store, 'ask-trust': ask, _ } = parse.args(args, {
-      boolean: ['json', 'dev', 'tmp-store', 'detached', 'ask-trust'],
-      string: ['store', 'link', 'checkout'],
-      alias: { store: 's', 'tmp-store': 't' },
-      default: { json: false, dev: false, detached: false, 'ask-trust': true }
-    })
-    askTrust = ask
-    if (!_[0]) {
-      if (devrun) {
-        _[0] = '.'
-        args.push(_[0])
-      } else {
-        throw new InputError('Missing argument: pear run <key|dir|alias>')
-      }
-    }
-    if (_[0].startsWith('pear:') && _[0].slice(5, 7) !== '//') {
-      throw new InputError('Key must start with pear://')
-    }
-    key = parse.runkey(_[0]).key
-    if (key !== null && _[0].startsWith('pear://') === false) {
-      throw new InputError('Key must start with pear://')
-    }
-    const cwd = os.cwd()
-    dir = key === null ? (_[0].startsWith('file:') ? fileURLToPath(_[0]) : _[0]) : cwd
+    const { json, detached, store } = cmd.flags
 
-    if (path.isAbsolute(dir) === false) {
-      const resolved = path.resolve(cwd, dir)
-      dir = args[args.indexOf(dir)] = resolved
-    }
-    if (dir !== cwd) os.chdir(dir)
-    if (key === null) {
-      try {
-        JSON.parse(fs.readFileSync(path.join(dir, 'package.json')))
-      } catch (err) {
-        throw new InputError(`A valid package.json file must exist at: "${dir}"`, { showUsage: false })
-      }
-    }
-    await output(json, await require('../lib/run')({ ipc, key, args, dev, dir, storage: store, detached }))
+    if (devrun && !cmd.args.link) cmd.args.link = '.'
+
+    const args = Bare.argv.slice(2)
+    await output(json, await require('../run')({ flags: cmd.flags, link: cmd.args.link, appArgs: cmd.rest, ipc, args, storage: store, detached }))
   } catch (err) {
-    if (err.code === 'ERR_PERMISSION_REQUIRED') {
-      if (askTrust === false) {
-        Bare.exit(1)
-        return
-      }
-      const sure = ansi.cross + ' Key pear://' + key?.z32 + ' is not known\n\nBe sure that software is trusted before running it\n\nType "TRUST" to allow execution or anything else to exit\n\n'
-      const prompt = interact(sure, [
-        {
-          name: 'trust',
-          default: '',
-          prompt: 'Trust application',
-          delim: '?',
-          validation: (value) => !(value.toLowerCase() !== 'trust' && value === 'TRUST'),
-          msg: ansi.cross + ' uppercase TRUST to confirm'
-        }
-      ])
-      const result = await prompt.run()
-      if (result.fields.trust === 'TRUST') {
-        await ipc.trust(key)
-        print('\n' + ansi.tick + ' pear://' + key?.z32 + ' is now trusted\n')
-        print('Use pear run again to execute trusted application\n')
-      } else {
-        print('')
-        print(err.message + '\n', false)
-      }
-    } else if (err instanceof InputError || err.code === 'ERR_INVALID_FLAG') {
-      print(err.message, false)
-      if (err.showUsage) ipc.userData.usage.output('run')
-    } else if (err.code === 'ENOENT') {
-      print(err.message[0].toUpperCase() + err.message.slice(1) + ': "' + dir + '"', false)
-    } else {
-      print('An error occured', false)
-      console.error(err)
-    }
-    Bare.exit(1)
+    if (err.code !== 'ERR_PERMISSION_REQUIRED') throw err
+    await trust({ ipc, key: err.key, message: err.message })
   }
 }
---
+++ run/definition.js
@@ -0,0 +1,25 @@
+'use strict'
+const { flag, hiddenFlag, arg, rest } = require('paparam')
+
+module.exports = [
+  arg('<link|dir>', 'Pear link, alias or directory to run app from'),
+  rest('[...app-args]', 'Application arguments'),
+  flag('--dev', 'Enable --devtools & --updates-diff'),
+  flag('--devtools', 'Open devtools with application [Desktop]'),
+  flag('--updates-diff', 'Enable diff computation for Pear.updates'),
+  flag('--no-updates', 'Disable updates firing via Pear.updates'),
+  flag('--link <url>', 'Simulate deep-link click open'),
+  flag('--store|-s <path>', 'Set the Application Storage path'),
+  flag('--tmp-store|-t', 'Automatic new tmp folder as store path'),
+  flag('--chrome-webrtc-internals', 'Enable chrome://webrtc-internals'),
+  flag('--unsafe-clear-app-storage', 'Clear app storage'),
+  flag('--unsafe-clear-preferences', 'Clear preferences (such as trustlist)'),
+  flag('--appling <path>', 'Set application shell path'),
+  flag('--checkout <n|release|staged>', 'Run a checkout from version length'),
+  flag('--detached', 'Wakeup existing app or run detached'),
+  hiddenFlag('--detach'),
+  hiddenFlag('--trace <n>'),
+  hiddenFlag('--swap <path>'),
+  hiddenFlag('--start-id <id>'),
+  hiddenFlag('--no-sandbox') // electron passthrough
+]
