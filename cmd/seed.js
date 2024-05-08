73e799c6d1af287d6f7ee918cd1272725772f2c1
David Mark Clements <david.mark.clements@gmail.com>
2024-05-08 13:26:01 +0100

    Paparam (#140)
    * Clean up errors (#125)
    
    * added errors class and replaced gui errors
    
    * clean up errors in preload, parse and run
    
    * refactored sidecar errors
    
    * replaced iface InputError
    
    * reverted errors clean up in cmd and prefixed with ERR
    
    * removed static functions from PearError
    
    * added ERR_ASSERTION
    
    * added invalid storage error
    
    * added ERR_INSTANCE_CLOSED
    
    * reverted changes in cmd/seed
    
    * renamed ERR_SHIFT_STORAGE to ERR_SHIFT_STORAGE_ERROR
    
    * reverted error cleanup in gui/* files
    
    ---------
    
    Co-authored-by: rafapaezbas <rafa@holepunch.com>
    
    * paparam
    
    * working
    
    * fixes & towards bail handling
    
    * command runners, error handling, rm usage, almost everything working, run still todo
    
    * bail shows usage for known input errors, unknown errors log and exit early
    
    * last InputError -> ERR_INPUT
    
    * tweaks, term run fix
    
    * ERR_UKNOWN_GC_RESOURCE -> ERR_INVALID_GC_RESOURCE
    
    * ERR_INVALID_INPUT
    
    * nearly, still some run issues
    
    * run working
    
    * media.access api fix
    
    * run/parse-link rm lib/parse
    
    * run fix
    
    * trace semi-fixed
    
    * warmup fix
    
    * working with appling
    
    * rebase fixes
    
    * improve unrecognized arg output, do not stage cli crash log
    
    * rm stray log
    
    * test fix
    
    ---------
    
    Co-authored-by: rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>
    Co-authored-by: rafapaezbas <rafa@holepunch.com>

--- cmd/seed.js
+++ cmd/seed.js
@@ -2,12 +2,12 @@
 const os = require('bare-os')
 const { readFile } = require('bare-fs/promises')
 const { join } = require('bare-path')
-const parse = require('../lib/parse')
-const { outputter, print, ansi, InputError } = require('./iface')
+const parseLink = require('../run/parse-link')
+const { outputter, ansi } = require('./iface')
 
 const output = outputter('seed', {
   seeding: ({ key, name, channel }) => `\n${ansi.pear} Seeding: ${key || `${name} [ ${channel} ]`}\n   ${ansi.dim('ctrl^c to stop & exit')}\n`,
-  key: (info) => `-o-:-\n    pear://${info}\n...`,
+  key: (info) => `---:\n pear://${info}\n...`,
   'content-key': (info) => `Content core key (hex) :-\n\n    ${info}\n`,
   'meta-key': (info) => `Meta discovery key (hex) :-\n\n    ${info}\n`,
   'meta-discovery-key': (info) => `Meta core discovery key (hex) :-\n\n    ${info}\n`,
@@ -16,37 +16,17 @@ const output = outputter('seed', {
   'peer-remove': (info) => `-_- peer drop ${info}`
 })
 
-module.exports = (ipc) => async function seed (args) {
-  const parsed = parse.args(args, {
-    boolean: ['verbose', 'json'],
-    string: ['seeders', 'name'],
-    alias: { verbose: 'v' }
-  })
-  const { _, json, verbose, seeders } = parsed
-  try {
-    const [from, dir = os.cwd()] = _
-    const isKey = parse.runkey(from.toString()).key !== null
-    const channel = isKey ? null : from
-    const key = isKey ? from : null
-    let { name } = parsed
-    if (!name && !key) {
-      const pkg = JSON.parse(await readFile(join(dir, 'package.json')))
-      name = pkg.pear?.name || pkg.name
-    }
-    const id = Bare.pid
-    await output(json, ipc.seed({ id, name, channel, key, verbose, seeders, dir, clientArgv: Bare.argv }))
-  } catch (err) {
-    if (err instanceof InputError || err.code === 'ERR_INVALID_FLAG') {
-      if (json) {
-        print(JSON.stringify({ cmd: 'seed', type: 'error', message: err.message, stack: err.stack, code: err.code }))
-      } else {
-        print(err.message, false)
-        ipc.userData.usage.output('seed')
-      }
-    } else {
-      print('An error occured', false)
-      console.error(err)
-    }
-    Bare.exit(1)
+module.exports = (ipc) => async function seed (cmd) {
+  const { json, verbose, seeders } = cmd.flags
+  const { dir = os.cwd() } = cmd.args
+  const isKey = parseLink(cmd.args.channel).key !== null
+  const channel = isKey ? null : cmd.args.channel
+  const link = isKey ? cmd.args.channel : null
+  let { name } = cmd.flags
+  if (!name && !link) {
+    const pkg = JSON.parse(await readFile(join(dir, 'package.json')))
+    name = pkg.pear?.name || pkg.name
   }
+  const id = Bare.pid
+  await output(json, ipc.seed({ id, name, channel, link, verbose, seeders, dir, clientArgv: Bare.argv }))
 }
