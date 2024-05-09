5adc6b27cce513f1736daa63e60fd7a9c8075dbb
dmc <david.mark.clements@gmail.com>
2024-05-09 15:11:52 +0200

    fixes, stage warmup still to fix

--- cmd/release.js
+++ cmd/release.js
@@ -6,9 +6,9 @@ const { ERR_INVALID_INPUT } = require('../lib/errors')
 const parseLink = require('../run/parse-link')
 
 const output = outputter('release', {
-  releasing: ({ name, channel }) => `\n${ansi.pear} Releasing ${name} [ ${channel} ]\n`,
+  releasing: ({ name, channel, link }) => `\n${ansi.pear} Releasing ${name} [ ${channel || link} ]\n`,
   'updating-to': ({ releaseLength, currentLength }) => `Current length is ${currentLength}\nSetting release to ${releaseLength}\n`,
-  released: ({ name, channel, length }) => `The ${name} app (${channel} channel) was successfully released.\nLatest length: ${length}\n`,
+  released: ({ name, channel, link, length }) => `The ${name} app (${channel || link} channel) was successfully released.\nLatest length: ${length}\n`,
   final: { output: 'print', message: 'Release complete\n', success: true }
 })
 
@@ -16,13 +16,13 @@ module.exports = (ipc) => async function release (cmd) {
   const { checkout, name, json } = cmd.flags
   const isKey = parseLink(cmd.args.channel).key !== null
   const channel = isKey ? null : cmd.args.channel
-  const key = isKey ? cmd.args.channel : null
-  if (!channel && !key) throw new ERR_INVALID_INPUT('A key or the channel name must be specified.')
+  const link = isKey ? cmd.args.channel : null
+  if (!channel && !link) throw new ERR_INVALID_INPUT('A pear link or the channel name must be specified.')
   let dir = cmd.args.dir || os.cwd()
   if (isAbsolute(dir) === false) dir = resolve(os.cwd(), dir)
   if (checkout !== undefined && Number.isInteger(+checkout) === false) {
     throw new ERR_INVALID_INPUT('--checkout flag must supply an integer if set')
   }
   const id = Bare.pid
-  await output(json, ipc.release({ id, name, channel, key, checkout, dir }))
+  await output(json, ipc.release({ id, name, channel, link, checkout, dir }))
 }
