73f04e5a351cbfe0b7107563b86b7be20d3e80aa
David Mark Clements <david.mark.clements@gmail.com>
2024-05-10 14:29:02 +0200

    ipc: stream handlers converted from async gens (#120)
    * ipc: stream handlers converted from async gens
    
    * fixes, stage warmup still to fix
    
    * run fix
    
    * ERR_INPUT not a constructor
    
    * ERR_INPUT -> ERR_INVALID_INPUT
    
    * tweaks/fixes
    
    * tweaks

--- cmd/release.js
+++ cmd/release.js
@@ -6,7 +6,7 @@ const { ERR_INVALID_INPUT } = require('../lib/errors')
 const parseLink = require('../run/parse-link')
 
 const output = outputter('release', {
-  releasing: ({ name, channel }) => `\n${ansi.pear} Releasing ${name} [ ${channel} ]\n`,
+  releasing: ({ name, channel, link }) => `\n${ansi.pear} Releasing ${name} [ ${channel || link} ]\n`,
   'updating-to': ({ releaseLength, currentLength }) => `Current length is ${currentLength}\nSetting release to ${releaseLength}\n`,
   released: ({ name, channel, length }) => `The ${name} app (${channel} channel) was successfully released.\nLatest length: ${length}\n`,
   error: ({ code, stack }) => `Releasing Error (code: ${code || 'none'}) ${stack}`,
@@ -17,13 +17,13 @@ module.exports = (ipc) => async function release (cmd) {
   const { checkout, name, json } = cmd.flags
   const isKey = parseLink(cmd.args.channel).key !== null
   const channel = isKey ? null : cmd.args.channel
-  const key = isKey ? cmd.args.channel : null
-  if (!channel && !key) throw new ERR_INVALID_INPUT('A key or the channel name must be specified.')
+  const link = isKey ? cmd.args.channel : null
+  if (!channel && !link) throw ERR_INVALID_INPUT('A valid pear link or the channel name must be specified.')
   let dir = cmd.args.dir || os.cwd()
   if (isAbsolute(dir) === false) dir = resolve(os.cwd(), dir)
   if (checkout !== undefined && Number.isInteger(+checkout) === false) {
-    throw new ERR_INVALID_INPUT('--checkout flag must supply an integer if set')
+    throw ERR_INVALID_INPUT('--checkout flag must supply an integer if set')
   }
   const id = Bare.pid
-  await output(json, ipc.release({ id, name, channel, key, checkout, dir }))
+  await output(json, ipc.release({ id, name, channel, link, checkout, dir }))
 }
