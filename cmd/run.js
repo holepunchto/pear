4bb8e108417f3dd30eb06eded51ec7efb04c55a4
dmc <david.mark.clements@gmail.com>
2024-05-13 18:34:07 +0200

    pear dev fix (config.args should always be an array)

--- cmd/run.js
+++ cmd/run.js
@@ -18,7 +18,8 @@ module.exports = (ipc) => async function run (cmd, devrun = false) {
     }
 
     const args = Bare.argv.slice(2)
-    await output(json, await require('../run')({ flags: cmd.flags, link: cmd.args.link, appArgs: cmd.rest, ipc, args, storage: store, detached }))
+    const appArgs = cmd.rest || []
+    await output(json, await require('../run')({ flags: cmd.flags, link: cmd.args.link, appArgs, ipc, args, storage: store, detached }))
   } catch (err) {
     if (err.code !== 'ERR_PERMISSION_REQUIRED') throw err
     await trust({ ipc, key: err.key, message: err.message })
