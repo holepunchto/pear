36925696f7d5fe431e56cb4bc978cc8fa6289363
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-06-20 16:58:17 +0800

    Add indicator if a hard or soft restart is being done

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -490,7 +490,7 @@ class Sidecar extends ReadyResource {
   }
 
   async restart ({ platform = false, hard = false } = {}, client) {
-    if (this.verbose) console.log('Restarting ' + (platform ? 'platform' : 'client'))
+    if (this.verbose) console.log(`${hard ? 'Hard' : 'Soft'} restarting ${platform ? 'platform' : 'client'}`)
     if (platform === false) {
       const { dir, cwd, cmdArgs, env } = client.userData.state
       const appling = client.userData.state.appling
