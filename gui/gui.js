6c688d5ec617b22829dc9b9c9e4d777f8a033cd7
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-06-22 01:54:47 +0800

    Use config.options.allowed hosts and move filters to after the interload

--- gui/gui.js
+++ gui/gui.js
@@ -986,17 +986,6 @@ class Window extends GuiCtrl {
     const session = electron.session.fromPartition(`persist:${this.sessname || this.state.key?.z32 || this.state.dir}`)
     session.setUserAgent(ua)
 
-    const allowedHosts = [new URL(this.entry).host, ...(this?.state?.manifest?.pear?.allowedHosts ?? [])]
-    const requestFilter = (request) => {
-      const requestUrl = new URL(request.url)
-      return allowedHosts.includes(requestUrl.host)
-        ? electron.net.fetch(request, { bypassCustomProtocolHandlers: true })
-        : Object.assign(new Response(null, { status: 403 }))
-    }
-
-    session.protocol.handle('https', requestFilter)
-    session.protocol.handle('http', requestFilter)
-
     const { show = true } = { show: (options.show || options.window?.show) }
     const { height = this.constructor.height, width = this.constructor.width } = options
     this.win = new BrowserWindow({
@@ -1067,6 +1056,17 @@ class Window extends GuiCtrl {
     const decalLoading = this.win.loadURL(decalUrl)
 
     if (interload && (await interload(this)) === false) return false
+
+    const allowedHosts = [new URL(this.entry).host, ...(this?.state?.config?.options?.allowedHosts ?? [])]
+    const requestFilter = (request) => {
+      const requestUrl = new URL(request.url)
+      return allowedHosts.includes(requestUrl.host)
+        ? electron.net.fetch(request, { bypassCustomProtocolHandlers: true })
+        : Object.assign(new Response(null, { status: 403 }))
+    }
+
+    session.protocol.handle('https', requestFilter)
+    session.protocol.handle('http', requestFilter)
 
     if (this.closing) return false
 
