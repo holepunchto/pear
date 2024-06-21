abb178fc45f26e9528037b79243d5f62fd559336
dmc <david.mark.clements@gmail.com>
2024-06-21 18:45:51 +0200

    test preinstall checks part of test run

---
+++ test/pretest.js
@@ -0,0 +1,47 @@
+'use strict'
+const fs = require('bare-fs')
+const path = require('bare-path')
+const { spawn } = require('bare-subprocess')
+const { isWindows } = require('which-runtime')
+
+const { protocol, pathname } = new URL(Pear.config.applink)
+const force = Pear.config.args.includes('--force-install')
+
+async function install () {
+  if (protocol !== 'file:') return
+  const root = path.normalize(pathname)
+  const dirs = [
+    path.join(root, 'test', 'fixtures', 'terminal', 'node_modules')
+  ]
+  for (const dir of dirs) {
+    if (force === false && await exists(dir)) continue
+    console.log(force ? 'reinstalling node_modules in' : `node_modules not found in`, path.dirname(dir))
+    console.log(`Running npm install...`)
+    await run('npm', ['install'], { stdio: 'inherit', cwd: path.dirname(dir), shell: isWindows })
+  }
+}
+
+async function exists (path) {
+  try {
+    await fs.promises.access(path)
+    return true
+  } catch {
+    return false
+  }
+}
+
+function run (cmd, args, opts) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(cmd, args, opts)
+    child.on('close', (code, signal) => {
+      if (!signal && (code === 0 || code === null)) {
+        resolve()
+      } else {
+        const reason = signal ? `due to signal: ${signal}` : `with code ${code}`
+        reject(new Error(`Command '${cmd} ${args.join(' ')}' failed ${reason}`))
+      }
+    })
+  })
+}
+
+module.exports = install().catch(console.error)
