3c178f79d53a9927e60c79ced3a3bb44a98502c5
dmc <david.mark.clements@gmail.com>
2024-06-26 17:28:46 +0200

    first command test ready

--- test/fixtures/terminal/index.js
+++ test/fixtures/terminal/index.js
@@ -1,22 +1,18 @@
 /* global Pear */
 import bareInspector from 'bare-inspector'
 import { Inspector } from 'pear-inspect'
-import Pipe from 'bare-pipe'
 import Helper from 'pear/test/helper'
 import cmd from 'pear/cmd'
 import API from 'pear/lib/api'
 
 const { teardown } = Pear
 
-const stdout = new Pipe(1)
-stdout.unref()
-
 const inspector = new Inspector({ inspector: bareInspector })
 const key = await inspector.enable()
 const inspectorKey = key.toString('hex')
 
-stdout.write(`{ "tag": "inspector", "data": { "key": "${inspectorKey}" }}`)
+console.log(`{ "tag": "inspector", "data": { "key": "${inspectorKey}" }}`)
 
-global.__PEAR_TEST__ = { inspector, inspectorKey, cmd, Helper, API }
+global.__PEAR_TEST__ = { inspector, inspectorKey, cmd, Helper, API, ipc: null, sub: null }
 
 teardown(async () => await inspector.disable())
