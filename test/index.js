4011409e1d798030b863aca45338adfaa4fc554a
rafapaezbas <rafaelpaezbastida@gmail.com>
2024-07-03 18:46:06 +0200

    re-activate update tests

--- test/index.js
+++ test/index.js
@@ -10,7 +10,7 @@ async function runTests () {
   await import('./01-smoke.test.js')
   await import('./02-shutdown.test.js')
   await import('./03-teardown.test.js')
-  // await import('./04-updates.test.js')
+  await import('./04-updates.test.js')
   // await import('./05-commands.test.js')
 
   test.resume()
