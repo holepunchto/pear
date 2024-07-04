8cecb6bea43bab8457b3da48afe4d852638d2ac7
unknown <rafaelpaezbastida@gmail.com>
2024-07-04 17:06:29 +0200

    re-enable tests

--- test/index.js
+++ test/index.js
@@ -7,9 +7,9 @@ async function runTests () {
 
   test.pause()
 
-  // await import('./01-smoke.test.js')
-  // await import('./02-shutdown.test.js')
-  // await import('./03-teardown.test.js')
+  await import('./01-smoke.test.js')
+  await import('./02-shutdown.test.js')
+  await import('./03-teardown.test.js')
   await import('./04-updates.test.js')
   // await import('./05-commands.test.js')
 
