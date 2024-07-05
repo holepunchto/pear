3552b827eed2fbf147ef517658e66c4abf7df26b
rafapaezbas <rafaelpaezbastida@gmail.com>
2024-07-05 17:09:20 +0200

    added worker test

---
+++ test/fixtures/worker/index.js
@@ -0,0 +1,10 @@
+const pipe = Pear.worker.pipe()
+
+let i = 0
+
+pipe.on('data', (data) => {
+  const str = data.toString()
+  if (str === 'ping') {
+    setInterval(() => pipe.write((i++).toString()), 2000)
+  }
+})
