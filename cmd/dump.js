052a07ecd67371d2466601aaa9de71609a9d7c12
dmc <david.mark.clements@gmail.com>
2024-07-15 16:24:09 +0200

    pear dump entrypoints, --list & <dir> as - support

--- cmd/dump.js
+++ cmd/dump.js
@@ -1,18 +1,23 @@
 'use strict'
-const os = require('bare-os')
+const { ERR_INVALID_INPUT } = require('../errors')
 const { isAbsolute, resolve } = require('bare-path')
 const { outputter } = require('./iface')
-const { ERR_INVALID_INPUT } = require('../errors')
 const output = outputter('stage', {
-  dumping: ({ key, dir }) => `\n🍐 Dumping ${key} into ${dir}`,
+  dumping: ({ link, dir, list }) => list ? '' : `\n🍐 Dumping ${link} into ${dir}`,
+  file: ({ key, value }) => `${key}${value ? '\n' + value : ''}`,
   complete: '\nDumping complete!\n',
   error: ({ code, stack }) => `Dumping Error (code: ${code || 'none'}) ${stack}`
 })
 
 module.exports = (ipc) => async function dump (cmd) {
-  const { checkout, json } = cmd.flags
-  const { link, dir = os.cwd() } = cmd.args
-  if (!dir) throw ERR_INVALID_INPUT('Output dir must be specified.')
-  if (!link) throw ERR_INVALID_INPUT('The pear link must be specified.')
-  await output(json, ipc.dump({ id: Bare.pid, link, dir: isAbsolute(dir) ? dir : resolve(os.cwd(), dir), checkout }))
+  const { checkout, list, json } = cmd.flags
+  const { link } = cmd.args
+  let { dir } = cmd.args
+  if (!link) throw ERR_INVALID_INPUT('<link> must be specified.')
+  if (!dir) throw ERR_INVALID_INPUT('<dir> must be specified.')
+  dir = dir === '-' ? '-' : (isAbsolute(dir) ? dir : resolve('.', dir))
+  if (list && Number.isNaN(Number(list))) {
+    throw ERR_INVALID_INPUT('--list must be a number')
+  }
+  await output(json, ipc.dump({ id: Bare.pid, link, dir, list, checkout }))
 }
--- subsystems/sidecar/ops/dump.js
+++ subsystems/sidecar/ops/dump.js
@@ -1,18 +1,20 @@
 'use strict'
-const hypercoreid = require('hypercore-id-encoding')
 const Mirror = require('mirror-drive')
 const LocalDrive = require('localdrive')
 const Bundle = require('../lib/bundle')
 const Opstream = require('../lib/opstream')
+const parseLink = require('../../../run/parse-link')
 
 module.exports = class Dump extends Opstream {
   constructor (...args) { super((...args) => this.#op(...args), ...args) }
 
-  async #op ({ link, dir, checkout }) {
+  async #op ({ link, dir, checkout, list }) {
     const { session, sidecar } = this
     await sidecar.ready()
-    const key = link ? hypercoreid.decode(link) : null
+    const parsed = parseLink(link)
+    const key = parsed.drive.key
     checkout = Number(checkout)
+    list = Number.isNaN(+list) ? -1 : Number(list)
     const corestore = sidecar._getCorestore(null, null)
     const bundle = new Bundle({ corestore, key, checkout })
 
@@ -20,9 +22,7 @@ module.exports = class Dump extends Opstream {
 
     if (sidecar.swarm) bundle.join(sidecar.swarm)
 
-    const pearkey = 'pear://' + hypercoreid.encode(bundle.drive.key)
-
-    this.push({ tag: 'dumping', data: { key: pearkey, dir } })
+    this.push({ tag: 'dumping', data: { link, dir, list } })
 
     try {
       await bundle.calibrate()
@@ -31,10 +31,34 @@ module.exports = class Dump extends Opstream {
       throw err
     }
 
-    const dst = new LocalDrive(dir)
     const src = bundle.drive
+    await src.ready()
+    if (list > -1) dir = '-'
+    if (dir === '-') {
+      const read = async (pathname, depth = 0) => {
+        depth++
+        pathname = pathname === '/' ? '' : pathname
+        const entry = pathname === '' ? null : await src.entry(pathname)
+        if (entry) {
+          const value = list > -1 ? null : await src.get(entry)
+          this.push({ tag: 'file', data: { key: pathname, value } })
+          return
+        }
+        for await (const file of src.readdir(pathname)) {
+          const subpath = pathname + '/' + file
+          const value = list > -1 ? null : await src.get(subpath)
+          this.push({ tag: 'file', data: { key: subpath, value } })
+          if (depth < list || list === 0) {
+            await read(subpath, depth)
+          }
+        }
+      }
+      await read(parsed.pathname)
+      return
+    }
 
-    const mirror = new Mirror(src, dst)
+    const dst = new LocalDrive(dir)
+    const mirror = new Mirror(src, dst, { prefix: parsed.pathname })
 
     for await (const diff of mirror) {
       if (diff.op === 'add') {
