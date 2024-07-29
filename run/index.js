6a76ffea3d0db2b7eaed92608d487bc5946f30e5
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-07-29 23:14:44 +0800

    Add support for desktop platform reload

--- run/index.js
+++ run/index.js
@@ -20,7 +20,7 @@ const {
 const parseLink = require('../lib/parse-link')
 const teardown = require('../lib/teardown')
 const { isWindows } = require('which-runtime')
-const { PLATFORM_LOCK } = require('../constants')
+const { PLATFORM_LOCK, BARE_RESTART_EXIT_CODE } = require('../constants')
 const fsext = require('fs-native-extensions')
 
 module.exports = async function run ({ ipc, args, cmdArgs, link, storage, detached, flags, appArgs, indices }) {
@@ -156,7 +156,19 @@ module.exports = async function run ({ ipc, args, cmdArgs, link, storage, detach
       cwd,
       ...{ env: { ...ENV, NODE_PRESERVE_SYMLINKS: 1 } }
     })
-    child.once('exit', (code) => {
+
+    const reloadSubscriber = ipc.messages({ type: 'pear/reload' })
+    reloadSubscriber.on('data', async () => {
+      ipc.stream.destroy()
+
+      const fd = await new Promise((resolve, reject) => fs.open(PLATFORM_LOCK, 'r+', (err, fd) => err ? reject(err) : resolve(fd)))
+      await fsext.waitForLock(fd)
+      await new Promise((resolve, reject) => fs.close(fd, (err) => err ? reject(err) : resolve(fd)))
+
+      Bare.exit(BARE_RESTART_EXIT_CODE)
+    })
+
+    child.once('exit', async (code) => {
       stream.push({ tag: 'exit', data: { code } })
       ipc.close()
     })
