d984e3bd9839e5444fbc3d8a485a1377cdf96d7b
David Mark Clements <david.mark.clements@gmail.com>
2024-08-20 17:53:52 +0200

    Revert "Bugfix: add err handler sync when streaming bundle (#262)" (#264)
    This reverts commit ac1685771ace6f1edeae1fc3cea26684ca4cf8e7.

--- subsystems/sidecar/lib/http.js
+++ subsystems/sidecar/lib/http.js
@@ -164,11 +164,7 @@ module.exports = class Http extends ReadyResource {
         }
         app.warmup({ protocol, batch })
       }
-
-      const meta = await this.entry(link.filename)
-      if (meta === null) return
-
-      const stream = bundle.streamFrom(meta)
+      const stream = await bundle.streamFrom(link.filename)
       await streamx.pipelinePromise(stream, res)
     }
   }
--- subsystems/sidecar/lib/tracer.js
+++ subsystems/sidecar/lib/tracer.js
@@ -48,7 +48,7 @@ module.exports = class Tracer extends Readable {
     return (seq) => this.capture(seq, this.constructor.META)
   }
 
-  capture (seq, core = this.constructor.DATA) {
+  async capture (seq, core = this.constructor.DATA) {
     if (Array.isArray(seq)) {
       const [blockLength, blockOffset] = seq
       for (let i = 0; i < blockLength; i++) this.capture(i + blockOffset)
