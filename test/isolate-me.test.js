88b0015e6560b585903780344bba287fa9f4da2c
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-09-17 00:03:29 +0800

    Remove logging

--- test/isolate-me.test.js
+++ test/isolate-me.test.js
@@ -4,7 +4,5 @@ const fs = require('bare-fs')
 
 const dir = path.join(fs.realpathSync(os.tmpdir()), 'tmp-pear-isolate')
 global.Pear.teardown(async () => {
-  console.log('before rm', dir)
-  await fs.promises.rm(dir, { recursive: true }).catch(() => {})
-  console.log('after rm')
+  fs.promises.rm(dir, { recursive: true }).catch(() => {})
 })
