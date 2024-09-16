6adca9f89d0f740be28367c45266ce203b3c0498
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-09-16 23:53:48 +0800

    Switch back to teardown

--- test/isolate-me.test.js
+++ test/isolate-me.test.js
@@ -3,7 +3,7 @@ const os = require('bare-os')
 const fs = require('bare-fs')
 
 const dir = path.join(fs.realpathSync(os.tmpdir()), 'tmp-pear-isolate')
-Bare.prependListener('beforeExit', async () => {
+global.Pear.teardown(async () => {
   console.log('before rm', dir)
   await fs.promises.rm(dir, { recursive: true }).catch(() => {})
   console.log('after rm')
