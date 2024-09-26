0c22e7b58e484858625859d28f77e681d8a3d2b2
dmc <david.mark.clements@gmail.com>
2024-09-26 13:05:18 +0200

    pear --as + pear --sidecar flag fix

--- boot.js
+++ boot.js
@@ -1,11 +1,23 @@
 /** @typedef {import('pear-interface')} */
 'use strict'
-const { isElectron, isElectronRenderer, isElectronWorker } = require('which-runtime')
+const { isElectron, isElectronRenderer, isElectronWorker, platform, arch, isWindows } = require('which-runtime')
+
+const BOOT_AS = 0
 const BOOT_SIDECAR = 1
 const BOOT_CLI = 2
 const BOOT_ELECTRON = 3
 const BOOT_ELECTRON_PRELOAD = 4
+let cmd = null
+
 switch (getBootType()) {
+  case BOOT_AS: {
+    const path = require('bare-path')
+    const argv = Bare.argv.slice(1)
+    const offset = argv[cmd.indices.flags.as] === cmd.flags.as ? 1 : 0
+    argv.splice(cmd.indices.flags.as - offset, 1 + offset)
+    require('bare-subprocess').spawn(path.join(cmd.flags.as, 'by-arch', platform + '-' + arch, 'bin', 'pear-runtime' + (isWindows ? '.exe' : '')), argv, { stdio: 'inherit' })
+    break
+  }
   case BOOT_SIDECAR: {
     require('./sidecar.js')
     break
@@ -30,8 +42,10 @@ function getBootType () {
       ? BOOT_ELECTRON_PRELOAD
       : BOOT_ELECTRON
   }
-  if (global.Bare.argv.includes('--sidecar')) {
-    return BOOT_SIDECAR
-  }
+  const { command, flag, rest, arg } = require('paparam')
+  cmd = command('pear', flag('--as <swap>'), flag('--sidecar'), arg('[cmd]'), rest('[...cmd-args]'))
+  cmd.parse(global.Bare.argv.slice(1))
+  if (cmd.flags.as) return BOOT_AS
+  if (cmd.flags.sidecar) return BOOT_SIDECAR
   return BOOT_CLI
 }
