e6eba2d07f030d521bca60d99e0df629f8b1724d
dmc <david.mark.clements@gmail.com>
2024-09-26 18:54:44 +0200

    still needed for now (rm after new builds)

--- scripts/pretest.js
+++ scripts/pretest.js
@@ -3,12 +3,10 @@ const fs = require('bare-fs')
 const path = require('bare-path')
 const { spawnSync } = require('bare-subprocess')
 const { isWindows } = require('which-runtime')
-
-const { protocol, pathname } = new URL(global.Pear.config.applink)
+const { pathname } = new URL(global.Pear.config.applink)
 const root = isWindows ? path.normalize(pathname.slice(1)) : pathname
 const force = global.Pear.config.args.includes('--force-install')
 
-if (protocol !== 'file:') return
 const dirs = [
   path.join(root, 'test', 'fixtures', 'harness', 'node_modules'),
   path.join(root, 'test', 'fixtures', 'encrypted', 'node_modules')
@@ -18,7 +16,5 @@ for (const dir of dirs) {
   console.log(force ? 'reinstalling node_modules in' : 'node_modules not found in', path.dirname(dir))
   console.log('Running npm install...')
   if (isWindows) spawnSync('pwsh', ['-Command', 'npm install'], { cwd: path.dirname(dir), stdio: 'inherit' })
-  else spawnSync('npm', ['install'], { stdio: 'inherit', cwd: path.dirname(dir), stdio: 'inherit' })
+  else spawnSync('npm', ['install'], { cwd: path.dirname(dir), stdio: 'inherit' })
 }
-
-
