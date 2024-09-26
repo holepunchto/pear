c93875f70fddb78c96bc92f81662756a8a0cf17e
dmc <david.mark.clements@gmail.com>
2024-09-26 18:48:50 +0200

    simplify

--- scripts/pretest.js
+++ scripts/pretest.js
@@ -1,36 +1,24 @@
 'use strict'
 const fs = require('bare-fs')
 const path = require('bare-path')
-const { spawn } = require('bare-subprocess')
+const { spawnSync } = require('bare-subprocess')
 const { isWindows } = require('which-runtime')
 
 const { protocol, pathname } = new URL(global.Pear.config.applink)
 const root = isWindows ? path.normalize(pathname.slice(1)) : pathname
 const force = global.Pear.config.args.includes('--force-install')
 
-async function install () {
-  if (protocol !== 'file:') return
-  const dirs = [
-    path.join(root, 'test', 'fixtures', 'harness', 'node_modules'),
-    path.join(root, 'test', 'fixtures', 'encrypted', 'node_modules')
-  ]
-  for (const dir of dirs) {
-    if (force === false && await exists(dir)) continue
-    console.log(force ? 'reinstalling node_modules in' : 'node_modules not found in', path.dirname(dir))
-    console.log('Running npm install...')
-    if (isWindows) spawn('pwsh', ['-Command', 'npm install'], { cwd: path.dirname(dir), stdio: 'inherit' })
-    else spawn('npm', ['install'], { stdio: 'inherit', cwd: path.dirname(dir) })
-  }
+if (protocol !== 'file:') return
+const dirs = [
+  path.join(root, 'test', 'fixtures', 'harness', 'node_modules'),
+  path.join(root, 'test', 'fixtures', 'encrypted', 'node_modules')
+]
+for (const dir of dirs) {
+  if (force === false && fs.existsSync(dir)) continue
+  console.log(force ? 'reinstalling node_modules in' : 'node_modules not found in', path.dirname(dir))
+  console.log('Running npm install...')
+  if (isWindows) spawnSync('pwsh', ['-Command', 'npm install'], { cwd: path.dirname(dir), stdio: 'inherit' })
+  else spawnSync('npm', ['install'], { stdio: 'inherit', cwd: path.dirname(dir), stdio: 'inherit' })
 }
 
-async function exists (path) {
-  try {
-    await fs.promises.access(path)
-    return true
-  } catch {
-    return false
-  }
-}
 
-module.exports = install()
-module.exports.catch(console.error)
