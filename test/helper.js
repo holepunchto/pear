754017df10cde912094bfcf485993f53f4e130b5
dmc <david.mark.clements@gmail.com>
2024-10-02 23:06:51 +0200

    w seeder client

--- test/helper.js
+++ test/helper.js
@@ -52,13 +52,13 @@ class Rig {
     comment('connecting to sidecar')
     await this.local.ready()
     comment('connected to sidecar')
-    // comment('staging platform...')
+    comment('staging platform...')
     // const staging = this.local.stage({ channel: `test-${this.id}`, name: `test-${this.id}`, dir: this.artefactDir, dryRun: false, bare: true })
     // await Helper.pick(staging, { tag: 'final' })
     // comment('platform staged')
     // comment('seeding platform')
-    // this.seeder = new Helper()
-    // await this.seeder.ready()
+    this.seeder = new Helper()
+    await this.seeder.ready()
     // const seeding = await this.seeder.seed({ channel: `test-${this.id}`, name: `test-${this.id}`, dir: this.artefactDir, key: null, cmdArgs: [] })
     // const until = await Helper.pick(seeding, [{ tag: 'key' }, { tag: 'announced' }])
     // this.key = await until.key
@@ -70,9 +70,9 @@ class Rig {
   }
 
   cleanup = async ({ comment }) => {
-    // comment('closing seeder client')
-    // await this.seeder.close()
-    // comment('seeder client closed')
+    comment('closing seeder client')
+    await this.seeder.close()
+    comment('seeder client closed')
     comment('closing local client')
     await this.local.close()
     comment('local client closed')
