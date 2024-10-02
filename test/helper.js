8d3b32d95c1c1efb61527687e27570a4f8be8ac6
dmc <david.mark.clements@gmail.com>
2024-10-02 23:24:40 +0200

    use Pear.teardown again

--- test/helper.js
+++ test/helper.js
@@ -26,12 +26,7 @@ Error.stackTraceLimit = Infinity
 
 const rigPear = path.join(tmp, 'rig-pear')
 
-Bare.on('beforeExit', async () => {
-  // console.log('# Teardown: Ensuring Rig Sidecar Shutdown')
-  // const helper = new Helper({ platform: rigPear })
-  // await helper.ready()
-  // await helper.shutdown()
-  // console.log('# Teardown: Rig Sidecar Shutdown')
+Pear.teardown(async () => {
   console.log('# Teardown: Shutting Down Local Sidecar')
   const local = new Helper()
   console.log('# Teardown: Connecting Local Sidecar')
