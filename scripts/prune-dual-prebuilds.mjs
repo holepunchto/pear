f08d3fe9510c7a0fa00c6f27d60e24b5c5ac838d
Mathias Buus <mathiasbuus@gmail.com>
2024-10-03 11:44:20 +0200

    add script to prune dual prebuilds pre staging platform

---
+++ scripts/prune-dual-prebuilds.mjs
@@ -0,0 +1,17 @@
+import Localdrive from 'localdrive'
+import { fileURLToPath } from 'url-file-url'
+import path from 'path'
+
+const drive = new Localdrive(path.join(fileURLToPath(import.meta.url), '../..'))
+
+const prune = []
+for await (const entry of drive.list('/')) {
+  if (!entry.key.endsWith('.bare')) continue
+  if (!(await drive.get(entry.key.replace(/\.bare$/, '.node')))) continue
+  prune.push(entry.key)
+}
+
+for (const key of prune) {
+  console.log('Pruning dual prebuild', key)
+  await drive.del(key)
+}
