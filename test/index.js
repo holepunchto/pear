7ed5f003a7f1d306ca37bf050c6820b7b39ca577
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-10-22 00:20:01 +0800

    Revert "Temporarily disable non-failing tests"
    This reverts commit 82887b2ad1fecc6e19798785d69dfbe37ca8e2a3.

--- test/index.js
+++ test/index.js
@@ -7,12 +7,12 @@ async function runTests () {
 
   test.pause()
 
-  // await import('./01-smoke.test.js')
-  // await import('./02-teardown.test.js')
-  // await import('./03-worker.test.js')
-  // await import('./04-encrypted.test.js')
-  // await import('./05-updates.test.js')
-  // await import('./06-shutdown.test.js')
+  await import('./01-smoke.test.js')
+  await import('./02-teardown.test.js')
+  await import('./03-worker.test.js')
+  await import('./04-encrypted.test.js')
+  await import('./05-updates.test.js')
+  await import('./06-shutdown.test.js')
   await import('./07-commands.test.js')
 
   test.resume()
