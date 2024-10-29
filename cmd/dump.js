0651467063e78273ff86be748bd2b45dc4d497f4
Marco <maidh91@gmail.com>
2024-10-29 22:53:19 +1100

    dump-dry-run

--- cmd/dump.js
+++ cmd/dump.js
@@ -6,7 +6,7 @@ const { outputter, permit, isTTY } = require('./iface')
 const output = outputter('stage', {
   dumping: ({ link, dir, list }) => list > -1 ? '' : `\n🍐 Dumping ${link} into ${dir}`,
   file: ({ key, value }) => `${key}${value ? '\n' + value : ''}`,
-  complete: '\nDumping complete!\n',
+  complete: ({ dryRun }) => { return dryRun ? '\nDumping dry run complete!\n' : '\nDumping complete!\n' },
   error: (err, info, ipc) => {
     if (err.info && err.info.encrypted && info.ask && isTTY) {
       return permit(ipc, err.info, 'dump')
@@ -19,11 +19,11 @@ const output = outputter('stage', {
 })
 
 module.exports = (ipc) => async function dump (cmd) {
-  const { checkout, json, encryptionKey, ask, force } = cmd.flags
+  const { dryRun, checkout, json, encryptionKey, ask, force } = cmd.flags
   const { link } = cmd.args
   let { dir } = cmd.args
   if (!link) throw ERR_INVALID_INPUT('<link> must be specified.')
   if (!dir) throw ERR_INVALID_INPUT('<dir> must be specified.')
   dir = dir === '-' ? '-' : (isAbsolute(dir) ? dir : resolve('.', dir))
-  await output(json, ipc.dump({ id: Bare.pid, link, dir, checkout, encryptionKey, force }), { ask }, ipc)
+  await output(json, ipc.dump({ id: Bare.pid, link, dir, dryRun, checkout, encryptionKey, force }), { ask }, ipc)
 }
