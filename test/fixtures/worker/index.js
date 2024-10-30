8bba39fc7198e927a49f0d5a9d808fb4114966a4
Marco <maidh91@gmail.com>
2024-10-30 23:43:11 +1100

    pipeWrite

--- test/fixtures/worker/index.js
+++ test/fixtures/worker/index.js
@@ -4,18 +4,23 @@ let i = 0
 let interval = null
 pipe.on('data', async (data) => {
   const str = data.toString()
+
+  function pipeWrite (value) {
+    pipe.write(JSON.stringify({ type: str, value }))
+  }
+
   if (str === 'ping') {
-    interval = setInterval(() => pipe.write(JSON.stringify({ type: str, value: i++ })), 2000)
+    interval = setInterval(() => pipeWrite(i++), 2000)
   }
   else if (str === 'versions') {
-    pipe.write(JSON.stringify({ type: str, value: await Pear.versions() }))
+    pipeWrite(await Pear.versions())
   }
   else if (str === 'dhtBootstrap') {
-    pipe.write(JSON.stringify({ type: str, value: Pear.config.dht.bootstrap }))
+    pipeWrite(Pear.config.dht.bootstrap)
   }
   else if (str === 'exit') {
     clearInterval(interval)
-    pipe.write(JSON.stringify({ type: str }))
+    pipeWrite('exit')
     Pear.exit()
   }
 })
