8c4c96ad1ff5f3aa13abfbc1dbb42aa7b05727a5
Marco <maidh91@gmail.com>
2024-11-02 01:12:50 +1100

    buffer

--- test/fixtures/require-assets/index.js
+++ test/fixtures/require-assets/index.js
@@ -3,7 +3,7 @@ const fs = require('bare-fs')
 const pipe = Pear.worker.pipe()
 pipe.on('data', () => {
   try {
-    pipe.write(fs.readFileSync(require.asset('./text-file.txt'), 'utf8'))
+    pipe.write(fs.readFileSync(require.asset('./text-file.txt')))
   } catch (err) {
     console.error(err)
     Pear.exit()
