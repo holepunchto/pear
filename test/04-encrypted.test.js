c91bb809e0b568ab4e79795aa93825a93ff1cc2c
Marco <maidh91@gmail.com>
2024-11-01 23:45:31 +1100

    encrypted

--- test/04-encrypted.test.js
+++ test/04-encrypted.test.js
@@ -10,16 +10,48 @@ test.hook('encrypted setup', rig.setup)
 
 test('stage, seed and run encrypted app', async function ({ ok, is, plan, comment, timeout, teardown }) {
   timeout(180000)
-  plan(5)
+  plan(8)
 
-  const encryptionKeyName = 'test-encryption-key'
   const helper = new Helper(rig)
-  const { pipe, key, link, encryptionKey, error } = await helper.__open({ dir: workerEncrypted, encryptionKeyName, comment, teardown })
-  is(encryptionKey.name, encryptionKeyName)
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const id = Math.floor(Math.random() * 10000)
+
+  comment('add encryption key')
+  const name = 'test-encryption-key'
+  const preimage = hypercoreid.encode(crypto.randomBytes(32))
+  const addEncryptionKey = this.encryptionKey({ action: 'add', name, value: preimage })
+  teardown(() => Helper.teardownStream(addEncryptionKey))
+  const encryptionKey = await Helper.pick(addEncryptionKey, { tag: 'added' })
+  is(encryptionKey.name, name)
+
+  comment('staging error without encryption key')
+  const stagingA = this.stage({ channel: `test-${id}`, name: `test-${id}`, dir, dryRun: false, bare: true })
+  const error = await Helper.pick(stagingA, { tag: 'error' })
   is(error.code, 'ERR_PERMISSION_REQUIRED')
 
-  const versions = await Helper.send(pipe, 'versions')
-  is(versions.app.key, key, 'app version matches staged key')
+  comment('staging')
+  const staging = helper.stage({ channel: `test-${id}`, name: `test-${id}`, dir, dryRun: false, encryptionKey: name, bare: true })
+  teardown(() => Helper.teardownStream(staging))
+  const staged = await Helper.pick(staging, { tag: 'final' })
+  ok(staged.success, 'stage succeeded')
+
+  comment('seeding')
+  const seeding = helper.seed({ channel: `test-${id}`, name: `test-${id}`, dir, key: null, encryptionKey: name, cmdArgs: [] })
+  teardown(() => Helper.teardownStream(seeding))
+  const until = await Helper.pick(seeding, [{ tag: 'key' }, { tag: 'announced' }])
+  const announced = await until.announced
+  ok(announced, 'seeding is announced')
+
+  const key = await until.key
+  ok(hypercoreid.isValid(key), 'app key is valid')
+
+  const link = `pear://${key}`
+
+  const { pipe } = await Helper.run({ link })
+  const versions = await Helper.untilResult(pipe)
+  is(JSON.parse(versions).app.key, key, 'app version matches staged key')
 
   comment('pear info encrypted app')
   const infoCmd = helper.info({ link, encryptionKey: encryptionKeyName, cmdArgs: [] })
--- test/fixtures/encrypted/index.js
+++ test/fixtures/encrypted/index.js
@@ -1,7 +1,8 @@
 const pipe = Pear.worker.pipe()
-pipe.on('data', async (data) => {
-  const command = data.toString()
-  if (command === 'versions') {
-    pipe.write(JSON.stringify(await Pear.versions()))
-  }
+pipe.on('data', () => {
+  Pear.versions().then((versions) => {
+    pipe.write(JSON.stringify(versions))
+  }).catch((err) => {
+    pipe.write(`${err}`)
+  })
 })
