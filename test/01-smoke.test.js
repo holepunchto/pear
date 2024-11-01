66bd38b8b12ec7a82c52d6161ce92a0b001b2ba1
Marco <maidh91@gmail.com>
2024-11-01 23:04:49 +1100

    build

--- test/01-smoke.test.js
+++ test/01-smoke.test.js
@@ -12,14 +12,8 @@ test('smoke', async function ({ ok, is, alike, plan, comment, teardown, timeout 
   timeout(180000)
   plan(10)
 
-  const versionsBuild = await build({ dir: versions, comment, teardown })
-  ok(hypercoreid.isValid(versionsBuild.key), 'app key is valid')
-  ok(versionsBuild.staged.success, 'stage succeeded')
-  ok(versionsBuild.announced, 'seeding is announced')
-  const dhtBootstrapBuild = await build({ dir: dhtBootstrap, comment, teardown })
-  ok(hypercoreid.isValid(dhtBootstrapBuild.key), 'app key is valid')
-  ok(dhtBootstrapBuild.staged.success, 'stage succeeded')
-  ok(dhtBootstrapBuild.announced, 'seeding is announced')
+  const versionsBuild = await build({ dir: versions, ok, comment, teardown })
+  const dhtBootstrapBuild = await build({ dir: dhtBootstrap, ok, comment, teardown })
   
   const versionsRun = await Helper.run({ link: versionsBuild.link })
   const dhtBootstrapRun = await Helper.run({ link: dhtBootstrapBuild.link })
@@ -37,9 +31,9 @@ test('smoke', async function ({ ok, is, alike, plan, comment, teardown, timeout 
 
 test('app with assets', async function ({ ok, is, plan, comment, teardown, timeout }) {
   timeout(180000)
-  plan(2)
+  plan(5)
 
-  const { link } = await build({ dir: requireAssets, comment, teardown })
+  const { link } = await build({ dir: requireAssets, ok, comment, teardown })
   const { pipe } = await Helper.run({ link })
 
   const asset = await Helper.untilResult(pipe)
@@ -51,9 +45,9 @@ test('app with assets', async function ({ ok, is, plan, comment, teardown, timeo
 
 test('app with assets in sub dep', async function ({ ok, is, plan, comment, teardown, timeout }) {
   timeout(180000)
-  plan(2)
+  plan(5)
 
-  const { link } = await build({ dir: subDepRequireAssets, comment, teardown })
+  const { link } = await build({ dir: subDepRequireAssets, ok, comment, teardown })
   const { pipe } = await Helper.run({ link })
 
   const asset = await Helper.untilResult(pipe)
@@ -63,7 +57,7 @@ test('app with assets in sub dep', async function ({ ok, is, plan, comment, tear
   ok(true, 'ended')
 })
 
-async function build ({ dir, comment = console.log, teardown = () => undefined }) {
+async function build ({ dir, ok, comment = console.log, teardown = () => undefined }) {
   const helper = new Helper()
   teardown(() => helper.close(), { order: Infinity })
   await helper.ready()
@@ -74,13 +68,18 @@ async function build ({ dir, comment = console.log, teardown = () => undefined }
   const staging = helper.stage({ channel: `test-${id}`, name: `test-${id}`, dir, dryRun: false, bare: true })
   teardown(() => Helper.teardownStream(staging))
   const staged = await Helper.pick(staging, { tag: 'final' })
+  ok(staged.success, 'stage succeeded')
 
   comment('seeding')
   const seeding = helper.seed({ channel: `test-${id}`, name: `test-${id}`, dir, key: null, cmdArgs: [] })
   teardown(() => Helper.teardownStream(seeding))
   const until = await Helper.pick(seeding, [{ tag: 'key' }, { tag: 'announced' }])
   const announced = await until.announced
+  ok(announced, 'seeding is announced')
+
   const key = await until.key
+  ok(hypercoreid.isValid(key), 'app key is valid')
+  
   const link = `pear://${key}`
 
   return { helper, key, link, staged, announced }
