e8e8da75ac6e7fb9dc247e30566d2ba8ce1c3ad4
Marco <maidh91@gmail.com>
2024-11-04 07:02:55 +1100

    enable test

--- test/index.js
+++ test/index.js
@@ -10,7 +10,7 @@ async function runTests () {
   await import('./01-smoke.test.js')
   // await import('./02-teardown.test.js')
   await import('./03-worker.test.js')
-  // await import('./04-encrypted.test.js')
+  await import('./04-encrypted.test.js')
   // await import('./05-updates.test.js')
   // await import('./06-shutdown.test.js')
 
