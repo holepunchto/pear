96097183263f80fa531768ce2858d69aaf601bd6
Marco <maidh91@gmail.com>
2024-11-05 22:26:30 +1100

    clean 1

--- test/02-teardown.test.js
+++ test/02-teardown.test.js
@@ -85,34 +85,18 @@ test('teardown on os kill', async function ({ ok, is, plan, comment, teardown, t
   const run = await Helper.run({ link })
   const { pipe } = run
 
-  const pidPromise = new Promise((resolve, reject) => {
-    const timeoutId = setTimeout(() => reject(new Error('timed out')), 5000)
-    pipe.on('data', (data) => {
-      clearTimeout(timeoutId)
-      const res = JSON.parse(data.toString())
-      if (res.id === 'pid') resolve(res.value)
-    })
-    pipe.on('close', () => {
-      clearTimeout(timeoutId)
-      reject(new Error('unexpected closed'))
-    })
-    pipe.on('end', () => {
-      clearTimeout(timeoutId)
-      reject(new Error('unexpected ended'))
-    })
-  })
+  const pidPromise = Helper.untilResult(pipe)
 
   pipe.write('start')
 
-  const pid = await pidPromise
+  const pid = +(await pidPromise)
   ok(pid > 0, 'worker pid is valid')
 
   const teardownPromise = new Promise((resolve, reject) => {
     const timeoutId = setTimeout(() => reject(new Error('timed out')), 5000)
-    pipe.on('data', (data) => {
+    pipe.once('data', (data) => {
       clearTimeout(timeoutId)
-      const res = JSON.parse(data.toString())
-      if (res.id === 'teardown') resolve(true)
+      resolve(data.toString())
     })
     pipe.on('close', () => {
       clearTimeout(timeoutId)
--- test/fixtures/teardown-os-kill/index.js
+++ test/fixtures/teardown-os-kill/index.js
@@ -1,10 +1,10 @@
 const program = global.Bare || global.process
 
 const pipe = Pear.worker.pipe()
-pipe.on('data', () => pipe.write(JSON.stringify({ id: 'pid', value: program.pid })))
+pipe.on('data', () => pipe.write(`${program.pid}`))
 
 Pear.teardown(async () => {
   await new Promise((resolve) => {
-    pipe.write(JSON.stringify({ id: 'teardown' }), resolve)
+    pipe.write('teardown', resolve)
   })
 })
