160eb5d83c67a42eef0f4c861fb6b18cc60aa3c0
Marco <maidh91@gmail.com>
2024-11-05 23:13:58 +1100

    encrypted

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -15,7 +15,7 @@ const force = Bare.argv.includes('--force-install')
 
 const dirs = [
   path.join(root, 'test', 'fixtures', 'harness', 'node_modules'),
-  path.join(root, 'test', 'fixtures', 'require-assets', 'node_modules'),
+  path.join(root, 'test', 'fixtures', 'encrypted', 'node_modules'),
   path.join(root, 'test', 'fixtures', 'app-with-assets', 'node_modules')
 ]
 for (const dir of dirs) {
