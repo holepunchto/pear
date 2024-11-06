9a89caf4877cd3dc989a804072b10850c379b18e
Jan Keith Darunday <jkcdarunday@gmail.com>
2024-11-07 05:57:14 +0800

    Use simpler workers

--- test/03-worker.test.js
+++ test/03-worker.test.js
@@ -4,6 +4,7 @@ const test = require('brittle')
 const path = require('bare-path')
 const Helper = require('./helper')
 const worker = path.join(Helper.localDir, 'test', 'fixtures', 'worker')
+const helloWorld = path.join(Helper.localDir, 'test', 'fixtures', 'hello-world')
 const workerRunner = path.join(Helper.localDir, 'test', 'fixtures', 'worker-runner')
 
 test('worker pipe', async function ({ is, plan, teardown }) {
@@ -48,9 +49,9 @@ test('worker should run directly in a terminal app', async function ({ is, plan,
 
   comment('Running worker using worker-runner...')
   const { pipe } = await Helper.run({ link })
-  const response = await Helper.untilResult(pipe, worker, 15_000)
+  const response = await Helper.untilResult(pipe, helloWorld)
 
-  is(response, '0123', 'worker should send expected response')
+  is(response, 'hello world', 'worker should send expected response')
 
   await Helper.untilClose(pipe)
 })
@@ -71,7 +72,7 @@ test('worker should run as a link in a terminal app', async function ({ is, plan
   await until1.final
 
   comment('Staging worker...')
-  const staging2 = helper.stage({ channel: `test-worker-${testId}`, name: `test-worker-${testId}`, key: null, dir: worker, cmdArgs: [], dryRun: false, bare: true, ignore: [] })
+  const staging2 = helper.stage({ channel: `test-worker-${testId}`, name: `test-worker-${testId}`, key: null, dir: helloWorld, cmdArgs: [], dryRun: false, bare: true, ignore: [] })
   teardown(() => Helper.teardownStream(staging2))
   const until2 = await Helper.pick(staging2, [{ tag: 'staging' }, { tag: 'final' }])
   const { link: workerLink } = await until2.staging
@@ -79,9 +80,9 @@ test('worker should run as a link in a terminal app', async function ({ is, plan
 
   comment('Running worker using worker-runner...')
   const { pipe } = await Helper.run({ link: runnerLink })
-  const response = await Helper.untilResult(pipe, workerLink, 15_000)
+  const response = await Helper.untilResult(pipe, workerLink)
 
-  is(response, '0123', 'worker should send expected response')
+  is(response, 'hello world', 'worker should send expected response')
 
   await Helper.untilClose(pipe)
 })
--- test/fixtures/worker-runner/index.js
+++ test/fixtures/worker-runner/index.js
@@ -3,14 +3,10 @@ const pipe = Pear.worker.pipe()
 pipe.once('data', (workerPath) => {
   const workerPipe = Pear.worker.run(workerPath)
 
-  const messages = []
   workerPipe.on('data', (data) => {
-    messages.push(data.toString())
-    if (messages.length === 4) {
-      pipe.write(messages.join(''))
-      workerPipe.write('exit')
-    }
+    pipe.write(data)
+    workerPipe.end()
   })
 
-  workerPipe.write('ping')
+  workerPipe.write('start')
 })
