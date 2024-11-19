e4d87edd0fe9b23f6f9c283f49ab123d5c9ae547
dmc <david.mark.clements@gmail.com>
2024-11-19 20:39:53 +0100

    errors, state, parse-link from pear-api

--- examples/assets/index.js
+++
@@ -1,4 +0,0 @@
-const fs = require('bare-fs')
-const filename = require.asset('./index.js')
-console.log('filename:', filename)
-fs.promises.readFile(filename, 'utf-8').then((file) => console.log(file))
