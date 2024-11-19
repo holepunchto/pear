b646befbb6dda4159fb75eeff1e694b7cd7aa888
dmc <david.mark.clements@gmail.com>
2024-11-19 20:52:57 +0100

    mod ns fixes

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 const IPC = require('pear-ipc')
 const crasher = require('pear-api/crasher')
 const tryboot = require('pear-api/tryboot')
-const { PLATFORM_LOCK, SWAP, SOCKET_PATH, CONNECT_TIMEOUT } = require('pear-api/constants.js')
+const { PLATFORM_LOCK, SWAP, SOCKET_PATH, CONNECT_TIMEOUT } = require('pear-api/constants')
 const cmd = require('./cmd')
 crasher('cli', SWAP)
 
