ca500fbf588146dd9f6f71510b8ec30d657461a6
Marco <maidh91@gmail.com>
2024-11-25 21:52:26 +1100

    ok

--- test/fixtures/worker-parent-desktop/app.js
+++ test/fixtures/worker-parent-desktop/app.js
@@ -2,8 +2,6 @@ const pipeIn = Pear.worker.pipe()
 const link = Pear.config.args[Pear.config.args.length - 1]
 const pipe = Pear.worker.run(link)
 pipe.resume()
-await new Promise((resolve) => setTimeout(resolve, 1000))
-pipe.end()
 await untilExit(pipe)
 Pear.Window.self.close()
 
