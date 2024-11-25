791cadf366b24725fe129c27b875e2a343061b7f
Marco <maidh91@gmail.com>
2024-11-25 21:18:59 +1100

    timeout

--- test/fixtures/worker-destroy-from-parent/index.js
+++ test/fixtures/worker-destroy-from-parent/index.js
@@ -1,4 +1,5 @@
 const link = Bare.argv[Bare.argv.length - 1]
 const pipe = Pear.worker.run(link)
 pipe.resume()
+await new Promise((resolve) => setTimeout(resolve, 1000))
 pipe.destroy()
--- test/fixtures/worker-parent/index.js
+++ test/fixtures/worker-parent/index.js
@@ -1,4 +1,3 @@
-
 const link = Bare.argv[Bare.argv.length - 1]
 const pipe = Pear.worker.run(link)
 pipe.resume()
