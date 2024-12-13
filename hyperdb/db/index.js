c334c567feb55dd20525e028aae5ba33d376d283
rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>
2024-12-13 17:44:45 +0100

    regenerated hyperdb schema (#498)

--- hyperdb/db/index.js
+++ hyperdb/db/index.js
@@ -6,52 +6,52 @@ const { IndexEncoder, c } = require('hyperdb/runtime')
 const { version, resolveStruct } = require('./messages.js')
 
 // '@pear/dht' collection key
-const collection0_key = new IndexEncoder([
-], { prefix: 0 })
+const collection16_key = new IndexEncoder([
+], { prefix: 16 })
 
-function collection0_indexify (record) {
+function collection16_indexify (record) {
   return []
 }
 
 // '@pear/dht' reconstruction function
-function collection0_reconstruct (version, keyBuf, valueBuf) {
+function collection16_reconstruct (version, keyBuf, valueBuf) {
   const value = c.decode(resolveStruct('@pear/dht/value', version), valueBuf)
   return value
 }
 // '@pear/dht' key reconstruction function
-function collection0_reconstruct_key (keyBuf) {
+function collection16_reconstruct_key (keyBuf) {
   return {}
 }
 
 // '@pear/dht'
-const collection0 = {
+const collection16 = {
   name: '@pear/dht',
-  id: 0,
+  id: 16,
   encodeKey (record) {
     const key = []
-    return collection0_key.encode(key)
+    return collection16_key.encode(key)
   },
   encodeKeyRange ({ gt, lt, gte, lte } = {}) {
-    return collection0_key.encodeRange({
-      gt: gt ? collection0_indexify(gt) : null,
-      lt: lt ? collection0_indexify(lt) : null,
-      gte: gte ? collection0_indexify(gte) : null,
-      lte: lte ? collection0_indexify(lte) : null
+    return collection16_key.encodeRange({
+      gt: gt ? collection16_indexify(gt) : null,
+      lt: lt ? collection16_indexify(lt) : null,
+      gte: gte ? collection16_indexify(gte) : null,
+      lte: lte ? collection16_indexify(lte) : null
     })
   },
   encodeValue (version, record) {
     return c.encode(resolveStruct('@pear/dht/value', version), record)
   },
   trigger: null,
-  reconstruct: collection0_reconstruct,
-  reconstructKey: collection0_reconstruct_key,
+  reconstruct: collection16_reconstruct,
+  reconstructKey: collection16_reconstruct_key,
   indexes: []
 }
 
 module.exports = {
   version,
   collections: [
-    collection0
+    collection16
   ],
   indexes: [
   ],
@@ -61,7 +61,7 @@ module.exports = {
 
 function resolveCollection (name) {
   switch (name) {
-    case '@pear/dht': return collection0
+    case '@pear/dht': return collection16
     default: return null
   }
 }
