0259231c8586eb258d25ae93dc6275d111282bed
dmc <david.mark.clements@gmail.com>
2024-12-16 20:59:55 +0100

    rm http server, dep fixes, pear-api alignment

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -37,6 +37,7 @@ const {
   WAKEUP, SALT, KNOWN_NODES_LIMIT
 } = require('pear-api/constants')
 const { ERR_INTERNAL_ERROR, ERR_PERMISSION_REQUIRED } = require('pear-api/errors')
+
 const State = require('./state')
 const ops = {
   GC: require('./ops/gc'),
@@ -302,6 +303,7 @@ class Sidecar extends ReadyResource {
   }
 
   async _open () {
+    await this.applings.set('runtime', DESKTOP_RUNTIME)
     await this.#ensureSwarm()
     LOG.info('sidecar', '- Sidecar Booted')
   }
@@ -734,6 +736,7 @@ class Sidecar extends ReadyResource {
 
     link = pearLink.normalize(link.startsWith('pear://') ? `pear://${hypercoreid.encode(key)}${parsedLink.pathname}${parsedLink.hash}` : link.startsWith('file://') ? link : pathToFileURL(link).href)
     const persistedBundle = await this.model.getBundle(link) || await this.model.addBundle(link, State.storageFromLink(parsedLink))
+
     const encryptionKey = persistedBundle.encryptionKey
     const appStorage = persistedBundle.appStorage
 
