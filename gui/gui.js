7babaea6d96c13c90699f44ab20c29a4526ce782
Eduard Castellano <educastellano08@gmail.com>
2025-01-09 17:03:28 +0100

    Add hideOnClose support (#529)
    * Add hideOnClose support
    
    * Typo

--- gui/gui.js
+++ gui/gui.js
@@ -123,6 +123,7 @@ class Menu {
           const view = win.getBrowserViews()[0]
           const session = view?.webContents.session || win.webContents.session
           for (const ctrl of PearGUI.ofSession(session)) {
+            ctrl.quitting = true
             await ctrl.close()
           }
         }
@@ -529,7 +530,8 @@ class App {
         autoHideMenuBar: unfilteredGuiOptions.autoHideMenuBar,
         hasShadow: unfilteredGuiOptions.hasShadow,
         opacity: unfilteredGuiOptions.opacity,
-        transparent: unfilteredGuiOptions.transparent
+        transparent: unfilteredGuiOptions.transparent,
+        hideOnClose: unfilteredGuiOptions.hideOnClose ?? unfilteredGuiOptions[process.platform]?.hideOnClose ?? false
       }
 
       const decalSession = electron.session.fromPartition('persist:pear')
@@ -768,6 +770,7 @@ class GuiCtrl {
   unloaded = null
   #unloading = null
   appkin = null
+  quitting = false
   static height = 540
   static width = 720
   static [kCtrl] = null
@@ -923,10 +926,12 @@ class GuiCtrl {
 
     const closeListener = (e) => {
       e.preventDefault()
+      if (this.options.hideOnClose) return
       if (this.unload) {
         this.unload({ type: 'close' })
       }
     }
+
     if (this.win) this.win.once('close', closeListener)
     this.unloader = new Promise((resolve) => { this.unloaded = resolve })
     const action = await until
@@ -957,6 +962,12 @@ class Window extends GuiCtrl {
   closing = false
   #viewInitialized = null
   #viewLoaded = null
+
+  #onactivate = () => {
+    if (this.closing || this.closed) return
+    this.show()
+  }
+
   async open (opts = {}) {
     if (this.win) return !this.closed
     this.opening = true
@@ -1008,8 +1019,13 @@ class Window extends GuiCtrl {
       if (this?.view?.webContents) this.view.webContents.focus()
     })
 
-    this.win.on('close', () => {
-      this.closing = true
+    this.win.on('close', (evt) => {
+      if (this.options.hideOnClose && this.quitting === false) {
+        evt.preventDefault()
+        this.win.hide()
+      } else {
+        this.closing = true
+      }
     })
 
     this.win.on('closed', () => {
@@ -1024,6 +1040,8 @@ class Window extends GuiCtrl {
         this.view = null
       }
     })
+
+    electron.app.on('activate', this.#onactivate)
 
     if (this.id === null) this.id = idify(this)
     this.constructor[kMap].set(this.id, this)
@@ -1249,6 +1267,7 @@ class Window extends GuiCtrl {
 
   async close () {
     this.closing = true
+    electron.app.off('activate', this.#onactivate)
     const closed = await super.close()
     this.closing = false
     return closed
