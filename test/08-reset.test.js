79f2f6024af8d71053d7ad14434c475a58cf300e
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-01-09 09:57:38 +0100

    added reset test

---
+++ test/08-reset.test.js
@@ -0,0 +1,41 @@
+'use strict'
+const test = require('brittle')
+const path = require('bare-path')
+const Helper = require('./helper')
+const resetDir = path.join(Helper.localDir, 'test', 'fixtures', 'reset')
+
+test('reset', async function ({ ok, is, plan, comment, teardown, timeout }) {
+  timeout(180000)
+  plan(3)
+
+  const dir = resetDir
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const id = Math.floor(Math.random() * 10000)
+
+  comment('staging')
+  const staging = helper.stage({ channel: `test-${id}`, name: `test-${id}`, dir, dryRun: false, bare: true })
+  teardown(() => Helper.teardownStream(staging))
+  const staged = await Helper.pick(staging, [{ tag: 'addendum' }, { tag: 'final' }])
+  const { key } = await staged.addendum
+  await staged.final
+
+  const link = `pear://${key}`
+
+  const run = await Helper.run({ link })
+  const before = await Helper.untilResult(run.pipe)
+  await Helper.untilClose(run.pipe)
+
+  await helper.reset({ link })
+
+  const runB = await Helper.run({ link })
+  const after = await Helper.untilResult(runB.pipe)
+  await Helper.untilClose(runB.pipe)
+
+  ok(before)
+  ok(after)
+  ok(before !== after)
+})
