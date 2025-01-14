bd009e0c1d61b878f94c7cb21c5cabdec3384dc3
Andrei Regiani <andrei.cpp@gmail.com>
2025-01-14 19:41:18 +0300

    `reset` merge conflict fix

--- cmd/iface.js
+++ cmd/iface.js
@@ -145,9 +145,9 @@ and then becomes the sidecar.`,
 
   touch: 'Creates a Pear Link using channel name if provided or else a randomly generated channel name.',
 
-  data: 'Data stored in this device.'
+  reset: 'Clears application storage for given application link”',
 
-  reset: 'Clears application storage for given application link”'
+  data: 'Local data stored in this device.'
 }
 
 const usage = { header, version, banner, descriptions, footer }
