72f9d8664cee283e84129738b1c7834a8eb02f3c
dmc <david.mark.clements@gmail.com>
2025-01-16 14:36:50 +0100

    addon loading fix

--- run.js
+++ run.js
@@ -6,11 +6,11 @@ const path = require('bare-path')
 const fsp = require('bare-fs/promises')
 const ENV = require('bare-env')
 const { spawn } = require('bare-subprocess')
-const { Readable } = require('streamx')
-const { isMac, isWindows, isLinux } = require('which-runtime')
-const constants = require('./constants')
-const State = require('./state')
-const API = require('./lib/api')
+const { isMac, isWindows } = require('which-runtime')
+const API = require('pear-api')
+const constants = require('pear-api/constants')
+const teardown = require('pear-api/teardown')
+const parseLink = require('pear-api/parse-link')
 const {
   ERR_INVALID_APPLING,
   ERR_PERMISSION_REQUIRED,
@@ -20,7 +20,7 @@ const parseLink = require('./lib/parse-link')
 const pearLink = require('pear-link')
 const teardown = require('./lib/teardown')
 
-module.exports = async function run ({ ipc, args, cmdArgs, link, storage, detached, flags, appArgs, indices }) {
+module.exports = async function run ({ ipc, args, cmdArgs, link, storage, detached, flags, appArgs }) {
   const { drive, pathname } = parseLink(link)
   const entry = isWindows ? path.normalize(pathname.slice(1)) : pathname
   const { key } = drive
@@ -52,13 +52,9 @@ module.exports = async function run ({ ipc, args, cmdArgs, link, storage, detach
     }
   }
 
-  const stream = new Readable({ objectMode: true })
   if (detached) {
     const { wokeup, appling } = await ipc.detached({ key, link, storage, appdev: key === null ? dir : null })
-    if (wokeup) {
-      ipc.close().catch(console.error)
-      return stream
-    }
+    if (wokeup) return ipc.close().catch(console.error)
 
     args = args.filter((arg) => arg !== '--detached')
     const opts = { detached: true, stdio: 'ignore', cwd }
@@ -66,8 +62,7 @@ module.exports = async function run ({ ipc, args, cmdArgs, link, storage, detach
     if (!appling) {
       args.unshift('run', '--detach')
       spawn(constants.RUNTIME, args, opts).unref()
-      ipc.close().catch(console.error)
-      return stream
+      return ipc.close().catch(console.error)
     }
 
     const applingApp = isMac ? appling.split('.app')[0] + '.app' : appling
@@ -79,102 +74,51 @@ module.exports = async function run ({ ipc, args, cmdArgs, link, storage, detach
       throw ERR_INVALID_APPLING('Appling does not exist')
     }
 
-    if (link.startsWith('pear://runtime')) {
-      if ((isLinux || isWindows) && !flags.sandbox) args.splice(indices.args.link, 0, '--no-sandbox')
-      args = [constants.BOOT, '--appling', appling, '--run', ...args]
-      spawn(constants.DESKTOP_RUNTIME, args, opts).unref()
-    } else {
-      if (isMac) spawn('open', [applingApp, '--args', ...args], opts).unref()
-      else spawn(applingApp, args, opts).unref()
-    }
-    ipc.close().catch(console.error)
-    return stream
+    if (isMac) spawn('open', [applingApp, '--args', ...args], opts).unref()
+    else spawn(applingApp, args, opts).unref()
+
+    return ipc.close().catch(console.error)
   }
 
-  const { startId, host, id, type = 'desktop', bundle, bail, app } = await ipc.start({ flags, env: ENV, dir, link, cwd, args: appArgs, cmdArgs, pid: os.pid() })
+  const { startId, id, bundle, bail } = await ipc.start({ flags, env: ENV, dir, link, cwd, args: appArgs, cmdArgs })
 
   if (bail?.code === 'ERR_PERMISSION_REQUIRED' && !flags.detach) {
     throw new ERR_PERMISSION_REQUIRED('Permission required to run key', bail.info)
   }
 
-  if (type === 'terminal') {
-    const state = new State({ flags, link, dir, cmdArgs, cwd })
+  const state = new State({ flags, link, dir, cmdArgs, cwd })
 
-    state.update({ host, id, type })
+  state.update({ id, startId })
 
-    if (state.error) {
-      console.error(state.error)
-      global.process?.exit(1) || global.Bare.exit(1)
+  if (state.error) throw state.error
+
+  await ipc.ready()
+  const config = await ipc.config()
+  state.update({ config })
+
+  global.Pear = new API(ipc, state, { teardown })
+
+  const protocol = new Module.Protocol({
+    exists (url) {
+      if (url.href.endsWith('.bare') || url.href.endsWith('.node')) return true
+      return Object.hasOwn(bundle.sources, url.href) || Object.hasOwn(bundle.assets, url.href)
+    },
+    read (url) {
+      return bundle.sources[url.href]
     }
+  })
 
-    await ipc.ready()
-    const ipcConfig = await ipc.config()
-    state.update({ config: ipcConfig })
+  // clear global handlers
+  Bare.removeAllListeners('uncaughtException')
+  Bare.removeAllListeners('unhandledRejection')
 
-    global.Pear = new API(ipc, state)
-
-    const protocol = new Module.Protocol({
-      exists (url) {
-        if (url.href.endsWith('.bare') || url.href.endsWith('.node')) return true
-        return Object.hasOwn(bundle.sources, url.href) || Object.hasOwn(bundle.assets, url.href)
-      },
-      read (url) {
-        return bundle.sources[url.href]
-      }
+  // preserves uncaught exception (otherwise it becomes uncaught rejection)
+  setImmediate(() => {
+    Module.load(new URL(bundle.entrypoint), {
+      protocol,
+      resolutions: bundle.resolutions
     })
-
-    // clear global handlers
-    Bare.removeAllListeners('uncaughtException')
-    Bare.removeAllListeners('unhandledRejection')
-
-    // preserves uncaught exception (otherwise it becomes uncaught rejection)
-    setImmediate(() => {
-      Module.load(new URL(bundle.entrypoint), {
-        protocol,
-        resolutions: bundle.resolutions
-      })
-    })
-
-    return stream
-  }
-
-  args.unshift('--start-id=' + startId)
-  const detach = args.includes('--detach')
-  if (type === 'desktop') {
-    if (isPath) args[indices.args.link] = 'file://' + pearLink.normalize(base.origin) + (base.entrypoint || '/')
-    args[indices.args.link] = args[indices.args.link].replace('://', '_||') // for Windows
-    if ((isLinux || isWindows) && !flags.sandbox) args.splice(indices.args.link, 0, '--no-sandbox')
-    if (app?.name) args.splice(indices.args.link, 0, '--app-name', app.name)
-    args = [constants.BOOT, ...args]
-    const stdio = detach ? 'ignore' : ['ignore', 'pipe', 'pipe']
-    const child = spawn(constants.DESKTOP_RUNTIME, args, {
-      stdio,
-      cwd,
-      ...{ env: { ...ENV, NODE_PRESERVE_SYMLINKS: 1 } }
-    })
-    child.once('exit', (code) => {
-      stream.push({ tag: 'exit', data: { code } })
-      ipc.close()
-    })
-    if (!detach) {
-      child.stdout.on('data', (data) => { stream.push({ tag: 'stdout', data }) })
-      child.stderr.on('data', (data) => {
-        const str = data.toString()
-        const ignore = str.indexOf('DevTools listening on ws://') > -1 ||
-              str.indexOf('NSApplicationDelegate.applicationSupportsSecureRestorableState') > -1 ||
-              str.indexOf('", source: devtools://devtools/') > -1 ||
-              str.indexOf('sysctlbyname for kern.hv_vmm_present failed with status -1') > -1 ||
-              str.indexOf('dev.i915.perf_stream_paranoid=0') > -1 ||
-              str.indexOf('libva error: vaGetDriverNameByIndex() failed') > -1 ||
-              str.indexOf('GetVSyncParametersIfAvailable() failed') > -1 ||
-              (str.indexOf(':ERROR:') > -1 && /:ERROR:.+cache/.test(str))
-        if (ignore) return
-        stream.push({ tag: 'stderr', data })
-      })
-    }
-  }
-
-  if (global.Pear) global.Pear.teardown(() => ipc.close())
+  })
 
   return stream
 }
