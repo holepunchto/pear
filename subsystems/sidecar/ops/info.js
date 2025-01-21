67fa239f29dce2ac1fc8247a09de98fd5217cf92
Marco <maidh91@gmail.com>
2025-01-21 23:12:02 +1100

    improve-pear-info

--- subsystems/sidecar/ops/info.js
+++ subsystems/sidecar/ops/info.js
@@ -45,7 +45,7 @@ module.exports = class Info extends Opstream {
       await bundle.ready()
     }
 
-    console.log('🚀 ~ Info ~ #op ~ drive:', key, key.toString('hex'), drive.key, drive)
+    console.log('🚀 ~ Info ~ #op ~ drive:', key, key.toString('hex'), drive.key, drive.key.toString('hex'))
     const z32 = hypercoreid.encode(key)
     if (isEnabled(showKey)) {
       const onlyShowKey = enabledFlags.size === 1
