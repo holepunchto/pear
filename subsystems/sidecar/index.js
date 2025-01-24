8f522f9e8bd2646c7526a1bb30c6ecf97baed943
dmc <david.mark.clements@gmail.com>
2025-01-24 15:02:32 +0100

    run file: link fix

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -734,7 +734,6 @@ class Sidecar extends ReadyResource {
 
     link = pearLink.normalize(link.startsWith('pear://') ? `pear://${hypercoreid.encode(key)}${parsedLink.pathname}${parsedLink.hash}` : link.startsWith('file://') ? link : pathToFileURL(link).href)
     const persistedBundle = await this.model.getBundle(link) || await this.model.addBundle(link, State.storageFromLink(parsedLink))
-
     const encryptionKey = persistedBundle.encryptionKey
     const appStorage = persistedBundle.appStorage
 
