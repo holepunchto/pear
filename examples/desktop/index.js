f987ac5a1586c64e61fb1c761d315ab964f6dc4e
dmc <david.mark.clements@gmail.com>
2025-01-28 22:20:04 +0100

    working

--- examples/desktop/index.js
+++ examples/desktop/index.js
@@ -9,3 +9,5 @@ await bridge.ready()
 
 const pipe = runtime.start({ bridge })
 Pear.teardown(() => pipe.end())
+
+// TODO: need resolved user-specified preload on state + need pear-electron renderer ipc -> electron-main ipc get method for that path
