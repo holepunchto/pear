d3c491b20f60591747f8eb12a1d5d82626833666
Marco <maidh91@gmail.com>
2025-01-30 22:32:30 +1100

    Fix Pear.tray.scaleFactor

--- gui/gui.js
+++ gui/gui.js
@@ -1127,13 +1127,15 @@ class Window extends GuiCtrl {
 
     if (this.closing) return false
 
+    const scaleFactor = electron.screen.getPrimaryDisplay().scaleFactor
+
     this.view = new BrowserView({
       ...(options.view || options),
       backgroundColor: options.backgroundColor || DEF_BG,
       webPreferences: {
         preload: require.main.filename,
         session,
-        additionalArguments: [JSON.stringify({ ...this.state.config, parentWcId: this.win.webContents.id, decalled: true })],
+        additionalArguments: [JSON.stringify({ ...this.state.config, parentWcId: this.win.webContents.id, decalled: true, scaleFactor })],
         autoHideMenuBar: true,
         experimentalFeatures: true,
         nodeIntegration: true,
@@ -1540,7 +1542,6 @@ class PearGUI extends ReadyResource {
     electron.ipcMain.handle('versions', (evt, ...args) => this.versions(...args))
     electron.ipcMain.handle('restart', (evt, ...args) => this.restart(...args))
     electron.ipcMain.handle('badge', (evt, ...args) => this.badge(...args))
-    electron.ipcMain.handle('scaleFactor', (evt, ...args) => this.scaleFactor(...args))
 
     electron.ipcMain.on('workerRun', (evt, link, args) => {
       const pipe = this.worker.run(link, args)
@@ -1801,10 +1802,6 @@ class PearGUI extends ReadyResource {
       this.get(id).win.setIcon(linuxBadgeIcon(count))
       return true
     }
-  }
-
-  scaleFactor () {
-    return electron.screen.getPrimaryDisplay().scaleFactor
   }
 }
 
