dffd0a200d76ca02fc32f384869a2ff1f29e58e3
dmc <david.mark.clements@gmail.com>
2025-01-30 23:53:34 +0100

    rm get bundle option

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -419,7 +419,6 @@ class Sidecar extends ReadyResource {
 
   get (params, client) {
     if (!client.userData) return
-    if (params.bundle === true) return client.userData.bundle.bundle(params.key)
     return client.userData.bundle.get(params.key)
   }
 
