12a19355edd278895d88f17e3bd412d768579e36
dmc <david.mark.clements@gmail.com>
2025-02-12 23:18:45 +0100

    pear dump --no-prune, --only implies --no-prune

--- cmd/dump.js
+++ cmd/dump.js
@@ -27,11 +27,11 @@ const output = outputter('dump', {
 })
 
 module.exports = (ipc) => async function dump (cmd) {
-  const { dryRun, checkout, json, ask, only, force } = cmd.flags
+  const { dryRun, checkout, json, only, force, ask, prune } = cmd.flags
   const { link } = cmd.args
   let { dir } = cmd.args
   if (!link) throw ERR_INVALID_INPUT('<link> must be specified.')
   if (!dir) throw ERR_INVALID_INPUT('<dir> must be specified.')
   dir = dir === '-' ? '-' : (isAbsolute(dir) ? dir : resolve('.', dir))
-  await output(json, ipc.dump({ id: Bare.pid, link, dir, dryRun, checkout, only, force }), { ask }, ipc)
+  await output(json, ipc.dump({ id: Bare.pid, link, dir, dryRun, checkout, only, force, prune }), { ask }, ipc)
 }
--- subsystems/sidecar/ops/dump.js
+++ subsystems/sidecar/ops/dump.js
@@ -12,7 +12,7 @@ const DriveMonitor = require('../lib/drive-monitor')
 module.exports = class Dump extends Opstream {
   constructor (...args) { super((...args) => this.#op(...args), ...args) }
 
-  async #op ({ link, dir, dryRun, checkout, only, force }) {
+  async #op ({ link, dir, dryRun, checkout, only, force, prune = !only }) {
     const { session, sidecar } = this
     await sidecar.ready()
     if (dir !== '-') {
@@ -106,11 +106,11 @@ module.exports = class Dump extends Opstream {
     const dst = new LocalDrive(dir)
     let select = null
     if (only) {
-      only = only.split(',').map((s) => s.trim())
+      only = Array.isArray(only) ? only : only.split(',').map((s) => s.trim())
       select = (key) => only.some((path) => key.startsWith(path[0] === '/' ? path : '/' + path))
     }
     const extraOpts = entry === null ? { prefix, filter: select } : { filter: select || ((key) => key === prefix) }
-    const mirror = src.mirror(dst, { dryRun, ...extraOpts })
+    const mirror = src.mirror(dst, { dryRun, prune, ...extraOpts })
     for await (const diff of mirror) {
       if (diff.op === 'add') {
         this.push({ tag: 'byte-diff', data: { type: 1, sizes: [diff.bytesAdded], message: diff.key } })
