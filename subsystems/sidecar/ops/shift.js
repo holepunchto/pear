046b11e3aa57d7fe55edce5a4ff238ef3eeb1c08
dmc <david.mark.clements@gmail.com>
2025-03-12 21:24:14 +0100

    shift fix

--- subsystems/sidecar/ops/shift.js
+++ subsystems/sidecar/ops/shift.js
@@ -1,11 +1,14 @@
 'use strict'
 const fs = require('bare-fs')
 const path = require('bare-path')
-const { randomBytes, discoveryKey } = require('hypercore-crypto')
+const { randomBytes } = require('hypercore-crypto')
+const pearLink = require('pear-link')
 const parseLink = require('pear-api/parse-link')
-const { PLATFORM_DIR, GC } = require('pear-api/constants')
+const { PLATFORM_DIR } = require('pear-api/constants')
 const { ERR_INVALID_INPUT } = require('pear-api/errors')
 const Opstream = require('../lib/opstream')
+
+const exists = (path) => fs.promises.stat(path).then(() => true, () => false)
 
 module.exports = class Shift extends Opstream {
   constructor (...args) {
@@ -13,39 +16,29 @@ module.exports = class Shift extends Opstream {
   }
 
   async #op ({ src, dst, force }) {
-    let from = null
-    let to = null
     this.push({ tag: 'moving', data: { src, dst } })
+
+    src = pearLink.normalize(src)
+    dst = pearLink.normalize(dst)
 
     if (!src) throw ERR_INVALID_INPUT('src must be specified')
     if (!dst) throw ERR_INVALID_INPUT('dst must be specified')
-    const srcKey = parseLink(src).drive?.key
-    const dstKey = parseLink(dst).drive?.key
-    if (!srcKey) throw ERR_INVALID_INPUT('Invalid source app key')
-    if (!dstKey) throw ERR_INVALID_INPUT('Invalid destination app key')
-    const byDkey = path.join(PLATFORM_DIR, 'app-storage', 'by-dkey')
-    from = path.join(byDkey, discoveryKey(srcKey).toString('hex'))
-    to = path.join(byDkey, discoveryKey(dstKey).toString('hex'))
-    const exists = (path) => fs.promises.stat(path).then(() => true, () => false)
-    let gc = null
-    try {
-      if (await exists(from) === false) {
-        throw ERR_INVALID_INPUT('No app storage for found for ' + src)
-      }
-      if (await exists(to)) {
-        if (force) {
-          gc = path.join(GC, randomBytes(8).toString('hex'))
-          await fs.promises.rename(to, gc)
-        } else {
-          throw ERR_INVALID_INPUT('App storage for ' + dst + ' already exists. Use --force to overwrite')
-        }
-      }
 
-      await fs.promises.rename(from, to)
-    } finally {
-      if (gc) await fs.promises.rm(gc, { recursive: true })
-    }
+    const parsedSrc = parseLink(src)
+    const parsedDst = parseLink(dst)
 
-    this.push({ tag: 'complete', data: { from, to, src, dst } })
+    if (!parsedSrc?.drive?.key) throw ERR_INVALID_INPUT('Invalid source app key')
+    if (!parsedDst?.drive?.key) throw ERR_INVALID_INPUT('Invalid destination app key')
+
+    const srcAppStorage = await this.sidecar.model.getAppStorage(src)
+    const dstAppStorage = await this.sidecar.model.getAppStorage(dst)
+
+    if (!srcAppStorage || !(await exists(srcAppStorage))) throw ERR_INVALID_INPUT(`No app storage found for ${src}`)
+    if (dstAppStorage && !force) throw ERR_INVALID_INPUT(`App storage for ${dst} already exists. Use --force to overwrite`)
+
+    const newSrcAppStorage = path.join(path.join(PLATFORM_DIR, 'app-storage'), 'by-random', randomBytes(16).toString('hex'))
+    await this.sidecar.model.shiftAppStorage(src, dst, newSrcAppStorage)
+
+    this.push({ tag: 'complete', data: { oldDst: dstAppStorage, newDst: srcAppStorage, newSrc: newSrcAppStorage, src, dst } })
   }
 }
