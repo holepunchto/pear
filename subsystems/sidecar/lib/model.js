84f55c39af7bd231270a477d52840632738c2a82
Andrei Regiani <andrei.cpp@gmail.com>
2025-03-27 00:10:36 +0700

    pearLink interface changes

--- subsystems/sidecar/lib/model.js
+++ subsystems/sidecar/lib/model.js
@@ -3,6 +3,7 @@ const HyperDB = require('hyperdb')
 const DBLock = require('db-lock')
 const pearLink = require('pear-link')
 const dbSpec = require('../../../spec/db')
+const { ALIASES } = require('../../../constants')
 
 module.exports = class Model {
   constructor (corestore) {
@@ -20,9 +21,9 @@ module.exports = class Model {
   }
 
   async getBundle (link) {
-    link = pearLink.origin(link)
-    LOG.trace('db', `GET ('@pear/bundle', ${JSON.stringify({ link })})`)
-    const bundle = await this.db.get('@pear/bundle', { link })
+    const { origin } = pearLink(ALIASES)(link)
+    LOG.trace('db', `GET ('@pear/bundle', ${JSON.stringify({ link: origin })})`)
+    const bundle = await this.db.get('@pear/bundle', { link: origin })
     return bundle
   }
 
@@ -32,10 +33,10 @@ module.exports = class Model {
   }
 
   async addBundle (link, appStorage) {
-    link = pearLink.origin(link)
+    const { origin } = pearLink(ALIASES)(link)
     const tx = await this.lock.enter()
-    LOG.trace('db', `INSERT ('@pear/bundle', ${JSON.stringify({ link, appStorage })})`)
-    await tx.insert('@pear/bundle', { link, appStorage })
+    LOG.trace('db', `INSERT ('@pear/bundle', ${JSON.stringify({ link: origin, appStorage })})`)
+    await tx.insert('@pear/bundle', { link: origin, appStorage })
     await this.lock.exit()
     return { link, appStorage }
   }
