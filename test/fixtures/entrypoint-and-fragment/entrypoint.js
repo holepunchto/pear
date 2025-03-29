8603f0ab831d628147b2ffac2640f88b29998efb
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-03-29 14:02:20 +0100

    fix undefined entrypoint and fragment in links

---
+++ test/fixtures/entrypoint-and-fragment/entrypoint.js
@@ -0,0 +1,9 @@
+const pipe = Pear.worker.pipe()
+pipe.on('data', () => {
+  try {
+    pipe.write(JSON.stringify({ entrypoint: Pear.config.entrypoint, fragment: Pear.config.fragment }) + '\n')
+  } catch (err) {
+    console.error(err)
+    Pear.exit()
+  }
+})
