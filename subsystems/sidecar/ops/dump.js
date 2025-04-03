ce127a23ae116785cc13a7174be5fe636ec1bffc
dmc <david.mark.clements@gmail.com>
2025-04-03 20:35:05 +0200

    pear dump support pear://0.<length>.<key>

--- subsystems/sidecar/ops/dump.js
+++ subsystems/sidecar/ops/dump.js
@@ -30,7 +30,7 @@ module.exports = class Dump extends Opstream {
     const isFile = isFileLink && (await fsp.stat(parsed.pathname)).isDirectory() === false
 
     const key = parsed.drive.key
-    checkout = Number(checkout)
+    checkout = checkout ? Number(checkout) : parsed.drive.length
 
     const query = await this.sidecar.model.getBundle(link)
     const encryptionKey = query?.encryptionKey
