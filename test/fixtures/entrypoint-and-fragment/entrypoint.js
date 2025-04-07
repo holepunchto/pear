921b772767eb164243dcdf4d8d93b7b75092726f
dmc <david.mark.clements@gmail.com>
2025-04-07 19:33:39 +0200

    alignment

--- test/fixtures/entrypoint-and-fragment/entrypoint.js
+++ test/fixtures/entrypoint-and-fragment/entrypoint.js
@@ -1,4 +1,4 @@
-const pipe = Pear.worker.pipe()
+const pipe = Pear.pipe
 pipe.on('data', () => {
   try {
     pipe.write(JSON.stringify({ entrypoint: Pear.config.entrypoint, fragment: Pear.config.fragment }) + '\n')
