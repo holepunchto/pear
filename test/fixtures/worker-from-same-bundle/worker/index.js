34bb29d1841c1d717e8cac270cf5fe4c90290815
dmc <david.mark.clements@gmail.com>
2025-04-07 19:33:39 +0200

    alignment

--- test/fixtures/worker-from-same-bundle/worker/index.js
+++
@@ -1,9 +0,0 @@
-const pipe = Pear.worker.pipe()
-pipe.on('data', () => {
-  try {
-    pipe.write(JSON.stringify({ entrypoint: Pear.config.entrypoint }) + '\n')
-  } catch (err) {
-    console.error(err)
-    Pear.exit()
-  }
-})
