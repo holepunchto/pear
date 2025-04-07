6d61f2d6abeff88e4d50991a1ca86542f575f0da
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-04-07 18:03:53 +0800

    Add error when ran as a different user (#637)
    * Add function for checking for root on posix systems
    
    * Inline root check
    
    * Merge requires
    
    * Update error wording
    
    * Add missing require from move
    
    * Rename variables
    
    * Use bare-os
    
    * Use Pear platform directory in wording
    
    * Update bare-os to latest
    
    * Update package-lock.json
    
    * Update root checker to compare uids using the latest bare-os
    
    * Rename checkRoot to checkUser
    
    * Move checkUser into its own file and move the check to boot.js
    
    * Improve error message
    
    * Inline the user permissions check into boot.js
    
    * Avoid reimporting constants
    
    * Make the error message more concise
    
    * Remove excess line
    
    * Set API.CONSTANTS instead of lazy loading

--- boot.js
+++ boot.js
@@ -1,11 +1,31 @@
 /** @typedef {import('pear-interface')} */
 'use strict'
+const { isWindows } = require('which-runtime')
+
 class API {
   static RTI = { checkout: require('./checkout') }
-  static get CONSTANTS () { return require('pear-api/constants') }
+  static CONSTANTS = null
   config = {}
 }
 global.Pear = new API()
+
+const constants = require('pear-api/constants')
+API.CONSTANTS = constants
+
+if (isWindows === false) {
+  const fs = require('bare-fs')
+  const os = require('bare-os')
+
+  const ownerUid = fs.statSync(constants.PLATFORM_DIR).uid
+  const userUid = os.userInfo().uid
+
+  if (ownerUid !== userUid) {
+    const err = new Error(`Current user does not own ${constants.PLATFORM_DIR}`)
+    err.name = 'User Permissions Error'
+    throw err
+  }
+}
+
 const BOOT_SIDECAR = 1
 const BOOT_CLI = 2
 switch (getBootType()) {
