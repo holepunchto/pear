8718b3fa9b33e8ac63993425ce88661d1dfc4d73
Geordan Gesink <165498764+geordangesink@users.noreply.github.com>
2025-04-15 18:04:20 +0200

    detached spawn to daemon (#695)
    * detached spawn to daemon
    
    * fix if case
    
    * fix and lint
    
    * unref only for subprocess
    
    * lint
    
    * Trigger GitHub Actions
    
    * remove unref; rename; remove default opt;
    
    * remove sc and unref; remove default opt
    
    * lint
    
    * bump daemon

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -2,6 +2,7 @@
 const fs = require('bare-fs')
 const path = require('bare-path')
 const { spawn } = require('bare-subprocess')
+const { spawn: daemon } = require('bare-daemon')
 const fsx = require('fs-native-extensions')
 const streamx = require('streamx')
 const ReadyResource = require('ready-resource')
@@ -553,7 +554,7 @@ class Sidecar extends ReadyResource {
       }
       const { dir, cwd, cmdArgs, env } = client.userData.state
       const appling = client.userData.state.appling
-      const opts = { cwd, env, detached: false, stdio: 'pipe' }
+      const opts = { cwd, env, detached: false }
       if (!client.closed) {
         await new Promise((resolve) => {
           if (client.closed) {
@@ -567,8 +568,8 @@ class Sidecar extends ReadyResource {
       }
       if (appling) {
         const applingPath = typeof appling === 'string' ? appling : appling?.path
-        if (isMac) spawn('open', [applingPath.split('.app')[0] + '.app'], opts).unref()
-        else spawn(applingPath, opts).unref()
+        if (isMac) spawn('open', [applingPath.split('.app')[0] + '.app'], opts)
+        else spawn(applingPath, opts)
       } else {
         const cmd = command('run', ...rundef)
         cmd.parse(cmdArgs.slice(1))
@@ -581,7 +582,7 @@ class Sidecar extends ReadyResource {
           cmdArgs.push(dir)
         }
 
-        spawn(RUNTIME, cmdArgs, opts).unref()
+        spawn(RUNTIME, cmdArgs, opts)
       }
 
       return
@@ -601,11 +602,11 @@ class Sidecar extends ReadyResource {
     await sidecarClosed
 
     for (const { cwd, dir, appling, cmdArgs, env } of restarts) {
-      const opts = { cwd, env, detached: true, stdio: 'ignore' }
+      const opts = { cwd, env }
       if (appling) {
         const applingPath = typeof appling === 'string' ? appling : appling?.path
-        if (isMac) spawn('open', [applingPath.split('.app')[0] + '.app'], opts).unref()
-        else spawn(applingPath, opts).unref()
+        if (isMac) daemon('open', [applingPath.split('.app')[0] + '.app'], opts)
+        else daemon(applingPath, opts)
       } else {
         const TARGET_RUNTIME = this.updater === null
           ? RUNTIME
@@ -622,7 +623,7 @@ class Sidecar extends ReadyResource {
           cmdArgs.push(dir)
         }
 
-        spawn(TARGET_RUNTIME, cmdArgs, opts).unref()
+        daemon(TARGET_RUNTIME, cmdArgs, opts)
       }
     }
   }
