f3aa942ced5827378eb809355453e9f63fd488e5
Geordan Gesink <georgegeordan@gmail.com>
2025-04-17 14:47:54 +0200

    restore test/index.js

--- test/index.js
+++ test/index.js
@@ -17,7 +17,6 @@ async function runTests () {
   await import('./08-shift.test.js')
   await import('./09-data.test.js')
   await import('./10-dump.test.js')
-  await import('./helper.js')
 
   test.resume()
 }
