8c95317ea0d3e68dd9d062b3c0070706e6596fee
Andrei Regiani <andrei.cpp@gmail.com>
2025-04-23 19:24:33 +0700

    `pear drop` and `pear data manifest` (#706)
    * pear-ipc dev
    
    * pear data manifest + pear drop
    
    * link validation into model
    
    * Solve review comments
    
    * label `version`
    
    * remove else (paparam bug)
    
    * pear-ipc 6.0.0
    
    * plan(17)
    
    ---------
    
    Co-authored-by: David Mark Clements <david.mark.clements@gmail.com>

--- cmd/drop.js
+++ cmd/drop.js
@@ -1,9 +1,11 @@
 'use strict'
+const parseLink = require('pear-api/parse-link')
 const { outputter, confirm, ansi } = require('pear-api/terminal')
+const { ERR_INVALID_INPUT } = require('pear-api/errors')
 const os = require('bare-os')
 const path = require('bare-path')
 
-const output = outputter('reset', {
+const output = outputter('drop', {
   reseting: ({ link }) => `\nReseting storage of application ${link}`,
   complete: () => 'Reset Complete\n',
   error: ({ code, stack }) => {
@@ -11,9 +13,13 @@ const output = outputter('reset', {
   }
 })
 
-module.exports = (ipc) => async function reset (cmd) {
+module.exports = (ipc) => async function drop (cmd) {
   const { json } = cmd.flags
   const link = cmd.args.link
+  if (link) {
+    const parsed = parseLink(link)
+    if (!parsed) throw ERR_INVALID_INPUT(`Link "${link}" is not a valid key`)
+  }
   const isPear = link.startsWith('pear://')
 
   const dialog = ansi.warning + `  ${ansi.bold('WARNING')} the storage of ${ansi.bold(link)} will be permanently deleted and cannot be recovered. To confirm type "RESET"\n\n`
@@ -23,5 +29,5 @@ module.exports = (ipc) => async function reset (cmd) {
   const msg = '\n' + ansi.cross + ' uppercase RESET to confirm\n'
   await confirm(dialog, ask, delim, validation, msg)
 
-  await output(json, ipc.reset({ link: isPear || path.isAbsolute(link) ? link : path.join(os.cwd(), link) }))
+  await output(json, ipc.drop({ link: isPear || path.isAbsolute(link) ? link : path.join(os.cwd(), link) }))
 }
