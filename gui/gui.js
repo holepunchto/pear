3404829d8e544f4167b1e63627d41c107ea258db
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-04-23 10:45:07 +0200

    fix main entrypoint for staged apps

--- gui/gui.js
+++ gui/gui.js
@@ -555,8 +555,10 @@ class App {
 
       const decalSession = electron.session.fromPartition('persist:pear')
 
+      const { main } = await this.ipc.config()
+
       decalSession.setUserAgent('Pear Platform')
-      const entry = state.entrypoint || '/' + state.main
+      const entry = state.entrypoint || '/' + (main || state.main)
       const identify = await this.ipc.identify({ startId: state.startId })
       const { id, host } = identify
 
