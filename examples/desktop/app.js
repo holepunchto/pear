0c233316902c0cc431d9d1d89a04827b6e52aa58
dmc <david.mark.clements@gmail.com>
2025-04-30 14:11:31 +0200

    example fix

--- examples/desktop/app.js
+++ examples/desktop/app.js
@@ -1,21 +1,22 @@
 /** @typedef {import('pear-interface')} */ /* global Pear */
-const { versions, config, updates, wakeups, Window } = Pear
+import ui from 'pear-electron'
+const { config } = Pear
 console.log('link', config.link)
 console.log('linkData', config.linkData)
 console.log('key', config.key)
 
-updates(function (data) {
+Pear.updates(function (data) {
   console.log('update available:', data)
 })
 
-wakeups(async (wakeup) => {
+Pear.wakeups(async (wakeup) => {
   console.log('GOT WAKEUP', wakeup)
-  await Window.self.focus({ steal: true })
+  await ui.app.focus({ steal: true })
 })
 
 document.getElementById('channel').innerText = config.channel || 'none [ dev ]'
 document.getElementById('release').innerText = config.release || (config.dev ? 'none [ dev ]' : '0')
-const { app, platform } = await versions()
+const { app, platform } = await Pear.versions()
 document.getElementById('platformKey').innerText = platform.key
 document.getElementById('platformFork').innerText = platform.fork
 document.getElementById('platformLength').innerText = platform.length
