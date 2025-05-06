4361bab125fa7da97bc1283fe5d22a35eddad28b
dmc <david.mark.clements@gmail.com>
2025-05-06 16:11:26 +0200

    bump pear-api

--- cmd/index.js
+++ cmd/index.js
@@ -235,7 +235,7 @@ module.exports = async (ipc, argv = Bare.argv.slice(1)) => {
     command('dht', summary('DHT known-nodes cache'), (cmd) => runners.data(ipc).dht(cmd)),
     command('gc', summary('Garbage collection records'), (cmd) => runners.data(ipc).gc(cmd)),
     command('manifest', summary('database internal versioning'), (cmd) => runners.data(ipc).manifest(cmd)),
-    flag('--secrets', 'Show sensitive information, i.e. encryption-keys'),
+    flag('--secrets', 'Show sensitive information'),
     flag('--json', 'Newline delimited JSON output'),
     () => { console.log(data.help()) }
   )
