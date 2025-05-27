36449addae31fc6722208dbd8e62327b3712b7aa
Andrei Regiani <andrei.cpp@gmail.com>
2025-05-28 02:36:44 +0800

    [pear-next] CLI `$ pear data assets` (#741)
    * $ pear data assets
    
    * Summary "on-disk assets for app"
    
    * Casing
    
    * Support `pear data assets [link]`, refactor `pear data apps`
    
    * Refactor resource 'link' to be 'apps'

--- cmd/index.js
+++ cmd/index.js
@@ -250,7 +250,8 @@ module.exports = async (ipc, argv = Bare.argv.slice(1)) => {
     command('apps', summary('Installed apps'), arg('[link]', 'Filter by link'), (cmd) => runners.data(ipc).apps(cmd)),
     command('dht', summary('DHT known-nodes cache'), (cmd) => runners.data(ipc).dht(cmd)),
     command('gc', summary('Garbage collection records'), (cmd) => runners.data(ipc).gc(cmd)),
-    command('manifest', summary('database internal versioning'), (cmd) => runners.data(ipc).manifest(cmd)),
+    command('manifest', summary('Database internal versioning'), (cmd) => runners.data(ipc).manifest(cmd)),
+    command('assets', summary('On-disk assets for app'), arg('[link]', 'Filter by link'), (cmd) => runners.data(ipc).assets(cmd)),
     flag('--secrets', 'Show sensitive information'),
     flag('--json', 'Newline delimited JSON output'),
     () => { console.log(data.help()) }
