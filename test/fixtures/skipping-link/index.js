539585981241cf5b9a10e49d64210a1a4ab1a0f8
Geordan Gesink <georgegeordan@gmail.com>
2025-06-23 19:26:18 +0200

    add test for malformed args in ops with link arg

---
+++ test/fixtures/skipping-link/index.js
@@ -0,0 +1,12 @@
+const pipe = Pear.pipe
+
+Pear.stage({ dir: '/path/to/dir' })
+
+pipe.on('data', () => {
+    try {
+      pipe.write('pass')
+    } catch (err) {
+      console.error(err)
+      Pear.exit()
+    }
+  })
