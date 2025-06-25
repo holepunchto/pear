331931d792c1dc844235e91aca39480965b4a7b5
dmc <david.mark.clements@gmail.com>
2025-06-25 12:36:47 +0200

    use pear-api/terminal byteSize

--- cmd/data.js
+++ cmd/data.js
@@ -1,8 +1,7 @@
 'use strict'
 const plink = require('pear-api/link')
-const { outputter, ansi } = require('pear-api/terminal')
+const { outputter, ansi, byteSize } = require('pear-api/terminal')
 const { ERR_INVALID_INPUT } = require('pear-api/errors')
-const byteSize = require('tiny-byte-size')
 
 const padding = '    '
 const placeholder = '[ No results ]\n'
