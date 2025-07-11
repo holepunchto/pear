d7cd0258910e8bce3e39ea83b5ba57c7a35202c2
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-07-11 10:39:54 +0200

    added pid and link to devtools errors

--- gui/gui.js
+++ gui/gui.js
@@ -1573,7 +1573,7 @@ class PearGUI extends ReadyResource {
       pipe.on('data', (data) => { evt.reply('workerPipeData', { data, id }) })
       pipe.on('end', () => { evt.reply('workerPipeEnd', { id }) })
       pipe.on('error', (err) => { evt.reply('workerPipeError', { stack: err.stack, id }) })
-      worker.stderr.on('data', (data) => { evt.reply('workerStdError', { data: data.toString(), id }) })
+      worker.stderr.on('data', (data) => { evt.reply('workerStdError', { pid: worker.pid, link: worker.link, data: data.toString(), id }) })
     })
 
     electron.ipcMain.on('workerPipeId', (evt) => {
