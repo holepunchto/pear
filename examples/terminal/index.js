3bb6ac2c59b59f8c5de10fa1bb56f30e645270ea
dmc <david.mark.clements@gmail.com>
2025-08-06 15:51:58 +0200

    improve terminal example

--- examples/terminal/index.js
+++ examples/terminal/index.js
@@ -1,11 +1,12 @@
 /** @typedef {import('pear-interface')} */ /* global Pear */
-Pear.updates((update) => {
+const updates = Pear.updates((update) => {
   console.log('Application update available:', update)
 })
 const [grn, rst, dim] = ['\x1b[32m', '\x1b[0m', '\x1b[2m']
 const v = ({ key, length, fork }) => `v${fork}.${length}.${(key += '').length <= 12 ? key : key.slice(0, 12) + '…'}`
 const { app, platform } = await Pear.versions()
-const out = `${grn}           ▅
+const out = `
+${grn}           ▅
            ▀
         ▂▂▄▟▙▃
        ▄▄▄▄▆▆▆▆         ${Pear.config.name}
@@ -16,6 +17,9 @@ const out = `${grn}           ▅
    ▄▄▄▄▄▄▄▄▆▆▆▆▆▆▆▆
    ▄▄▄▄▄▄▄▄▆▆▆▆▆▆▆▆     ${rst}${grn}Welcome to the IoP
      ▄▄▄▄▄▄▆▆▆▆▆▆
-       ▄▄▄▄▆▆▆▆
-`
+       ▄▄▄▄▆▆▆▆         
+       
+       exit: ^C${rst}`
 console.log('\n\x1b[s\x1b[J' + out)
+
+Pear.teardown(() => { updates.end() })
