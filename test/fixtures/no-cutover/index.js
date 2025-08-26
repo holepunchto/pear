c562589ff66e83311b739442f6ef626c6a9e3bf8
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-08-27 05:34:19 +0800

    Add no-cutover fixture

---
+++ test/fixtures/no-cutover/index.js
@@ -0,0 +1,13 @@
+Pear.constructor.CUTOVER = false
+
+const pipe = require('pear-pipe')()
+
+let updates
+pipe.on('data', (data) => {
+  if (data.toString().trim() === 'start-listener') updates = Pear.updates((data) => {
+    pipe.write(JSON.stringify(data) + '\n')
+  })
+})
+
+pipe.on('end', () => updates?.end?.())
+pipe.resume()
