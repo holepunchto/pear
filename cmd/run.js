5453a727cb9f3691185a01b36784cc9d909ee96d
unknown <georgegeordan@gmail.com>
2025-08-26 19:26:24 +0200

    only slice pathname

--- cmd/run.js
+++ cmd/run.js
@@ -74,7 +74,7 @@ module.exports = (ipc) => async function run (cmd, devrun = false) {
   if (onDisk === false && isPear === false) throw ERR_INVALID_INPUT('Key must start with pear://')
 
   const cwd = os.cwd()
-  const pathnameNoBase = isWindows ? (onDisk ? pathname : cwd).slice(1) : (onDisk ? pathname : cwd)
+  const pathnameNoBase = isWindows ? (onDisk ? pathname.slice(1) : cwd) : (onDisk ? pathname : cwd)
   let dir = normalize(flags.base || pathnameNoBase)
   let pkg = null
 
