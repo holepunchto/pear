9da0b72773e41ec7f2a571ca38d08922f5e85a5b
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-09-03 03:30:31 +0800

    Remove unused updateAvailable field

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -59,7 +59,6 @@ class Sidecar extends ReadyResource {
   spindownt = null
   spindownms = SPINDOWN_TIMEOUT
   decomissioned = false
-  updateAvailable = null
   swarm = null
   keyPair = null
   discovery = null
@@ -333,8 +332,6 @@ class Sidecar extends ReadyResource {
   }
 
   async updateNotify (version, info = {}) {
-    this.updateAvailable = { version, info }
-
     if (info.link) LOG.info('sidecar', 'Application update available:')
     else if (version.force) LOG.info('sidecar', 'Platform Force update (' + version.force.reason + '). Updating to:')
     else LOG.info('sidecar', 'Platform update available. Restart to update to:')
