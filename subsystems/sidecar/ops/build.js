ad70b9f48eeaaaeb1c8cfa065bac117e2b0d6041
Andrei Regiani <andrei.cpp@gmail.com>
2025-09-16 01:27:12 -0500

    bare-make: use JS API

--- subsystems/sidecar/ops/build.js
+++ subsystems/sidecar/ops/build.js
@@ -2,6 +2,7 @@
 const { spawnSync } = require('bare-subprocess')
 const fs = require('bare-fs')
 const os = require('bare-os')
+const make = require('bare-make')
 const Opstream = require('../lib/opstream')
 
 module.exports = class Build extends Opstream {
@@ -29,12 +30,10 @@ module.exports = class Build extends Opstream {
     if (result.status !== 0) throw new Error(`npm exited with code ${result.status}: ${result.stderr?.toString()}`)
 
     this.push({ tag: 'generate', data: {} })
-    result = spawnSync('bare-make', ['generate'], { cwd: repoDir, stdio: 'inherit' })
-    if (result.status !== 0) throw new Error(`bare-make generate exited with code ${result.status}: ${result.stderr?.toString()}`)
+    await make.generate()
 
     this.push({ tag: 'build', data: {} })
-    result = spawnSync('bare-make', ['build'], { cwd: repoDir, stdio: 'inherit' })
-    if (result.status !== 0) throw new Error(`bare-make build exited with code ${result.status}: ${result.stderr?.toString()}`)
+    await make.build()
 
     this.push({ tag: 'complete', data: { dir } })
   }
