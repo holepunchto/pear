d44cd4471c1fa0b73b759d8ac2a9c43d0a4dbde7
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-09-17 12:32:57 +0800

    Add id to each listener subscription

--- test/fixtures/no-cutover/index.js
+++ test/fixtures/no-cutover/index.js
@@ -2,14 +2,18 @@ Pear.constructor.CUTOVER = false
 
 const pipe = require('pear-pipe')()
 
-let updates
+let id = 0
+let updates = []
 pipe.on('data', (data) => {
-  if (data.toString().trim() === 'start-listener') updates = Pear.updates((data) => {
-    pipe.write(JSON.stringify(data) + '\n')
-  })
+  if (data.toString().trim() === 'start-listener') {
+    const listenerId = ++id
+    updates.push(Pear.updates((data) => {
+      pipe.write(JSON.stringify({ id: listenerId, data }) + '\n')
+    }))
+  }
 })
 
-pipe.on('end', () => updates?.end?.())
+pipe.on('end', () => updates.forEach(u => u?.end?.()))
 pipe.resume()
 
 Pear.versions().then((versions) => pipe.write(JSON.stringify(versions) + '\n'))
