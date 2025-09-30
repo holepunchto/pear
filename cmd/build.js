3a273c7272d36af64a5a5133cde641454baad6f4
Andrei Regiani <andrei.cpp@gmail.com>
2025-09-30 17:02:14 -0300

    use stream

--- cmd/build.js
+++ cmd/build.js
@@ -1,9 +1,19 @@
 'use strict'
 const os = require('bare-os')
+const { outputter, ansi } = require('pear-terminal')
 const build = require('pear-build')
 
+const output = outputter('build', {
+  init: ({ dir }) => `\n${ansi.pear} Building appling into ${dir}\n`,
+  generate: () => 'Generating project...\n',
+  build: () => 'Building project...\n',
+  complete: ({ dir }) => `\n${ansi.tick} Build complete: ${dir}\n`,
+  error: ({ message }) => `Error: ${message}\n`
+})
+
 module.exports = () => async function build (cmd) {
+  const { json } = cmd.flags
   const link = cmd.args.link
   const { dir = os.cwd() } = cmd.args
-  await build({ link, dir })
+  await output(json, build({ link, dir }))
 }
