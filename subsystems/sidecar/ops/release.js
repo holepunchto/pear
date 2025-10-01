45738f9a1d4c196db278aae2e50c78cde9e2bd9b
dmc <david.mark.clements@gmail.com>
2025-10-01 04:55:38 +0200

    release channel fix

--- subsystems/sidecar/ops/release.js
+++ subsystems/sidecar/ops/release.js
@@ -1,7 +1,8 @@
 'use strict'
-const hypercoreid = require('hypercore-id-encoding')
+const Hyperdrive = require('hyperdrive')
 const { randomBytes } = require('hypercore-crypto')
 const { ERR_UNSTAGED } = require('pear-errors')
+const plink = require('pear-link')
 const Bundle = require('../lib/bundle')
 const Opstream = require('../lib/opstream')
 const State = require('../state')
@@ -12,26 +13,29 @@ module.exports = class Release extends Opstream {
   }
 
   async #op({ name, channel, checkout, link, dir, cmdArgs }) {
-    const key = link ? hypercoreid.decode(link) : null
-
-    const { session } = this
-
+    const parsed = link ? plink.parse(link) : null
+    let key = parsed?.drive.key ?? null
     const state = new State({
       id: `releaser-${randomBytes(16).toString('hex')}`,
       flags: { checkout, channel, link },
       dir,
       cmdArgs
     })
-
-    await this.sidecar.ready()
-
+    const { session } = this
+    const pkg = await State.localPkg(state)
+    await State.build(state, pkg)
     name = name || state.name
 
     this.push({ tag: 'releasing', data: { name, channel, link } })
 
-    const corestore = this.sidecar.getCorestore(name || state.name, channel, {
+    await this.sidecar.ready()
+
+    const corestore = this.sidecar.getCorestore(name, channel, {
       writable: true
     })
+    await corestore.ready()
+
+    if (key === null) await Hyperdrive.getDriveKey(corestore)
 
     const bundle = new Bundle({ corestore, channel, key })
     await session.add(bundle)
@@ -39,7 +43,7 @@ module.exports = class Release extends Opstream {
 
     if (manifest === null) {
       throw ERR_UNSTAGED(
-        `The "${name}" app has not been staged on ${channel ? '"' + channel + '" channel' : link}.`
+        `Failed to release "${name}" app on ${channel ? '"' + channel + '" channel' : link}.`
       )
     }
 
