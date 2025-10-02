4f7632e9ce39e68c36c4ce3ad10d0482a436e3cc
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-02 04:09:47 -0300

    validate input

--- cmd/build.js
+++ cmd/build.js
@@ -2,6 +2,8 @@
 const os = require('bare-os')
 const { outputter, ansi } = require('pear-terminal')
 const pearBuild = require('pear-build')
+const plink = require('pear-link')
+const { ERR_INVALID_INPUT } = require('pear-errors')
 
 const output = outputter('build', {
   init: ({ dir }) => `\n${ansi.pear} Building appling into ${dir}\n`,
@@ -15,6 +17,8 @@ module.exports = (ipc) =>
   async function build(cmd) {
     const { json } = cmd.flags
     const link = cmd.args.link
+    const { drive }  = plink.parse(link)
+    if (!drive.key) throw ERR_INVALID_INPUT(`Link "${link}" is not a valid key`)
     const { dir = os.cwd() } = cmd.args
     await output(json, pearBuild({ link, dir }))
   }
