79e4787727b1a7707ba31875c75982f77dd6e88b
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-09 03:48:17 -0300

    use init template

--- cmd/build.js
+++ cmd/build.js
@@ -1,9 +1,9 @@
 'use strict'
 const os = require('bare-os')
+const fs = require('bare-fs')
 const pearBuild = require('pear-build')
 const plink = require('pear-link')
 const { outputter, ansi } = require('pear-terminal')
-const { ERR_INVALID_INPUT } = require('pear-errors')
 
 const output = outputter('build', {
   init: ({ dir }) => `\n${ansi.pear} Building into ${dir}\n`,
@@ -25,11 +25,47 @@ module.exports = (ipc) => {
 
   return async function build(cmd) {
     const { json } = cmd.flags
-    const channel = cmd.args.channel
-    const link = cmd.args.link
+    const isKey = plink.parse(cmd.args.channel).drive.key !== null
+    const channel = isKey ? cmd.args.link : cmd.args.channel
+    const link = isKey ? cmd.args.channel : null
+    if (!channel && !link)
+      throw ERR_INVALID_INPUT(
+        'A valid pear link or the channel name must be specified.'
+      )
+    // TODO: handle channel case
     const { drive }  = plink.parse(link)
-    if (!drive.key) throw ERR_INVALID_INPUT(`Link "${link}" is not a valid key`)
     const { dir = os.cwd() } = cmd.args
+    const distributables = path.join(dir, 'distributables')
+    await fs.promises.mkdir(distributables, { recursive: true })
+    const stream = await ipc.dump(link, { dir: distributables, list: ['package.json', 'icons'] })
+    await new Promise(resolve => stream.once('end', resolve))
+
+    const pkg = await import(path.join(distributables, 'package.json'), { assert: { type: 'json' } })
+    const build = pkg.default.pear.build
+
+    const tmpl = 'init/templates/distributables'
+    const defaults = { 
+      "id": `${drive.key}`,
+      "name": `${build.name || pkg.default.name}`,
+      "link": `${link}`,
+      "version": `${build.version || pkg.default.version}`,
+      "author": `${build.author || pkg.default.author}`,
+      "description": `${build.description || pkg.default.description}`,
+      "darwin.identifier": `${build.darwin?.identifier || ''}`,
+      "darwin.category": `${build.darwin?.category || ''}`,
+      "darwin.signingidentity": `${build.darwin?.['signing-identity'] || ''}`,
+      "darwin.entitlements": `${build.darwin?.['entitlements'] || ''}`,
+      "win.signingsubject": `${build.win32?.['signing-subject'] || ''}`,
+      "win.signingthumbprint": `${build.win32?.['signing-thumbprint'] || ''}`,
+      "linux.category": `${build.linux?.category || ''}`
+    }
+    await require('../init')(tmpl, dir, {
+          cwd: os.cwd(),
+          ipc,
+          force: true,
+          defaults,
+          tmpl
+        })
     await output(json, pearBuild({ link, dir }))
   }
 }
