024a8329a3c7945044d99af5409f764b94feb1f2
Andrei Regiani <andrei.cpp@gmail.com>
2025-10-14 03:27:38 -0300

    opwait, init tmpl defaults, manifest

--- cmd/build.js
+++ cmd/build.js
@@ -3,6 +3,7 @@ const os = require('bare-os')
 const fs = require('bare-fs')
 const pearBuild = require('pear-build')
 const plink = require('pear-link')
+const opwait = require('pear-opwait')
 const { outputter, ansi } = require('pear-terminal')
 
 const output = outputter('build', {
@@ -33,32 +34,26 @@ module.exports = (ipc) => {
         'A valid pear link or the channel name must be specified.'
       )
     if (channel) {
-      // TODO resolve link from channel arg
-      ipc.info({ link, channel, manifest: true })
+      const { manifest } = await opwait(ipc.info({ link, channel, manifest: true }))
     }
-
-    // TODO get post processed manifest
-    ipc.info({ link, manifest: true })
-    const pkg = {} // TODO load it
-
+    const { manifest } = await opwait(ipc.info({ link, manifest: true }))
     const { drive } = plink.parse(link)
-    const build = pkg.default.pear.build
+    const build = manifest.pear.build
     const { dir = os.cwd() } = cmd.args
     const distributables = path.join(dir, 'distributables')
     await fs.promises.mkdir(distributables, { recursive: true })
-
     const tmpl = 'init/templates/distributables'
     const defaults = { 
       "id": `${drive.key}`,
-      "name": `${build.name || pkg.default.name}`,
+      "name": `${build.name || manifest.name}`,
       "link": `${link}`,
-      "version": `${build.version || pkg.default.version}`,
-      "author": `${build.author || pkg.default.author}`,
-      "description": `${build.description || pkg.default.description}`,
+      "version": `${build.version || manifest.version}`,
+      "author": `${build.author || manifest.author}`,
+      "description": `${build.description || manifest.description}`,
       "darwin.identifier": `${build.darwin?.identifier || ''}`,
       "darwin.category": `${build.darwin?.category || ''}`,
       "darwin.signingidentity": `${build.darwin?.['signing-identity'] || ''}`,
-      "darwin.entitlements": `${build.darwin?.['entitlements'] || ''}`,
+      "darwin.entitlements": `${build.darwin?.entitlements || ''}`,
       "win.signingsubject": `${build.win32?.['signing-subject'] || ''}`,
       "win.signingthumbprint": `${build.win32?.['signing-thumbprint'] || ''}`,
       "linux.category": `${build.linux?.category || ''}`
@@ -71,10 +66,8 @@ module.exports = (ipc) => {
           tmpl
         })
 
-    // overwrite default template icons
-    const stream = await ipc.dump(link, { dir: distributables, list: ['icons'] })
-    await new Promise(resolve => stream.once('end', resolve))
-
+    // overwrite default template icons using opwait
+    await opwait(ipc.dump(link, { dir: distributables, list: ['icons'] }))
     await output(json, pearBuild({ link, dir }))
   }
 }
