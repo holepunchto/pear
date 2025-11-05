921f6dd073a9bca67db4f4b486dd90aebc555494
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-11-05 23:04:43 +0800

    Add pre test

---
+++ test/14-pre.test.js
@@ -0,0 +1,117 @@
+'use strict'
+const test = require('brittle')
+const path = require('bare-path')
+const Helper = require('./helper')
+const { spawn } = require('bare-subprocess')
+const { RUNTIME } = require('pear-constants')
+const { Duplex } = require('streamx')
+const pre = path.join(Helper.localDir, 'test', 'fixtures', 'pre')
+const seedOpts = (id) => ({
+  channel: `test-${id}`,
+  name: `test-${id}`,
+  key: null,
+  dir: pre,
+  cmdArgs: []
+})
+const stageOpts = (id, dir) => ({
+  ...seedOpts(id, dir),
+  dryRun: false,
+  ignore: []
+})
+
+const rig = new Helper.Rig()
+function trimAnsi(str) {
+  return str.replaceAll('\x1b[?25l', '').replaceAll('\x1b[?25h', '')
+}
+
+async function run({ link, args = [], argv = [] }) {
+  const sp = spawn(
+    RUNTIME,
+    ['run', ...argv, '--pre-io', '--trusted', link, ...args],
+    {
+      stdio: ['inherit', 'pipe', 'inherit'],
+      windowsHide: true
+    }
+  )
+
+  const pipe = new Duplex()
+  sp.once('exit', (exitCode) => {
+    if (exitCode !== 0) pipe.emit('crash', { exitCode })
+    pipe.push(null)
+  })
+  sp.stdout.on('data', (data) => {
+    for (const line of data.toString().split(/\r?\n/g)) {
+      if (trimAnsi(line).trim().length === 0) continue
+      pipe.push({ type: 'stdout', data: line })
+    }
+  })
+
+  return { pipe }
+}
+
+test.hook('pre setup', rig.setup)
+
+test('should execute pre and update config when running locally', async function ({
+  ok,
+  is,
+  plan,
+  comment
+}) {
+  plan(4)
+
+  comment('\trunning')
+  const { pipe } = await run({ link: pre })
+
+  const preResult = await Helper.untilData(pipe)
+  is(preResult.type, 'stdout', 'should output to stdout')
+  ok(preResult.data.includes('configure'), 'pre should run configure step')
+
+  const result = await Helper.untilData(pipe)
+  is(result.type, 'stdout', 'should output to stdout')
+  is(
+    result.data,
+    '{"name":"pre-success"}',
+    'pre should have updated name in config'
+  )
+
+  await Helper.untilClose(pipe)
+})
+
+test('should not execute pre and update config when running locally', async function ({
+  is,
+  plan,
+  comment,
+  teardown
+}) {
+  plan(2)
+
+  const testId = Helper.getRandomId()
+  const stager = new Helper(rig)
+  teardown(() => stager.close(), { order: Infinity })
+  await stager.ready()
+
+  comment('\tstaging')
+  const staging = stager.stage(stageOpts(testId))
+  teardown(() => Helper.teardownStream(staging))
+  const until = await Helper.pick(staging, [
+    { tag: 'staging' },
+    { tag: 'final' }
+  ])
+  const { link } = await until.staging
+  await until.final
+
+  comment('\trunning')
+  const { pipe } = await run({ link })
+
+  const result = await Helper.untilData(pipe)
+  is(result.type, 'stdout', 'should output to stdout')
+  is(
+    result.data,
+    '{"name":"pre-did-not-run"}',
+    'pre should have updated name in config'
+  )
+
+  await Helper.untilClose(pipe)
+})
+
+test.hook('pre cleanup', rig.cleanup)
