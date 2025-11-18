4c18181103368a93b765dcf1104e5269302b1442
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-11-18 20:39:46 +0800

    Use pear-updates and pear-restart in fixture

--- test/fixtures/update-restart/index.js
+++ test/fixtures/update-restart/index.js
@@ -1,10 +1,12 @@
 const fs = require('bare-fs')
+const updates = require('pear-updates')
+const restart = require('pear-restart')
 
 const output = Pear.app.args[0]
 if (!output) throw new Error('Output file path required as first argument')
 
-Pear.updates(() => {
-  Pear.restart({ platform: false })
+updates(() => {
+  restart({ platform: false })
 })
 
 Pear.versions().then((versions) => {
