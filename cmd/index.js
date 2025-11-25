b9098fff9eac4ce890f2950447ff5da20b3f07cd
David Mark Clements <david.mark.clements@gmail.com>
2025-11-25 23:36:25 +0100

    pear gc cores (#944)
    * pear gc cores
    
    * compact

--- cmd/index.js
+++ cmd/index.js
@@ -322,7 +322,7 @@ module.exports = async (ipc, argv = Bare.argv.slice(1)) => {
       arg('[link]', 'Clear asset by link'),
       runners.gc(ipc)
     ),
-    command('corestore', summary('Clear platform corestore'), runners.gc(ipc)),
+    command('cores', summary('Clear corestore cores'), runners.gc(ipc)),
     flag('--json', 'Newline delimited JSON output'),
     () => {
       console.log(gc.help())
