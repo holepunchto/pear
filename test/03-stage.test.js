288d3c511779d5776113746b26bcbe40fabe2d0c
dmc <david.mark.clements@gmail.com>
2025-12-10 23:30:50 +0100

    tests organizing

--- test/03-stage.test.js
+++ test/03-stage.test.js
@@ -1,8 +1,33 @@
 'use strict'
+const fs = require('bare-fs')
 const test = require('brittle')
 const path = require('bare-path')
-const fs = require('bare-fs')
 const Helper = require('./helper')
+
+const stageAppMin = path.join(
+  Helper.localDir,
+  'test',
+  'fixtures',
+  'stage-app-min'
+)
+const stageAppMinWithOnly = path.join(
+  Helper.localDir,
+  'test',
+  'fixtures',
+  'stage-app-min-with-only'
+)
+const stageAppMinWithEntrypoints = path.join(
+  Helper.localDir,
+  'test',
+  'fixtures',
+  'stage-app-min-with-entrypoints'
+)
+const stagePearMain = path.join(
+  Helper.localDir,
+  'test',
+  'fixtures',
+  'stage-pear-main'
+)
 
 const warmup = path.join(Helper.localDir, 'test', 'fixtures', 'warmup')
 const prefetch = path.join(
@@ -36,84 +61,198 @@ const appWithUnignore = path.join(
   'app-with-unignore'
 )
 
-test('stage warmup with entrypoints', async function ({
-  ok,
-  is,
-  plan,
-  comment,
-  teardown,
-  timeout
-}) {
-  timeout(180000)
-  plan(4)
-
-  const dir = warmup
+test('basic stage min desktop app', async ({ teardown, ok, is, comment }) => {
+  const dir = stageAppMin
 
   const helper = new Helper()
   teardown(() => helper.close(), { order: Infinity })
   await helper.ready()
 
   const id = Helper.getRandomId()
-
-  comment('staging')
   const staging = helper.stage({
     channel: `test-${id}`,
     name: `test-${id}`,
     dir,
-    dryRun: false
+    dryRun: false,
+    compact: true
   })
   teardown(() => Helper.teardownStream(staging))
+
+  const stagedFiles = []
+  staging.on('data', async (data) => {
+    if (data?.tag === 'byte-diff') {
+      stagedFiles.push(data.data.message)
+    }
+  })
 
   const staged = await Helper.pick(staging, [
     { tag: 'warmed' },
     { tag: 'final' }
   ])
   const warmed = await staged.warmed
-  ok((await staged.final).success, 'stage succeeded')
-
   ok(warmed.blocks > 0, 'Warmup contains some blocks')
   ok(warmed.total > 0, 'Warmup total is correct')
   is(warmed.success, true, 'Warmup completed')
+
+  await staged.final
+
+  const expectedStagedFiles = [
+    '/package.json',
+    '/app.js',
+    '/index.js',
+    '/index.html',
+    '/node_modules/bare-events/package.json',
+    '/node_modules/bare-events/index.js',
+    '/node_modules/bare-events/lib/errors.js',
+    '/node_modules/ready-resource/package.json',
+    '/node_modules/ready-resource/index.js'
+  ]
+
+  comment('Only files in the dependency tree are staged')
+  ok(stagedFiles.length === expectedStagedFiles.length)
+  ok(stagedFiles.every((e) => expectedStagedFiles.includes(e)))
 })
 
-test('stage warmup with prefetch', async function ({
+test('basic stage min desktop app with entrypoints', async ({
+  teardown,
   ok,
   is,
-  plan,
-  comment,
-  teardown,
-  timeout
-}) {
-  timeout(180000)
-  plan(4)
-
-  const dir = prefetch
+  comment
+}) => {
+  const dir = stageAppMinWithEntrypoints
 
   const helper = new Helper()
   teardown(() => helper.close(), { order: Infinity })
   await helper.ready()
 
   const id = Helper.getRandomId()
-
-  comment('staging')
   const staging = helper.stage({
     channel: `test-${id}`,
     name: `test-${id}`,
     dir,
-    dryRun: false
+    dryRun: false,
+    compact: true
   })
   teardown(() => Helper.teardownStream(staging))
+
+  const stagedFiles = []
+  staging.on('data', async (data) => {
+    if (data?.tag === 'byte-diff') {
+      stagedFiles.push(data.data.message)
+    }
+  })
 
   const staged = await Helper.pick(staging, [
     { tag: 'warmed' },
     { tag: 'final' }
   ])
   const warmed = await staged.warmed
-  ok((await staged.final).success, 'stage succeeded')
-
   ok(warmed.blocks > 0, 'Warmup contains some blocks')
   ok(warmed.total > 0, 'Warmup total is correct')
   is(warmed.success, true, 'Warmup completed')
+  await staged.final
+
+  const expectedStagedFiles = [
+    '/package.json',
+    '/index.js',
+    '/entrypoint.js',
+    '/dep.js',
+    '/assets/file1.txt',
+    '/assets/file2.txt'
+  ]
+
+  comment('Only files in the dependency tree and assets are staged')
+  ok(stagedFiles.length === expectedStagedFiles.length)
+  ok(stagedFiles.every((e) => expectedStagedFiles.includes(e)))
+})
+
+test('basic stage min desktop app with only and include', async ({
+  teardown,
+  ok,
+  is,
+  comment
+}) => {
+  const dir = stageAppMinWithOnly
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const id = Helper.getRandomId()
+  const staging = helper.stage({
+    channel: `test-${id}`,
+    name: `test-${id}`,
+    dir,
+    dryRun: false,
+    compact: true
+  })
+  teardown(() => Helper.teardownStream(staging))
+
+  const stagedFiles = []
+  staging.on('data', async (data) => {
+    if (data?.tag === 'byte-diff') {
+      stagedFiles.push(data.data.message)
+    }
+  })
+
+  const staged = await Helper.pick(staging, [
+    { tag: 'warmed' },
+    { tag: 'final' }
+  ])
+  const warmed = await staged.warmed
+  ok(warmed.blocks > 0, 'Warmup contains some blocks')
+  ok(warmed.total > 0, 'Warmup total is correct')
+  is(warmed.success, true, 'Warmup completed')
+  await staged.final
+
+  const expectedStagedFiles = [
+    '/package.json',
+    '/index.js',
+    '/folder/foo.js',
+    '/folder/bar.js',
+    '/node_modules/ready-resource/package.json',
+    '/node_modules/ready-resource/index.js'
+  ]
+
+  comment('Only files in the dependency tree and pear.stage.only are staged')
+  ok(stagedFiles.length === expectedStagedFiles.length)
+  ok(stagedFiles.every((e) => expectedStagedFiles.includes(e)))
+})
+
+test.skip('stage pear.main file', async ({ teardown, ok, comment }) => {
+  const dir = stagePearMain
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const id = Helper.getRandomId()
+  const staging = helper.stage({
+    channel: `test-${id}`,
+    name: `test-${id}`,
+    dir,
+    dryRun: false,
+    compact: true
+  })
+  teardown(() => Helper.teardownStream(staging))
+
+  const stagedFiles = []
+  staging.on('data', async (data) => {
+    if (data?.tag === 'byte-diff') {
+      stagedFiles.push(data.data.message)
+    }
+  })
+
+  const staged = await Helper.pick(staging, [{ tag: 'final' }])
+  await staged.final
+
+  const expectedStagedFiles = ['/package.json', '/app.js']
+
+  console.log(expectedStagedFiles)
+
+  comment('Only files in the dependency tree are staged')
+  ok(stagedFiles.length === expectedStagedFiles.length)
+  ok(stagedFiles.every((e) => expectedStagedFiles.includes(e)))
 })
 
 test('stage with ignore', async function ({ ok, is, plan, teardown }) {
@@ -536,4 +675,84 @@ test('stage with purge config', async function ({ ok, is, comment, teardown }) {
     removedFiles.includes('/config-purge-file.js'),
     'config-purge-file.js should be purged'
   )
+})
+
+test('stage warmup with entrypoints', async function ({
+  ok,
+  is,
+  plan,
+  comment,
+  teardown,
+  timeout
+}) {
+  timeout(180000)
+  plan(4)
+
+  const dir = warmup
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const id = Helper.getRandomId()
+
+  comment('staging')
+  const staging = helper.stage({
+    channel: `test-${id}`,
+    name: `test-${id}`,
+    dir,
+    dryRun: false
+  })
+  teardown(() => Helper.teardownStream(staging))
+
+  const staged = await Helper.pick(staging, [
+    { tag: 'warmed' },
+    { tag: 'final' }
+  ])
+  const warmed = await staged.warmed
+  ok((await staged.final).success, 'stage succeeded')
+
+  ok(warmed.blocks > 0, 'Warmup contains some blocks')
+  ok(warmed.total > 0, 'Warmup total is correct')
+  is(warmed.success, true, 'Warmup completed')
+})
+
+test('stage warmup with prefetch', async function ({
+  ok,
+  is,
+  plan,
+  comment,
+  teardown,
+  timeout
+}) {
+  timeout(180000)
+  plan(4)
+
+  const dir = prefetch
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const id = Helper.getRandomId()
+
+  comment('staging')
+  const staging = helper.stage({
+    channel: `test-${id}`,
+    name: `test-${id}`,
+    dir,
+    dryRun: false
+  })
+  teardown(() => Helper.teardownStream(staging))
+
+  const staged = await Helper.pick(staging, [
+    { tag: 'warmed' },
+    { tag: 'final' }
+  ])
+  const warmed = await staged.warmed
+  ok((await staged.final).success, 'stage succeeded')
+
+  ok(warmed.blocks > 0, 'Warmup contains some blocks')
+  ok(warmed.total > 0, 'Warmup total is correct')
+  is(warmed.success, true, 'Warmup completed')
 })
