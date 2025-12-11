bdd9333832e800de6697b295ba70ddf5fb191130
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-12-11 23:12:22 +0800

    Replace usages of Pear.config with Pear.app

--- test/fixtures/worker-length/index.js
+++ test/fixtures/worker-length/index.js
@@ -1,5 +1,5 @@
 const pipe = Pear.worker.pipe()
-const subworker = Pear.worker.run(Pear.config.link + '/worker/index.js')
+const subworker = Pear.worker.run(Pear.app.link + '/worker/index.js')
 
 subworker.on('data', (data) => {
    pipe.write(data + '\n')
