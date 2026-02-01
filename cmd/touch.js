ae4ad39db733db3ceb64d00ab3ee2371f101efa7
davidmarkclements <david.mark.clements@gmail.com>
2026-01-31 23:37:42 -0600

    touch revamp

--- cmd/touch.js
+++ cmd/touch.js
@@ -1,11 +1,10 @@
 'use strict'
 const { outputter, ansi } = require('pear-terminal')
-const { randomBytes } = require('hypercore-crypto')
 const os = require('bare-os')
 
 const output = outputter('touch', {
-  result: ({ key }, { header, channel }) => {
-    return `${header}\n\n${ansi.bold(channel)}\n${key}\n`
+  result: ({ verlink, link, channel }) => {
+    return `\n${ansi.dim(`[ channel: ${channel} ]`)}\n\n${ansi.gray(ansi.dim(verlink))}\n\n[  ${link}  ]\n`
   },
   error: ({ message }) => {
     return `Error: ${message}\n`
@@ -16,9 +15,6 @@ module.exports = async function touch(cmd) {
   const ipc = global.Pear[global.Pear.constructor.IPC]
   const dir = os.cwd()
   const json = cmd.flags.json
-  const channel = cmd.args.channel || randomBytes(16).toString('hex')
-  await output(json, ipc.touch({ dir, channel }), {
-    channel,
-    header: cmd.command.header
-  })
+  const channel = cmd.args.channel
+  await output(json, ipc.touch({ dir, channel }))
 }
--- subsystems/sidecar/ops/touch.js
+++ subsystems/sidecar/ops/touch.js
@@ -1,6 +1,8 @@
 'use strict'
 const path = require('bare-path')
 const hypercoreid = require('hypercore-id-encoding')
+const { randomBytes } = require('hypercore-crypto')
+const plink = require('pear-link')
 const Hyperdrive = require('hyperdrive')
 const { ERR_INVALID_PROJECT_DIR } = require('pear-errors')
 const Opstream = require('../lib/opstream')
@@ -11,20 +13,41 @@ module.exports = class Touch extends Opstream {
     super((...args) => this.#op(...args), ...args)
   }
 
-  async #op({ dir, channel }) {
+  async #op({ dir, channel = randomBytes(16).toString('hex') }) {
     const { sidecar } = this
     await sidecar.ready()
-    const pkg = await State.localPkg({ dir })
-    if (pkg === null)
-      throw ERR_INVALID_PROJECT_DIR(
-        `"${path.join(dir, 'package.json')}" not found. Pear project must have a package.json`
-      )
-    const corestore = sidecar.getCorestore(State.appname(pkg), channel)
+
+    let name = ''
+    try {
+      const pkg = await State.localPkg({ dir })
+      if (pkg !== null) {
+        name = State.appname(pkg)
+      }
+    } catch {
+      // ignore
+    }
+
+    const corestore = sidecar.getCorestore(name, channel)
     await corestore.ready()
     const key = await Hyperdrive.getDriveKey(corestore)
+    const drive = new Hyperdrive(corestore, key)
+    await drive.ready()
+    const { length, fork } = drive.core
+    const verlink = plink.serialize({
+      protocol: 'pear:',
+      drive: { key, fork, length }
+    })
+    const link = plink.serialize({ protocol: 'pear:', drive: { key } })
     this.push({
       tag: 'result',
-      data: { key: hypercoreid.normalize(key), channel }
+      data: {
+        key: hypercoreid.normalize(key),
+        length,
+        fork,
+        link,
+        verlink,
+        channel
+      }
     })
   }
 }
