44348d54c596671302868002b5fc38989da28f4b
Eric Bauerfeld <eric@dideric.is>
2026-02-26 13:19:04 -0500

    Add stats to pear seed output (#1021)
    * pear seed output v2
    
    * remove verbose flag
    
    * handle non tty output
    
    * simplify stats push on exit
    
    * rename table vars
    
    * rename push to row
    
    * refactor pushStats method
    
    * make stats an instance variable
    
    * add layout class and append mode
    
    * clean up --append description
    
    * remove console logs
    
    * rename --append to --no-tty
    
    * remove console.log
    
    * clear stats interval after all seed notices

--- cmd/index.js
+++ cmd/index.js
@@ -131,8 +131,8 @@ module.exports = async (ipc, argv = Bare.argv.slice(1)) => {
     `,
     arg('<link>', 'Pear link to seed'),
     arg('[dir]', 'Project directory path (default: .)'),
-    flag('--verbose|-v', 'Additional output'),
     flag('--name <name>', 'Advanced. Override app name'),
+    flag('--no-tty', 'Disable tty features'),
     flag('--no-ask', 'Suppress permission prompt'),
     flag('--json', 'Newline delimited JSON output'),
     commands.seed
