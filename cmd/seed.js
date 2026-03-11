38f702963064ca0391e098a9822815fc15c4c9d6
Andrei Regiani <andrei.cpp@gmail.com>
2026-03-11 09:37:01 -0300

    log style

--- cmd/seed.js
+++ cmd/seed.js
@@ -432,7 +432,7 @@ module.exports = async function seed(cmd) {
       download
     }) {
       const network = appendMode
-        ? `${peers} peers / uploaded ${byteSize(upload.totalBytes)} @ ${byteSize(upload.speed)}/s / downloaded ${byteSize(download.totalBytes)} @ ${byteSize(download.speed)}/s`
+        ? `${peers} peers: upload ${byteSize(upload.totalBytes)} @ ${byteSize(upload.speed)}/s: download ${byteSize(download.totalBytes)} @ ${byteSize(download.speed)}/s`
         : `[ Peers: ${peers} ] [ ${ansi.up} ${byteSize(upload.totalBytes)} - ${byteSize(upload.speed)}/s ] [ ${ansi.down} ${byteSize(download.totalBytes)} - ${byteSize(download.speed)}/s ]`
 
       stats.update({
