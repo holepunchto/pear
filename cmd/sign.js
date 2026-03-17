e6f2462c0b143edaa39b8146b9ca9d0e502bfad6
davidmarkclements <david.mark.clements@gmail.com>
2026-03-18 00:53:43 +0100

    align with hypercore-sign, add pw capture

--- cmd/sign.js
+++ cmd/sign.js
@@ -1,13 +1,17 @@
 'use strict'
-const { outputter } = require('pear-terminal')
-const hs = require('hypercore-sign')
+const fs = require('bare-fs')
+const path = require('bare-path')
+const { outputter, password } = require('pear-terminal')
+const { PLATFORM_DIR } = require('pear-constants')
+
 const output = outputter('sign', {})
 
 module.exports = async function sign(cmd) {
   const { json } = cmd.flags
   const { request } = cmd.args
-  await output(
-    json,
-    hs.sign(request) // TODO - hypercore-sign needs sign, and to be a stream/async gen
-  )
+  const key = fs.readSync(path.join(PLATFORM_DIR, 'sign', 'default'))
+  const pwd = await password()
+  const signables = request // TODO: why isn't sign function just supporting request ?
+  const signatures = sign(signables, key, pwd)
+  await output(json, [{ tag: 'final', data: { signatures } }])
 }
