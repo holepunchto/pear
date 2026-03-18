975213f4822e790876f975470ae8ae75a2edeebb
davidmarkclements <david.mark.clements@gmail.com>
2026-03-18 21:53:35 +0100

    ordering, pruning

--- test/02-stage.test.js
+++ test/02-stage.test.js
@@ -2,7 +2,82 @@
 const test = require('brittle')
 const tmp = require('test-tmp')
 const Localdrive = require('localdrive')
+const hypercoreid = require('hypercore-id-encoding')
 const Helper = require('./helper')
+
+test('app with assets', async function ({ ok, plan, comment, teardown, timeout }) {
+  timeout(180000)
+  plan(3)
+
+  const dir = Helper.fixture('require-assets')
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+  const link = await Helper.touchLink(helper)
+
+  comment('staging')
+  const staging = helper.stage({
+    link,
+    dir,
+    dryRun: false
+  })
+  teardown(() => Helper.teardownStream(staging))
+  const staged = await Helper.pick(staging, { tag: 'final' })
+  ok(staged.success, 'stage succeeded')
+
+  comment('seeding')
+  const seeding = helper.seed({
+    link,
+    dir,
+    key: null,
+    cmdArgs: []
+  })
+  teardown(() => Helper.teardownStream(seeding))
+  const until = await Helper.pick(seeding, [{ tag: 'key' }, { tag: 'announced' }])
+  const announced = await until.announced
+  ok(announced, 'seeding is announced')
+
+  const key = await until.key
+  ok(hypercoreid.isValid(key), 'app key is valid')
+})
+
+test('app with assets in sub dep', async function ({ ok, plan, comment, teardown, timeout }) {
+  timeout(180000)
+  plan(3)
+
+  const dir = Helper.fixture('sub-dep-require-assets')
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+  const link = await Helper.touchLink(helper)
+
+  comment('staging')
+  const staging = helper.stage({
+    link,
+    dir,
+    dryRun: false
+  })
+  teardown(() => Helper.teardownStream(staging))
+  const staged = await Helper.pick(staging, { tag: 'final' })
+  ok(staged.success, 'stage succeeded')
+
+  comment('seeding')
+  const seeding = helper.seed({
+    link,
+    dir,
+    key: null,
+    cmdArgs: []
+  })
+  teardown(() => Helper.teardownStream(seeding))
+  const until = await Helper.pick(seeding, [{ tag: 'key' }, { tag: 'announced' }])
+  const announced = await until.announced
+  ok(announced, 'seeding is announced')
+
+  const key = await until.key
+  ok(hypercoreid.isValid(key), 'app key is valid')
+})
 
 test('pear stage min desktop app', async ({ teardown, ok, is, comment }) => {
   const dir = Helper.fixture('stage-app-min')
--- test/03-seed.test.js
+++ test/03-seed.test.js
@@ -1,9 +1,13 @@
 'use strict'
 const test = require('brittle')
+const tmp = require('test-tmp')
+const Corestore = require('corestore')
+const Hyperdrive = require('hyperdrive')
+const Hyperswarm = require('hyperswarm')
 const hypercoreid = require('hypercore-id-encoding')
 const Helper = require('./helper')
 
-test('smoke', async function ({ ok, plan, comment, teardown, timeout }) {
+test('pear seed basic stage and seed', async function ({ ok, plan, comment, teardown, timeout }) {
   timeout(180000)
   plan(3)
 
@@ -40,12 +44,11 @@ test('smoke', async function ({ ok, plan, comment, teardown, timeout }) {
   ok(hypercoreid.isValid(key), 'app key is valid')
 })
 
-test('app with assets', async function ({ ok, plan, comment, teardown, timeout }) {
+test('pear seed announces, join, drop', async function ({ ok, plan, comment, teardown, timeout }) {
   timeout(180000)
   plan(3)
 
-  const dir = Helper.fixture('require-assets')
-
+  const dir = Helper.fixture('minimal')
   const helper = new Helper()
   teardown(() => helper.close(), { order: Infinity })
   await helper.ready()
@@ -58,8 +61,7 @@ test('app with assets', async function ({ ok, plan, comment, teardown, timeout }
     dryRun: false
   })
   teardown(() => Helper.teardownStream(staging))
-  const staged = await Helper.pick(staging, { tag: 'final' })
-  ok(staged.success, 'stage succeeded')
+  await Helper.pick(staging, { tag: 'final' })
 
   comment('seeding')
   const seeding = helper.seed({
@@ -69,47 +71,35 @@ test('app with assets', async function ({ ok, plan, comment, teardown, timeout }
     cmdArgs: []
   })
   teardown(() => Helper.teardownStream(seeding))
-  const until = await Helper.pick(seeding, [{ tag: 'key' }, { tag: 'announced' }])
+  const until = await Helper.pick(seeding, [
+    { tag: 'key' },
+    { tag: 'announced' },
+    { tag: 'peer-add' },
+    { tag: 'peer-remove' }
+  ])
   const announced = await until.announced
   ok(announced, 'seeding is announced')
-
   const key = await until.key
-  ok(hypercoreid.isValid(key), 'app key is valid')
-})
 
-test('app with assets in sub dep', async function ({ ok, plan, comment, teardown, timeout }) {
-  timeout(180000)
-  plan(3)
+  const peerStore = new Corestore(await tmp())
+  teardown(() => peerStore.close())
+  await peerStore.ready()
+  const peerDrive = new Hyperdrive(peerStore, key)
+  await peerDrive.ready()
 
-  const dir = Helper.fixture('sub-dep-require-assets')
-
-  const helper = new Helper()
-  teardown(() => helper.close(), { order: Infinity })
-  await helper.ready()
-  const link = await Helper.touchLink(helper)
-
-  comment('staging')
-  const staging = helper.stage({
-    link,
-    dir,
-    dryRun: false
+  const peerSwarm = new Hyperswarm({ bootstrap: Helper.dhtBootstrap })
+  teardown(() => peerSwarm.destroy())
+  peerSwarm.on('connection', (conn) => {
+    peerDrive.corestore.replicate(conn)
   })
-  teardown(() => Helper.teardownStream(staging))
-  const staged = await Helper.pick(staging, { tag: 'final' })
-  ok(staged.success, 'stage succeeded')
+  peerSwarm.join(peerDrive.discoveryKey)
+  await peerDrive.get('/package.json')
 
-  comment('seeding')
-  const seeding = helper.seed({
-    link,
-    dir,
-    key: null,
-    cmdArgs: []
-  })
-  teardown(() => Helper.teardownStream(seeding))
-  const until = await Helper.pick(seeding, [{ tag: 'key' }, { tag: 'announced' }])
-  const announced = await until.announced
-  ok(announced, 'seeding is announced')
+  const joined = await until['peer-add']
+  ok(joined, 'peer joins')
 
-  const key = await until.key
-  ok(hypercoreid.isValid(key), 'app key is valid')
+  await peerSwarm.destroy()
+
+  const dropped = await until['peer-remove']
+  ok(dropped, 'peer drops')
 })
