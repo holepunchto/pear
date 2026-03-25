0eaf5338a7465f1f1dac3ff5ee6849f1559df700
Andrei Regiani <andrei.cpp@gmail.com>
2026-03-25 15:40:34 -0300

    ModelNext

---
+++ scripts/build-hyperdb.js
@@ -0,0 +1,54 @@
+/* SCHEMA SAFETY RULES
+
+1. Schema fields are append-only: once a new field is added, it cannot be removed (comment as deprecated instead)
+2. Deleting/resetting files in `./spec` directory is forbidden
+3. After making changes: `npm run hyperdb:build`. Once merged into the main branch, there is no undoing.
+*/
+const path = require('path')
+const Hyperschema = require('hyperschema')
+const Builder = require('hyperdb/builder')
+
+const SCHEMA_DIR = path.join(__dirname, 'spec', 'schema')
+const DB_DIR = path.join(__dirname, 'spec', 'db')
+
+const schema = Hyperschema.from(SCHEMA_DIR, { versioned: false })
+const pearSchema = schema.namespace('pear')
+
+pearSchema.register({
+  name: 'node',
+  fields: [
+    {
+      name: 'host',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'port',
+      type: 'uint',
+      required: true
+    }
+  ]
+})
+
+pearSchema.register({
+  name: 'dht',
+  fields: [
+    {
+      name: 'nodes',
+      type: '@pear/node',
+      array: true
+    }
+  ]
+})
+
+Hyperschema.toDisk(schema)
+
+const db = Builder.from(SCHEMA_DIR, DB_DIR)
+const pearDB = db.namespace('pear')
+
+pearDB.collections.register({
+  name: 'dht',
+  schema: '@pear/dht'
+})
+
+Builder.toDisk(db)
---
+++ subsystems/sidecar/lib/db.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const DBLock = require('db-lock')
+
+if (global.LOG === undefined) global.LOG = { trace() {} }
+const LOG = global.LOG
+
+class Lock extends DBLock {
+  #manual = false
+  #tx = null
+  constructor(db) {
+    super({
+      enter: () => {
+        this.#tx = db.transaction()
+        return this.#tx
+      },
+      exit: (tx) => tx.flush(),
+      maxParallel: 1
+    })
+  }
+
+  enter() {
+    if (this.#manual && this.#tx !== null) return this.#tx
+    return super.enter()
+  }
+
+  exit() {
+    if (this.#manual) return Promise.resolve()
+    return super.exit()
+  }
+
+  manual() {
+    this.#manual = true
+    return async () => {
+      try {
+        await super.exit()
+      } finally {
+        this.#manual = false
+      }
+    }
+  }
+}
+
+class Model {
+  constructor(rocks) {
+    this.db = rocks
+    this.lock = new Lock(this.db)
+  }
+
+  async getDhtNodes() {
+    LOG.trace('db', 'GET', '@pear/dht', '[nodes]')
+    return (await this.db.get('@pear/dht'))?.nodes || []
+  }
+
+  async setDhtNodes(nodes) {
+    const tx = await this.lock.enter()
+    const insert = { nodes }
+    LOG.trace('db', 'INSERT', '@pear/dht', insert)
+    await tx.insert('@pear/dht', insert)
+    await this.lock.exit()
+  }
+
+  async close() {
+    LOG.trace('db', 'CLOSE')
+    await this.db.close()
+  }
+}
+
+exports.spec = require('../../../spec/db')
+exports.Model = Model
