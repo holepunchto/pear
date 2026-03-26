10f61a53966dd2d65a0d8141784f23374ace000c
Andrei Regiani <andrei.cpp@gmail.com>
2026-03-26 10:11:36 -0300

    Fresh HyperDB (#1045)
    * pear sign, pear keygen, pear multisig
    
    * align with hypercore-sign, add pw capture
    
    * full flow
    
    * tests + fixes
    
    * bump pear-ipc
    
    * lint fixes
    
    * rm unnecessaries
    
    * sign and keygen belong to multisig, usage output improvements
    
    * test fix
    
    * tweaks
    
    * rearrange, explicit verlink terms
    
    * keygen -> keys
    
    * fixes, usage improvements
    
    * rm dupe
    
    * use param dedent via tagged fn
    
    * quorom value needed
    
    * ModelNext
    
    * modelNext
    
    * new spec/ generated
    
    * npm run hyperdb:build
    
    * lint
    
    * add model test
    
    * DHT on modelNext
    
    * db.js clean up
    
    * tests clean up
    
    * modelNext -> db.model
    
    * refine
    
    * Apply suggestion from @davidmarkclements
    
    ---------
    
    Co-authored-by: davidmarkclements <david.mark.clements@gmail.com>

---
+++ subsystems/sidecar/lib/db.js
@@ -0,0 +1,71 @@
+'use strict'
+const DBLock = require('db-lock')
+
+class Lock extends DBLock {
+  #manual = false
+  #tx = null
+  constructor(db) {
+    super({
+      enter: () => {
+        this.#tx = db.transaction()
+        return this.#tx
+      },
+      exit: (tx) => tx.flush(),
+      maxParallel: 1
+    })
+  }
+
+  enter() {
+    if (this.#manual && this.#tx !== null) return this.#tx
+    return super.enter()
+  }
+
+  exit() {
+    if (this.#manual) return Promise.resolve()
+    return super.exit()
+  }
+
+  manual() {
+    this.#manual = true
+    return async () => {
+      try {
+        await super.exit()
+      } finally {
+        this.#manual = false
+      }
+    }
+  }
+}
+
+class Model {
+  constructor(hyperdb) {
+    this.hyperdb = hyperdb
+    this.lock = new Lock(this.hyperdb)
+  }
+
+  async ready() {
+    LOG.trace('db', 'READY')
+    await this.hyperdb.ready()
+  }
+
+  async getDhtNodes() {
+    LOG.trace('db', 'GET', '@pear/dht', '[nodes]')
+    return (await this.hyperdb.get('@pear/dht'))?.nodes || []
+  }
+
+  async setDhtNodes(nodes) {
+    const tx = await this.lock.enter()
+    const insert = { nodes }
+    LOG.trace('db', 'INSERT', '@pear/dht', insert)
+    await tx.insert('@pear/dht', insert)
+    await this.lock.exit()
+  }
+
+  async close() {
+    LOG.trace('db', 'CLOSE')
+    await this.hyperdb.close()
+  }
+}
+
+exports.spec = require('../../../spec/db')
+exports.Model = Model
