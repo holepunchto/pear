b832934d43da655ac1a2ccffe12bffa3dde160fe
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-04-23 02:38:06 +0800

    Configure jobs in test/index.mjs instead

--- test/index.mjs
+++ test/index.mjs
@@ -5,6 +5,8 @@ runTests()
 async function runTests() {
   const test = (await import('brittle')).default
 
+  test.configure({ jobs: 4 })
+
   test.pause()
 
   await test.load(import.meta.resolve('./00-inspect.test.js'))
