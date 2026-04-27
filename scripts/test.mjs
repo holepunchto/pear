183f1af1e02fcb6d0aec499dbdcfa6dae35bcc3f
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-04-27 14:19:15 +0800

    Test with 1 thread

--- scripts/test.mjs
+++ scripts/test.mjs
@@ -47,7 +47,7 @@ spawnSync(RUNTIME, ['sidecar', 'shutdown'], { stdio: 'inherit' })
 
 const tests = spawn(
   isWindows ? 'npx.cmd' : 'npx',
-  ['brittle-bare', '-j', '4', path.join('test', 'index.mjs'), ...Bare.argv.slice(2)],
+  ['brittle-bare', '-j', '1', path.join('test', 'index.mjs'), ...Bare.argv.slice(2)],
   {
     cwd: root,
     stdio: 'inherit',
