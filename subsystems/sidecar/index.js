2a65b4bd4bbe8ae76624d3ea5a51432f1174ee71
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-05-12 14:18:44 +0800

    Integrate runtime updater with old update systems

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -13,7 +13,6 @@ const hypercoreid = require('hypercore-id-encoding')
 const Iambus = require('iambus')
 const safetyCatch = require('safety-catch')
 const sodium = require('sodium-native')
-const Updater = require('pear-updater')
 const PearRuntimeUpdater = require('pear-runtime-updater')
 const IPC = require('pear-ipc')
 const { isMac, isWindows } = require('which-runtime')
@@ -41,7 +40,6 @@ const HyperDB = require('hyperdb')
 const hyperdb = require('./lib/model')
 const db = require('./lib/db')
 const registerUrlHandler = require('../../url-handler')
-const { version, productName, upgrade } = require('../../package.json')
 const ops = {
   GC: require('./ops/gc'),
   Stage: require('./ops/stage'),
@@ -64,14 +62,12 @@ const CUTOVER_DELAY = 20_000
 const CHECKMARK = isWindows ? '^' : '✔'
 
 class Sidecar extends ReadyResource {
-  static Updater = Updater
+  static Updater = PearRuntimeUpdater
 
   spindownt = null
   spindownms = SPINDOWN_TIMEOUT
   decomissioned = false
   swarm = null
-  runtimeUpdater = null
-  runtimeUpdaterSwarm = null
   keyPair = null
   discovery = null
 
@@ -79,7 +75,7 @@ class Sidecar extends ReadyResource {
     global.Bare.exit()
   }
 
-  constructor({ updater, drive, corestore, nodes }) {
+  constructor({ updater, corestore, nodes }) {
     super()
 
     const rocks = HyperDB.rocks(corestore.storage.rocks.session(), hyperdb.spec)
@@ -109,23 +105,10 @@ class Sidecar extends ReadyResource {
     this.version = CHECKOUT
 
     this.updater = updater
-    if (this.updater) {
-      this.updater.on('updating', (checkout) => {
-        const { key, length } = checkout
-        LOG.info(
-          'sidecar',
-          key === this.version.key
-            ? `- Updating to length ${length}...`
-            : `- Switching to key ${key} with length ${length}...`
-        )
-        this.updatingNotify(checkout)
-      })
-      this.updater.on('update', (checkout) => this.updateNotify(checkout))
-    }
+    if (this.updater) this.#bindUpdaterEvents()
 
     this.#spindownCountdown()
 
-    this.drive = drive
     this.corestore = corestore
     this.nodes = nodes
     this.ipc = new IPC.Server({
@@ -377,9 +360,6 @@ class Sidecar extends ReadyResource {
     await this.model.db.ready()
     await this.db.model.ready()
     await this.#ensureSwarm()
-    this.#startRuntimeUpdater().catch((err) => {
-      LOG.error('sidecar', 'Runtime updater start failed', err)
-    })
     LOG.info('sidecar', '- Sidecar Booted')
     const gcCycle = async () => {
       await this.model.gc()
@@ -930,7 +910,6 @@ class Sidecar extends ReadyResource {
   }
 
   async #close() {
-    await this.#closeRuntimeUpdater()
     await this.applings.close()
     clearInterval(this.gcInterval)
     clearTimeout(this.lazySwarmTimeout)
@@ -952,87 +931,24 @@ class Sidecar extends ReadyResource {
     LOG.info('sidecar', CHECKMARK + ' Sidecar Closed')
   }
 
-  async #startRuntimeUpdater() {
-    if (!upgrade) return
-
-    const app = global.Bare?.argv?.[0]
-    if (!app) return
-
-    const name = path.basename(app) || productName || 'pear'
-    const updater = new PearRuntimeUpdater({
-      dir: PLATFORM_DIR,
-      store: this.corestore,
-      version,
-      upgrade,
-      app,
-      name
-    })
-    this.runtimeUpdater = updater
-    this.#bindRuntimeUpdaterEvents(updater)
-
-    await updater.ready()
-
-    const keyPair = await this.corestore.createKeyPair('pear-runtime-updater')
-
-    const swarm = new Hyperswarm({
-      keyPair,
-      bootstrap: this.nodes
-    })
-    this.runtimeUpdaterSwarm = swarm
-    swarm.on('connection', (connection) => this.corestore.replicate(connection))
-    swarm.on('error', (err) => LOG.error('sidecar', 'Runtime updater swarm error', err))
-
-    const topic = swarm.join(updater.drive.core.discoveryKey, {
-      client: true,
-      server: false
-    })
-    topic.flushed().catch((err) => LOG.error('sidecar', 'Runtime updater swarm join failed', err))
-  }
-
-  #bindRuntimeUpdaterEvents(updater) {
+  #bindUpdaterEvents(updater) {
     updater.on('updating', () => {
-      LOG.info('sidecar', 'Runtime updater: updating')
+      const key = updater.key
+      const length = updater.drive.core.length
+      LOG.info(
+        'sidecar',
+        key === version.key
+          ? `- Updating to length ${length}...`
+          : `- Switching to key ${key} with length ${length}...`
+      )
+      this.updatingNotify({ key, length })
     })
 
-    updater.on('updating-delta', (data) => {
-      LOG.info('sidecar', 'Runtime updater: updating-delta', data)
-    })
-
-    updater.on('updated', async () => {
-      LOG.info('sidecar', 'Runtime updater: updated')
-      try {
-        await updater.applyUpdate()
-        LOG.info('sidecar', 'Runtime updater: applyUpdate complete')
-      } catch (err) {
-        LOG.error('sidecar', 'Runtime updater: applyUpdate failed', err)
-      }
-    })
+    updater.on('updated', () => this.updateNotify({ key: updater.key, length: updater.length }))
 
     updater.on('error', (err) => {
-      LOG.error('sidecar', 'Runtime updater error', err)
+      LOG.error('sidecar', 'Updater error', err)
     })
-  }
-
-  async #closeRuntimeUpdater() {
-    if (this.runtimeUpdaterSwarm) {
-      try {
-        await this.runtimeUpdaterSwarm.destroy()
-      } catch (err) {
-        LOG.error('sidecar', 'Runtime updater close: swarm destroy failed', err)
-      } finally {
-        this.runtimeUpdaterSwarm = null
-      }
-    }
-
-    if (this.runtimeUpdater) {
-      try {
-        await this.runtimeUpdater.close()
-      } catch (err) {
-        LOG.error('sidecar', 'Runtime updater close: updater close failed', err)
-      } finally {
-        this.runtimeUpdater = null
-      }
-    }
   }
 
   async _close() {
@@ -1051,9 +967,9 @@ class Sidecar extends ReadyResource {
     await this.ipc.close()
 
     if (this.updater) {
-      if ((await this.updater.applyUpdate()) !== null) {
-        LOG.info('sidecar', CHECKMARK + ' Applied update')
-      }
+      await this.updater.applyUpdate()
+      LOG.info('sidecar', CHECKMARK + ' Applied update')
+      await this.updater.close()
     }
     this.bus.destroy()
   }
@@ -1063,9 +979,8 @@ class Sidecar extends ReadyResource {
     this.bailout = setTimeout(async () => {
       if (this.updater) {
         try {
-          if ((await this.updater.applyUpdate()) !== null) {
-            LOG.info('sidecar', LOG.CHECKMARK + ' Applied update')
-          }
+          await this.updater.applyUpdate()
+          LOG.info('sidecar', LOG.CHECKMARK + ' Applied update')
         } catch (err) {
           LOG.error('sidecar', err)
         }
