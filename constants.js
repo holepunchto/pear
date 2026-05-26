49fa3b2f6b64ca2731cca15ba0c27ea6ad081f5c
rafapaezbas <rafa@holepunch.to>
2026-05-26 20:06:59 +0200

    cleanup constants and added upgrade link to crasher

--- constants.js
+++ constants.js
@@ -6,7 +6,7 @@ const path = require('bare-path')
 const os = require('bare-os')
 const process = require('bare-process')
 const b4a = require('b4a')
-const { ALIASES, EOLS } = require('pear-aliases')
+const pkg = require('./package.json')
 
 const BIN = 'by-arch/' + platform + '-' + arch + '/bin/'
 const LOCALDEV = process.execPath
@@ -21,16 +21,14 @@ const PLATFORM_LOCK = toPath(new URL('pear.lock', PLATFORM_URL))
 const RUNTIME_EXEC = isWindows ? 'pear-runtime.exe' : 'pear-runtime'
 
 exports.LOCALDEV = LOCALDEV
-exports.ALIASES = ALIASES
-exports.EOLS = EOLS
+
+exports.UPGRADE = pkg.upgrade
 
 exports.PLATFORM_DIR = PLATFORM_DIR
 exports.PLATFORM_LOCK = PLATFORM_LOCK
 exports.PLATFORM_HYPERDB = toPath(new URL('db', PLATFORM_URL))
 exports.GC = toPath(new URL('gc', PLATFORM_URL))
 exports.PLATFORM_CORESTORE = toPath(new URL('corestores/platform', PLATFORM_URL))
-exports.UPGRADE_LOCK = toPath(new URL('lock', PLATFORM_URL))
-exports.APPLINGS_PATH = toPath(new URL('applings', PLATFORM_URL))
 exports.SOCKET_PATH = isWindows
   ? `\\\\.\\pipe\\${IPC_ID}-${pipeId(PLATFORM_DIR)}`
   : `${PLATFORM_DIR}/${IPC_ID}.sock`
@@ -41,8 +39,6 @@ exports.IDLE_TIMEOUT = 30_000
 exports.SPINDOWN_TIMEOUT = 60_000
 
 exports.RUNTIME = toPath(new URL(BIN + RUNTIME_EXEC, PLATFORM_URL))
-
-exports.SALT = b4a.from('d134aa8b0631f1193b5031b356d82dbea214389208fa4a0bcdf5c2e062d8ced2', 'hex')
 
 exports.KNOWN_NODES_LIMIT = 100
 
