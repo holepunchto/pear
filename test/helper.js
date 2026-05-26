5380a6a555480acc4127127e0168ca6d92333cd2
rafapaezbas <rafa@holepunch.to>
2026-05-26 19:24:18 +0200

    lint fix

--- test/helper.js
+++ test/helper.js
@@ -67,7 +67,9 @@ class Helper extends IPC.Client {
       return b4a.toString(buf, 'hex')
     }
 
-    const local = opts.localDir ? path.join(opts.localDir, 'pear') : path.join(path.dirname(__dirname), 'pear')
+    const local = opts.localDir
+      ? path.join(opts.localDir, 'pear')
+      : path.join(path.dirname(__dirname), 'pear')
     const lock = path.join(local, 'pear.lock')
     const socketPath = isWindows ? `\\\\.\\pipe\\pear-${pipeId(platformDir)}` : `${local}/pear.sock`
     const connectTimeout = 20_000
