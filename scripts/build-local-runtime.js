64a95a8e7e158f81326feff08d106a3de9a402bd
Geordan Gesink <165498764+geordangesink@users.noreply.github.com>
2026-05-27 16:38:16 +0200

    add win32 builds and ps1 symlink (#1095)
    * add win32 builds and ps1 symlink
    
    * restore upgrade link

--- scripts/build-local-runtime.js
+++ scripts/build-local-runtime.js
@@ -17,5 +17,13 @@ if (!supported.has(host)) {
 }
 
 const npmCmd = os.platform() === 'win32' ? 'npm.cmd' : 'npm'
-const res = spawnSync(npmCmd, ['run', script], { cwd: root, stdio: 'inherit' })
+const res = spawnSync(npmCmd, ['run', script], {
+  cwd: root,
+  stdio: 'inherit',
+  shell: os.platform() === 'win32'
+})
+if (res.error) {
+  console.error(res.error.message)
+  process.exit(1)
+}
 if (res.status !== 0) process.exit(res.status || 1)
