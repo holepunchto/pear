a496089d11ed16e400ad59a225b360abe604c387
unknown <georgegeordan@gmail.com>
2026-05-27 19:12:46 +0200

    adding linux arm and windows arm to CI

--- scripts/link-win32-dev.js
+++ scripts/link-win32-dev.js
@@ -3,9 +3,10 @@
 
 const fs = require('fs')
 const path = require('path')
+const { arch } = require('which-runtime')
 
 const root = path.resolve(__dirname, '..')
-const runtimeRel = path.join('by-arch', 'win32-x64', 'bin', 'pear.exe')
+const runtimeRel = path.join('by-arch', `win32-${arch}`, 'bin', 'pear.exe')
 const runtime = path.join(root, runtimeRel)
 const ps1 = path.join(root, 'pear.ps1')
 
