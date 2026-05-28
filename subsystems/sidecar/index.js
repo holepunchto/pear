bb3c5c3f8fb23418cbd7ab898a751551956b8067
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-05-28 17:49:20 +0800

    Cleanup version.force and TODOs

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -123,16 +123,9 @@ class Sidecar extends ReadyResource {
   }
 
   updateNotify(version) {
-    // TODO: @keith either fix or cleanup force
-    if (version.force) {
-      LOG.info('sidecar', 'Platform Force update (' + version.force.reason + '). Updating to:')
-    } else {
-      LOG.info('sidecar', 'Platform update available. Restart to update to:')
-    }
-
+    LOG.info('sidecar', 'Platform update available. Restart to update to:')
     LOG.info('sidecar', ' ' + plink.serialize({ drive: version }))
 
-    // TODO: @keith should spindown 0 only happen when updating to new key?
     this.spindownms = 0
     this.#spindownCountdown()
   }
