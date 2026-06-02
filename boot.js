0448bf3c94d7f464d9fb5b8559426b8f2bdc44da
rafapaezbas <rafa@holepunch.to>
2026-06-02 14:30:03 +0200

    refactor boot.js

--- boot.js
+++ boot.js
@@ -3,22 +3,22 @@
 const fs = require('bare-fs')
 const os = require('bare-os')
 const { isWindows } = require('which-runtime')
-const CONSTANTS = require('./constants.js')
+const { PLATFORM_DIR } = require('./constants.js')
 const Logger = require('./lib/logger.js')
 const { cmdArgs } = require('./argv')
 
 global.Pear = { config: {} } // TODO remove after moving pear-ref
 
-if (fs.existsSync(CONSTANTS.PLATFORM_DIR) === false) {
-  fs.mkdirSync(CONSTANTS.PLATFORM_DIR, { recursive: true })
+if (fs.existsSync(PLATFORM_DIR) === false) {
+  fs.mkdirSync(PLATFORM_DIR, { recursive: true })
 }
 
 if (isWindows === false) {
-  const stat = fs.statSync(CONSTANTS.PLATFORM_DIR)
+  const stat = fs.statSync(PLATFORM_DIR)
   const user = os.userInfo()
 
   if (stat.uid !== user.uid) {
-    const err = new Error(`Current user does not own ${CONSTANTS.PLATFORM_DIR}`)
+    const err = new Error(`Current user does not own ${PLATFORM_DIR}`)
     err.name = 'User Permissions Error'
     throw err
   }
