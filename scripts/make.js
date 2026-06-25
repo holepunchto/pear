6e208b9e6e8d2f5810d59837fbe7a7fea086b644
rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>
2026-06-25 17:37:45 +0200

    Support env vars in make (#1122)
    * support environment variable to set channel
    
    * setup dev and stage keys
    
    * Update scripts/make.js
    
    Co-authored-by: Jan Keith Darunday <jkcdarunday@gmail.com>
    
    ---------
    
    Co-authored-by: rafapaezbas <rafael.paez@tether.io>
    Co-authored-by: Jan Keith Darunday <jkcdarunday@gmail.com>

--- scripts/make.js
+++ scripts/make.js
@@ -1,8 +1,13 @@
 const { spawn } = require('bare-subprocess')
 const { platform, arch } = require('which-runtime')
+const env = require('bare-env')
 
-const channel = global.Bare.argv[2]
+const channel = env.CHANNEL && env.CHANNEL.length > 0 ? env.CHANNEL : global.Bare.argv[2]
 const host = `${platform}-${arch}`
+
+if (!['dev', 'stage', 'production'].includes(channel)) {
+  throw new Error(`Channel ${channel} not supported`)
+}
 
 const child = spawn(
   'bare-build',
