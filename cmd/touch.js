34a328fcf2dc16a89fd36767d8ee004ce3da56f4
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-07-09 02:49:11 +0800

    Add validation for vanity

--- cmd/touch.js
+++ cmd/touch.js
@@ -2,6 +2,7 @@
 const context = require('../context')
 const { outputter } = require('../lib/terminal.js')
 const findVanityKey = require('../lib/vanity.js')
+const z32 = require('z32')
 
 const output = outputter('touch', {
   final: ({ link }) => {
@@ -18,8 +19,21 @@ const output = outputter('touch', {
 
 module.exports = async function touch(cmd) {
   const vanity = cmd.flags.vanity
-  const keyPair = vanity ? await findVanityKey(vanity) : undefined
 
+  if (vanity) {
+    try {
+      z32.decode(vanity)
+    } catch (e) {
+      throw new Error(`Vanity key must contain only z32 characters (${e.message})`)
+    }
+
+    if (vanity.length > 4)
+      console.warn(
+        'Warning: Vanity strings longer than 4 characters may take a long time to generate.'
+      )
+  }
+
+  const keyPair = vanity ? await findVanityKey(vanity) : undefined
   const ipc = context.getIPC()
   const json = cmd.flags.json
   await output({ json, ctrlTTY: false, log: (line) => console.log(line) }, ipc.touch({ keyPair }))
