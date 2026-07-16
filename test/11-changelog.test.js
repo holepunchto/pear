5f292eb9071b54394b5803001a3cda0a91772bea
Andrei Regiani <andrei.cpp@gmail.com>
2026-07-16 03:51:58 -0300

    Refactor: module `11-changelog.test` (#1141)
    * 11-changelog.test
    
    * clean up

---
+++ test/11-changelog.test.js
@@ -0,0 +1,45 @@
+'use strict'
+const test = require('brittle')
+const Helper = require('./helper')
+
+test('pear changelog without link returns platform changelog', async ({ not, plan, teardown }) => {
+  plan(1)
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const info = helper.info({ link: null, changelog: { max: 1 }, cmdArgs: [] })
+  teardown(() => Helper.teardownStream(info))
+
+  const data = await Helper.pick(info, { tag: 'changelog' })
+  not(data.changelog, '[ No Changelog ]')
+})
+
+test('pear changelog with link', async ({ ok, is, plan, teardown }) => {
+  plan(3)
+
+  const dir = Helper.fixture('minimal')
+
+  const helper = new Helper()
+  teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const link = await Helper.touchLink(helper)
+  const staging = helper.stage({ link, dir, dryRun: false })
+  teardown(() => Helper.teardownStream(staging))
+  const staged = await Helper.pick(staging, { tag: 'final' })
+  ok(staged.success, 'stage succeeded')
+
+  const seeding = helper.seed({ link, dir, key: null, cmdArgs: [] })
+  teardown(() => Helper.teardownStream(seeding))
+  const seeded = await Helper.pick(seeding, [{ tag: 'key' }, { tag: 'announced' }])
+  await seeded.announced
+
+  const info = helper.info({ link, changelog: { max: 1 }, cmdArgs: [] })
+  teardown(() => Helper.teardownStream(info))
+
+  const data = await Helper.pick(info, { tag: 'changelog' })
+  is(data.version, 'v1.0.0')
+  is(data.changelog, '## v1.0.0\n\nInitial release.')
+})
