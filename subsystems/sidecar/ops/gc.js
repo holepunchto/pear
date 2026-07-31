ff6de51f0d26b7f799e59ce87cd552019eaf0970
thundron <la@thundron.dev>
2026-07-31 15:22:08 +0200

    remove pear gc sidecars
    Signed-off-by: thundron <la@thundron.dev>

--- subsystems/sidecar/ops/gc.js
+++ subsystems/sidecar/ops/gc.js
@@ -1,9 +1,6 @@
 'use strict'
-const { isWindows } = require('which-runtime')
-const os = require('bare-os')
 const fs = require('bare-fs')
 const path = require('bare-path')
-const { spawn } = require('bare-subprocess')
 const { PLATFORM_DIR } = require('../../../constants.js')
 const { ERR_INVALID_GC_RESOURCE } = require('pear-errors')
 const Opstream = require('../lib/opstream')
@@ -17,7 +14,6 @@ module.exports = class GC extends Opstream {
 
   #op(params) {
     if (params.resource === 'releases') return this.releases(params)
-    if (params.resource === 'sidecars') return this.sidecars(params)
     if (params.resource === 'cores') return this.cores(params)
     throw ERR_INVALID_GC_RESOURCE('Invalid resource to gc: ' + params.resource)
   }
@@ -54,64 +50,8 @@ module.exports = class GC extends Opstream {
     this.push({ tag: 'complete', data: { resource, count } })
   }
 
-  sidecars(params) {
-    const { resource, data = {} } = params
-    const { pid } = data
-    const name = 'pear-runtime'
-    const flag = '--sidecar'
-
-    const [sh, args] = isWindows
-      ? [
-          'cmd.exe',
-          [
-            '/c',
-            `wmic process where (name like '%${name}%') get name,executablepath,processid,commandline /format:csv`
-          ]
-        ]
-      : ['/bin/sh', ['-c', `ps ax | grep -i -- '${name}' | grep -i -- '${flag}'`]]
-
-    const sp = spawn(sh, args)
-    let output = ''
-    let pidIndex = isWindows ? -1 : 0
-    let isHeader = !!isWindows
-    let count = 0
-
-    sp.stdout.on('data', (data) => {
-      output += data.toString()
-      const lines = output.split(isWindows ? '\r\r\n' : '\n')
-      output = lines.pop()
-      for (const line of lines) {
-        if (!line.trim()) continue
-        const columns = line.split(isWindows ? ',' : ' ').filter((col) => col)
-        if (isHeader && isWindows) {
-          const index = columns.findIndex((col) => /processid/i.test(col.trim()))
-          pidIndex = index !== -1 ? index : 4
-          isHeader = false
-        } else {
-          const id = parseInt(columns[pidIndex])
-          if (!isNaN(id) && ![Bare.pid, sp.pid, pid].includes(id)) {
-            os.kill(id)
-            this.push({ tag: 'remove', data: { resource, id } })
-            count++
-          }
-        }
-      }
-    })
-
-    return new Promise((resolve, reject) => {
-      sp.on('exit', (code, signal) => {
-        if (code !== 0 || signal) {
-          reject(new Error(`Process exited with code: ${code}, signal: ${signal}`))
-          return
-        }
-        this.push({ tag: 'complete', data: { resource, count } })
-        resolve()
-      })
-    })
-  }
-
   async cores(params) {
-    const { resource, data = {} } = params
+    const { resource } = params
     const { sidecar } = this
 
     const discoveryKeys = []
