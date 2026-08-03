89326097feb69d1bfd448a9cdc3b772e4d612140
rafapaezbas <rafael.paez@tether.io>
2026-08-03 15:37:17 +0200

    cleanup dead gc releases functionality

--- subsystems/sidecar/ops/gc.js
+++ subsystems/sidecar/ops/gc.js
@@ -13,41 +13,8 @@ module.exports = class GC extends Opstream {
   }
 
   #op(params) {
-    if (params.resource === 'releases') return this.releases(params)
     if (params.resource === 'cores') return this.cores(params)
     throw ERR_INVALID_GC_RESOURCE('Invalid resource to gc: ' + params.resource)
-  }
-
-  async releases(params) {
-    const { resource } = params
-    let count = 0
-    const symlinkPath = path.join(PLATFORM_DIR, 'current')
-    const dkeyDir = path.join(PLATFORM_DIR, 'by-dkey')
-
-    try {
-      await fs.promises.stat(dkeyDir)
-    } catch {
-      this.push({ tag: 'complete', data: { resource, count } })
-      return
-    }
-
-    const current = await fs.promises.readlink(symlinkPath)
-    const currentDirPath = path.dirname(current)
-    const currentDirName = path.basename(currentDirPath)
-
-    const dirs = await fs.promises.readdir(dkeyDir, { withFileTypes: true })
-
-    const dirNames = dirs.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name)
-
-    for (const dirName of dirNames) {
-      if (dirName !== currentDirName) {
-        const dirPath = path.join(dkeyDir, dirName)
-        await fs.promises.rm(dirPath, { recursive: true })
-        this.push({ tag: 'remove', data: { resource, id: dirName } })
-        count++
-      }
-    }
-    this.push({ tag: 'complete', data: { resource, count } })
   }
 
   async cores(params) {
