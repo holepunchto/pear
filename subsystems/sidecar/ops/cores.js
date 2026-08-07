c394c917bcc65fa1e2e0a306b74e3fee47fdf270
thundron <la@thundron.dev>
2026-08-07 10:00:44 +0200

    make link mandatory in pear gc cores (#1149)
    * make pear gc cores link mandatory
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * validate pear gc cores links in command
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * validate pear gc cores links in sidecar
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * avoid opening unknown gc cores
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * scope pear gc cores to linked drive
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * preserve writable gc cores
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * derive gc blob core from manifest
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * return gc count through opstream
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * Add logs to gc (#1162)
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    * check that cores gc'd don't appear after removal
    
    Signed-off-by: thundron <la@thundron.dev>
    
    * remove individual cores
    
    * removed --force flag message in gc cores
    
    * gc content key too
    
    ---------
    
    Signed-off-by: thundron <la@thundron.dev>
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    Co-authored-by: Darshan Sen <raisinten@gmail.com>
    Co-authored-by: rafapaezbas <rafael.paez@tether.io>
    Co-authored-by: rafapaezbas <15270736+rafapaezbas@users.noreply.github.com>

--- subsystems/sidecar/ops/cores.js
+++ subsystems/sidecar/ops/cores.js
@@ -52,7 +52,8 @@ module.exports = class Cores extends Opstream {
         tag: 'core',
         data: {
           link,
-          writable
+          writable,
+          length: core.length
         }
       })
     }
---
+++ test/13-gc.test.js
@@ -0,0 +1,83 @@
+'use strict'
+const test = require('brittle')
+const Corestore = require('corestore')
+const Hyperdrive = require('hyperdrive')
+const Hyperswarm = require('hyperswarm')
+const plink = require('pear-link')
+const Helper = require('./helper')
+
+test('pear gc cores with link', async (t) => {
+  t.plan(3)
+
+  const helper = new Helper()
+  t.teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const target = await createDrive(t)
+  const other = await createDrive(t)
+  await cacheDrive(t, helper, target.link)
+  await cacheDrive(t, helper, other.link)
+
+  const collecting = helper.gc({ resource: 'cores', data: { link: target.link } })
+  t.teardown(() => Helper.teardownStream(collecting))
+
+  const result = await Helper.pick(collecting, [{ tag: 'final' }])
+  const final = await result.final
+  t.is(final.success, true)
+
+  const links = []
+  const cores = helper.cores()
+  t.teardown(() => Helper.teardownStream(cores))
+  cores.on('data', ({ tag, data }) => {
+    if (tag === 'core') links.push(data.link)
+  })
+  const listed = await Helper.pick(cores, [{ tag: 'final' }])
+  await listed.final
+
+  t.ok(!links.includes(target.link), 'gc cores are not listed')
+  t.ok(!links.includes(target.content), 'gc cores are not listed')
+})
+
+test('pear gc cores without link', async (t) => {
+  t.plan(1)
+
+  const helper = new Helper()
+  t.teardown(() => helper.close(), { order: Infinity })
+  await helper.ready()
+
+  const collecting = helper.gc({ resource: 'cores', data: {} })
+  t.teardown(() => Helper.teardownStream(collecting))
+
+  await t.exception(async () => {
+    const result = await Helper.pick(collecting, [{ tag: 'final' }])
+    await result.final
+  }, /A link must be specified/)
+})
+
+async function createDrive(t) {
+  const store = new Corestore(await t.tmp())
+  t.teardown(() => store.close())
+  await store.ready()
+
+  const drive = new Hyperdrive(store)
+  await drive.ready()
+  await drive.put('/index.js', Buffer.from('module.exports = true'))
+
+  const swarm = new Hyperswarm({ bootstrap: Helper.dhtBootstrap })
+  t.teardown(() => swarm.destroy())
+  swarm.on('connection', (connection) => store.replicate(connection))
+  const topic = swarm.join(drive.discoveryKey, { server: true, client: false })
+  await topic.flushed()
+
+  return {
+    link: plink.serialize({ drive: { key: drive.key } }),
+    content: drive.blobs.core.key
+  }
+}
+
+async function cacheDrive(t, helper, link) {
+  const dumping = helper.dump({ link, dir: await t.tmp() })
+  t.teardown(() => Helper.teardownStream(dumping))
+  const result = await Helper.pick(dumping, [{ tag: 'final' }])
+  await result.final
+}
