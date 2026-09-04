d133cc39e853963b061823c9ef114b55f570807e
thundron <la@thundron.dev>
2026-09-04 12:49:20 +0200

    add relay flag (#1209)
    Signed-off-by: thundron <la@thundron.dev>

--- subsystems/sidecar/index.js
+++ subsystems/sidecar/index.js
@@ -72,6 +72,7 @@ class Sidecar extends ReadyResource {
     this.activeBlindRelay = null
     this.activeBlindPeer = null
     this.nodes = nodes
+    this.relayPublicKey = null
     this.ipc = new IPC.Server({
       handlers: this,
       socketPath: SOCKET_PATH
@@ -176,6 +177,10 @@ class Sidecar extends ReadyResource {
 
   blindRelay(params, client) {
     return new ops.BlindRelay(params, client, this)
+  }
+
+  relay({ publicKey }) {
+    this.relayPublicKey = publicKey
   }
 
   versions(params, client) {
@@ -319,7 +324,8 @@ class Sidecar extends ReadyResource {
 
     this.swarm = new Hyperswarm({
       dht: this.dht,
-      keyPair: this.keyPair
+      keyPair: this.keyPair,
+      relayThrough: (force) => (force || this.dht.randomized ? this.relayPublicKey : null)
     })
     this.swarm.once('close', () => {
       this.swarm = null
