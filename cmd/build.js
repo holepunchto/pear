e3896dfe5ff9ccd89a0f75529ac6a3929067ed28
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-09-23 21:41:42 +0800

    Use building event instead of deriving app details

--- cmd/build.js
+++ cmd/build.js
@@ -1,9 +1,7 @@
 'use strict'
 const path = require('bare-path')
-const fs = require('bare-fs')
 const { Readable } = require('streamx')
 const pearBuild = require('pear-build')
-const { ERR_NOT_FOUND, ERR_INVALID_INPUT } = require('pear-errors')
 const { outputter, ansi, stdio } = require('../lib/terminal.js')
 
 const output = outputter('build', {
@@ -29,26 +27,25 @@ const output = outputter('build', {
 
 module.exports = async function build(cmd) {
   const json = cmd.flags.json
-  if (!cmd.flags.package) throw ERR_INVALID_INPUT('package.json path must be specified.')
-
-  const pkgPath = path.resolve(cmd.flags.package)
-
-  // TODO: Use pkg from pear-build when PR is merged
-  const pkg = await getParsedJSON('package.json', pkgPath)
-
-  const name = pkg.name || pkg.productName || ''
-  const version = pkg.version || ''
-  const target = path.resolve(cmd.flags.target || `${name}-${version}`)
 
   const stream = new Readable()
   const outputPromise = output(json, stream)
   outputPromise.catch(() => {})
 
-  stream.push({ tag: 'building', data: { name, version, target } })
-
   const builder = pearBuild(cmd.flags)
   builder.on('error', (err) => {
     stream.destroy(err)
+  })
+
+  builder.on('building', ({ pkg, target }) => {
+    stream.push({
+      tag: 'building',
+      data: {
+        name: pkg.productName || pkg.name || '',
+        version: pkg.version || '',
+        target
+      }
+    })
   })
 
   builder.on('mirrored', ({ from, to }) => {
@@ -70,23 +67,5 @@ module.exports = async function build(cmd) {
   } catch (err) {
     stream.destroy(err)
     throw err
-  }
-}
-
-async function getParsedJSON(name, path) {
-  const file = await fs.promises.readFile(path, 'utf8').catch((err) => {
-    if (err.code === 'ENOENT') {
-      throw ERR_NOT_FOUND(name + ' not found', { path, cause: err })
-    }
-    if (err.code === 'EISDIR') {
-      throw ERR_INVALID_INPUT(name + ' must be a file', { path, cause: err })
-    }
-    throw err
-  })
-
-  try {
-    return JSON.parse(file)
-  } catch (err) {
-    throw ERR_INVALID_INPUT(name + ' is not a valid JSON', { path, cause: err })
   }
 }
